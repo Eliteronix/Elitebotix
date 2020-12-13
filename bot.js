bd201032129a8e31bcf8f0b7446dd78504b9fcba
Eliteronix <Eliteronix@users.noreply.github.com>
2020-12-13 14:50:01 +0100

    EL-4

--- bot.js
+++ bot.js
@@ -1,27 +1,44 @@
+//Log message upon starting the bot
 console.log('Bot is starting...');
 
+//require the dotenv node module
 require('dotenv').config();
 
+//require the discord.js module
 const Discord = require('discord.js');
+//import the config variables from config.json
+const {prefix, botid} = require('./config.json');
+//create a Discord client with discord.js
 const client = new Discord.Client();
+//login with the Discord client using the Token from the .env file
 client.login(process.env.BOTTOKEN);
 
+//declare weebEmojis array
+var weebEmojis = ['owo', 'uwu', 'UwU', 'OwO', 'OuO'];
+
+//declare what the discord client should do when it's ready
 client.on('ready', readyDiscord);
 
+//declare the function which will be used when ready
 function readyDiscord() {
+    //log a message when ready
     console.log('The Bot is ready.');
 }
 
+//declare what the discord client should do when it's ready
 client.on('message', gotMessage);
 
+//declare function which will be used when message received
 function gotMessage(msg) {
-    console.log(msg.content);
-    if(msg.author.username != 'Elitebotix') {
-        if (msg.content === 'Beep') {
-            msg.channel.send('Boop');
-        }
-        if (msg.content === 'owo') {
-            msg.channel.send('owo');
+    //log the message
+    console.log(msg);
+
+    //check if the message wasn't sent by the bot itself
+    if(msg.author.id != botid) {
+
+        //Answer with a random weebEmoji if a weebEmoji was sent
+        if (weebEmojis.includes(msg.content)) {
+            msg.channel.send(weebEmojis[Math.floor(Math.random()*weebEmojis.length)]);
         }
     }
 }
