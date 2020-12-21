5af2b62de7f9e6bb7c9badf91703b3386b77e9bc
Eliteronix <Eliteronix@users.noreply.github.com>
2020-12-21 16:27:29 +0100

    Create creator.js

---
+++ commands/creator.js
@@ -0,0 +1,27 @@
+//Require discord.js module
+const Discord = require('discord.js');
+
+module.exports = {
+	name: 'creator',
+	description: 'Sends an info card about the developer',
+	aliases: ['developer'],
+	guildOnly: true,
+	cooldown: 5,
+	execute(msg, args, prefixCommand) {
+		if (prefixCommand) {
+			// inside a command, event listener, etc.
+			const creatorInfoEmbed = new Discord.MessageEmbed()
+				.setColor('#0492C2')
+				.setTitle('Creator info card')
+				.setThumbnail(`${msg.client.users.cache.find(user => user.id === '138273136285057025').avatarURL()}`)
+				.addFields(
+					{ name: 'Discord', value: 'Eliteronix#4208'},
+					{ name: 'Github', value: 'Eliteronix' },
+					{ name: 'Twitter', value: '@Eliteronix' }
+				)
+				.setTimestamp();
+
+			msg.channel.send(creatorInfoEmbed);
+		}
+	},
+};
