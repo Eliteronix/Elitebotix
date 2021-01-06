46832ffd7e1841fd9f26ae38a26c2fc231efd19a
Eliteronix <Eliteronix@users.noreply.github.com>
2021-01-06 16:47:03 +0100

    Edited reaction role command and ReactionRolesHeader Table

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -1,20 +1,82 @@
+//require the discord.js module
+const Discord = require('discord.js');
+
+//require the ReactionRolesHeader Table
+const { ReactionRolesHeader } = require('../dbObjects');
+
+//require the sequelize module
+const Sequelize = require('sequelize');
+
 module.exports = {
 	name: 'reactionrole',
 	aliases: ['reactionroles','rr'],
-	description: 'Create and manage reaction roles',
-	usage: '<bug/feature/request> <description>', //Change
+	description: 'Create and manage reaction roles', //Maybe change color, description.. afterwards with set
+	usage: '<embed/role> <name of new embed/existing embed ID> <emoji> <@role> <@description>', //Change
 	permissions: 'MANAGE_ROLES',
 	permissionsTranslated: 'Manage Roles',
 	guildOnly: true,
 	args: true,
 	cooldown: 5,
 	//noCooldownMessage: true,
-	execute(msg, args, prefixCommand) {
+	async execute(msg, args, prefixCommand) {
 		if (prefixCommand) {
-			//e!rr add embed <name> //Write ID in Footer
-			//e!rr remove embed <name> or <ID>
-			//e!rr add role <embedId> <@role> <emoji> <description>
-			//e!rr remove role <embedId> <@role> <emoji> <description>
+			//Check the first argument
+			if(args[0] === 'embed'){
+				//Check the second argument
+				if(args[1] === 'add'){
+					//Check if a name was set for the embed
+					if(args[2]){
+						//Remove the first two items
+						args.shift();
+						args.shift();
+						//Join the name string
+						const embedName = args.join(' ');
+						const reactionRolesHeader = await ReactionRolesHeader.findOne({
+							order: Sequelize.fn('max', Sequelize.col('reactionHeaderId')),
+						});
+						let embedId;
+						if(reactionRolesHeader){
+							embedId = reactionRolesHeader.reactionHeaderId + 1;
+						} else {
+							embedId = '1';
+						}
+						const reactionRoleEmbed = new Discord.MessageEmbed()
+							.setColor('#0099ff')
+							.setTitle(embedName)
+							.setFooter(`Reactionrole - EmbedId: ${embedId}`);
+
+						msg.channel.send(reactionRoleEmbed);
+					} else {
+						msg.channel.send('Please specify what name you want to give the embed you want to create.');
+					}
+
+				//Check the second argument
+				} else if(args[1] === 'remove'){
+
+				} else {
+					//incorrect second argument
+					msg.channel.send('Please specify if you want to add or remove the embed.');
+				}
+			//Check the first argument
+			} else if(args[0] === 'role'){
+				//Check the second argument
+				if(args[1] === 'add'){
+
+				//Check the second argument
+				} else if(args[1] === 'remove'){
+
+				} else {
+					//incorrect second argument
+					msg.channel.send('Please specify if you want to add or remove the embed.');
+				}
+			} else {
+				//Incorrect first argument
+				msg.channel.send('Please provide what kind of object you want to add / remove.');
+			}
+			//e!rr embed 	add 	<name> //Write ID in Footer
+			//e!rr embed 	remove 	<name> or <ID>
+			//e!rr role 	add 	<embedId> 	<emoji>		<@role> 	<description>
+			//e!rr role 	remove 	<embedId> 	<@role>
 			
 			//reactionRoles.forEach(entry => {
   			//	embed.addField(entry, 'looped field');
--- models/ReactionRolesHeader.js
+++ models/ReactionRolesHeader.js
@@ -1,13 +1,18 @@
 module.exports = (sequelize, DataTypes) => {
 	return sequelize.define('ReactionRolesHeader', {
-		reactionHeaderId: {
-			type: DataTypes.STRING,
-			allowNullValue: false,
+		reactionRolesHeaderId: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
 			primaryKey: true,
 		},
 		guildId: {
 			type: DataTypes.STRING,
 			allowNullValue: false,
+		},
+		reactionHeaderId: {
+			type: DataTypes.STRING,
+			allowNullValue: false,
+			primaryKey: true,
 		},
 		reactionTitle: {
 			type: DataTypes.STRING,
