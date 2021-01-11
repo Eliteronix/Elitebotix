40ed2f3355cc67c5bf893b059b2f887822c5c2a9
Marvin Zimmermann <64707347+Eliteronix@users.noreply.github.com>
2021-01-11 10:58:38 +0100

    Update dbInit.js

--- dbInit.js
+++ dbInit.js
@@ -1,11 +1,5 @@
 const Sequelize = require('sequelize');
 
-// //Import ReactionRolesHeader Table
-// const { ReactionRolesHeader } = require('./dbObjects');
-
-// //Import ReactionRoles Table
-// const { ReactionRoles } = require('./dbObjects');
-
 const sequelize = new Sequelize('database', 'username', 'password', {
 	host: 'localhost',
 	dialect: 'sqlite',
@@ -17,19 +11,6 @@ require('./models/Guilds')(sequelize, Sequelize.DataTypes);
 require('./models/ReactionRoles')(sequelize, Sequelize.DataTypes);
 require('./models/ReactionRolesHeader')(sequelize, Sequelize.DataTypes);
 require('./models/AutoRoles')(sequelize, Sequelize.DataTypes);
-
-// ReactionRolesHeader.sync({ force: true })
-// 	.then(async () => {
-// 		console.log('Database synced');
-// 		sequelize.close();
-// 	})
-// 	.catch(console.error);
-// ReactionRoles.sync({ force: true })
-// 	.then(async () => {
-// 		console.log('Database synced');
-// 		sequelize.close();
-// 	})
-// 	.catch(console.error);
 
 sequelize.sync({ alter: true })
 	.then(async () => {
