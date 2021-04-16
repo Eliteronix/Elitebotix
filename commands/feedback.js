ac70325c8698da5d1df9967f94098867b976f03b
Eliteronix <zimmermann.mariomarvin@gmail.com>
2021-04-16 21:49:23 +0200

    Update feedback.js

--- commands/feedback.js
+++ commands/feedback.js
@@ -87,3 +87,154 @@ module.exports = {
 		}
 	},
 };
+
+// This code sample uses the 'node-fetch' library:
+// https://www.npmjs.com/package/node-fetch
+const fetch = require('node-fetch');
+
+const bodyData = `{
+  "update": {},
+  "fields": {
+    "summary": "Main order flow broken",
+    "parent": {
+      "key": "PROJ-123"
+    },
+    "issuetype": {
+      "id": "10000"
+    },
+    "components": [
+      {
+        "id": "10000"
+      }
+    ],
+    "customfield_20000": "06/Jul/19 3:25 PM",
+    "customfield_40000": {
+      "type": "doc",
+      "version": 1,
+      "content": [
+        {
+          "type": "paragraph",
+          "content": [
+            {
+              "text": "Occurs on all orders",
+              "type": "text"
+            }
+          ]
+        }
+      ]
+    },
+    "customfield_70000": [
+      "jira-administrators",
+      "jira-software-users"
+    ],
+    "project": {
+      "id": "10000"
+    },
+    "description": {
+      "type": "doc",
+      "version": 1,
+      "content": [
+        {
+          "type": "paragraph",
+          "content": [
+            {
+              "text": "Order entry fails when selecting supplier.",
+              "type": "text"
+            }
+          ]
+        }
+      ]
+    },
+    "reporter": {
+      "id": "5b10a2844c20165700ede21g"
+    },
+    "fixVersions": [
+      {
+        "id": "10001"
+      }
+    ],
+    "customfield_10000": "09/Jun/19",
+    "priority": {
+      "id": "20000"
+    },
+    "labels": [
+      "bugfix",
+      "blitz_test"
+    ],
+    "timetracking": {
+      "remainingEstimate": "5",
+      "originalEstimate": "10"
+    },
+    "customfield_30000": [
+      "10000",
+      "10002"
+    ],
+    "customfield_80000": {
+      "value": "red"
+    },
+    "security": {
+      "id": "10000"
+    },
+    "environment": {
+      "type": "doc",
+      "version": 1,
+      "content": [
+        {
+          "type": "paragraph",
+          "content": [
+            {
+              "text": "UAT",
+              "type": "text"
+            }
+          ]
+        }
+      ]
+    },
+    "versions": [
+      {
+        "id": "10000"
+      }
+    ],
+    "duedate": "2019-05-11",
+    "customfield_60000": "jira-software-users",
+    "customfield_50000": {
+      "type": "doc",
+      "version": 1,
+      "content": [
+        {
+          "type": "paragraph",
+          "content": [
+            {
+              "text": "Could impact day-to-day work.",
+              "type": "text"
+            }
+          ]
+        }
+      ]
+    },
+    "assignee": {
+      "id": "5b109f2e9729b51b54dc274d"
+    }
+  }
+}`;
+
+fetch('https://your-domain.atlassian.com/rest/api/3/issue', {
+	method: 'POST',
+	headers: {
+		// eslint-disable-next-line no-undef
+		'Authorization': `Basic ${Buffer.from(
+			'email@example.com:<api_token>'
+		).toString('base64')}`,
+		'Accept': 'application/json',
+		'Content-Type': 'application/json'
+	},
+	body: bodyData
+})
+	.then(response => {
+		console.log(
+			`Response: ${response.status} ${response.statusText}`
+		);
+		return response.text();
+	})
+	.then(text => console.log(text))
+	.catch(err => console.error(err));
