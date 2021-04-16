b5c337bcb3e69cb742936f9215ca944fc53cddd5
Eliteronix <zimmermann.mariomarvin@gmail.com>
2021-04-16 22:18:36 +0200

    Create givePointsToPlayers.js

---
+++ MOTD/givePointsToPlayers.js
@@ -0,0 +1,10 @@
+module.exports = {
+	getMOTDMapsOnTime: async function (client) {
+		console.log(client);
+
+		//Players gain Points from qualifications -> Should probably have a max and then go down
+		// -> Maybe 1k maximum to gain
+		//Players also gain points from knockout -> Should scale by the lobby
+		// -> Maybe about 10% of the max points a player could have gotten in that lobby for qualis
+	}
+};
