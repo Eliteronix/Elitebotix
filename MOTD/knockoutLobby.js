b964f4822809d551d4d4e4474f5d22d2da9d094c
Eliteronix <zimmermann.mariomarvin@gmail.com>
2021-04-19 22:51:36 +0200

    Updated MOTD

--- MOTD/knockoutLobby.js
+++ MOTD/knockoutLobby.js
@@ -2,6 +2,15 @@ module.exports = {
 	knockOutLobby: async function (client, mappool, lobbyNumber, players, users) {
 		//Map [0] has been played already
 
-
+		//Map [1] 16 -> 14
+		//Map [2] 14 -> 12
+		//Map [3] 12 -> 10
+		//Map [4] 10 -> 8 --DT
+		//Map [5] 8 -> 6
+		//Map [6] 6 -> 5
+		//Map [7] 5 -> 4
+		//Map [8] 4 -> 3 --DT
+		//Map [9] 3 -> 2
+		//Map [10] 2 -> 1
 	}
 };
--- MOTD/qualifier.js
+++ MOTD/qualifier.js
@@ -25,6 +25,19 @@ module.exports = {
 		}
 
 		// Catch case of qualifiers actually being played
+		//Qualifiers
+		//Send messages to inform players
+		await sendQualifierMessages(client, mappool[0], users);
+
+		//wait 10 minutes
+		await setTimeout(function () {
+
+		}, 1000 * 60 * 10);
+
+		//Grab qualifier results of all players
+		const results = await getQualifierResults(mappool[0], players);
+
+		//Divide sorted players into knockout lobbies
 		divideIntoGroups(client, mappool, 1, players, users);
 	}
 };
@@ -51,4 +64,29 @@ function divideIntoGroups(client, mappool, lobbyNumber, players, users) {
 	if (otherPlayers.length > 0) {
 		divideIntoGroups(client, mappool, lobbyNumber + 1, otherPlayers, otherUsers);
 	}
+}
+
+async function sendQualifierMessages(client, map, users) {
+	for (let i = 0; i < users.length; i++) {
+		let data = [];
+		data.push(`There are ${users.length} players registered today for this bracket!`);
+		data.push('Try to get your best score possible in the next 10 minutes on the following map to qualify for a knockout lobby.');
+		data.push('**The map is FreeMod - `NF` will be countered and scores will be doubled - Don\'t use `ScoreV2`, `Relax`, `Autopilot`, `Auto`**');
+		data.push(`${map.artist} - ${map.title} [${map.version}]`);
+		data.push(`${Math.round(map[i].difficulty.rating * 100) / 100}* | ${Math.floor(map[i].length.total / 60)}:${(map[i].length.total % 60).toString().padStart(2, '0')} | ${map[i].bpm} BPM | CS ${map[i].difficulty.size} | HP ${map[i].difficulty.drain} | OD ${map[i].difficulty.overall} | AR ${map[i].difficulty.approach}`);
+		data.push(`Website: <https://osu.ppy.sh/b/${map[i].id}> | osu! direct: <osu://dl/${map[i].beatmapSetId}>`);
+		await users[i].send(data, { split: true })
+			.catch(async () => {
+				const channel = await client.channels.fetch('833803740162949191');
+				channel.send(`<@${users[i].id}>, it seems like I can't DM you. Please enable DMs so that I can keep you up to date with the match procedure!`);
+			});
+	}
+}
+
+async function getQualifierResults(map, players) {
+	let results = [];
+
+	
+
+	return results;
 }
