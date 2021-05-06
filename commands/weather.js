a25f1a32f3a00d31e83dbff478578b657d7a53b2
Eliteronix <zimmermann.mariomarvin@gmail.com>
2021-05-06 23:17:53 +0200

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -15,7 +15,7 @@ module.exports = {
 	args: true,
 	cooldown: 5,
 	//noCooldownMessage: true,
-	tags: 'general',
+	tags: 'misc',
 	prefixCommand: true,
 	// eslint-disable-next-line no-unused-vars
 	async execute(msg, args) {
@@ -27,6 +27,10 @@ module.exports = {
 
 		weather.find({ search: args.join(' '), degreeType: degreeType }, async function (err, result) {
 			if (err) console.log(err);
+
+			if (!result[0]) {
+				return msg.channel.send(`Could not find location \`${args.join(' ').replace(/`/g, '')}\``);
+			}
 
 			const weather = result[0];
 
@@ -51,30 +55,58 @@ module.exports = {
 				}
 			}
 
-			// Write the title of the changelog
+			// Write the title
 			ctx.font = 'bold 35px comfortaa, sans-serif';
 			ctx.fillStyle = '#ffffff';
 			ctx.textAlign = 'center';
 			ctx.fillText(`Weather for ${weather.location.name}`, canvas.width / 2, 50);
+
+			// Write the weather of the current day
+			ctx.font = 'bold 30px comfortaa, sans-serif';
+			ctx.textAlign = 'center';
+			ctx.fillText(`${weather.current.day} at ${weather.current.observationtime.substring(0, 5)}`, 350, 175);
+			ctx.fillText(`Temp.: ${weather.current.temperature}°${degreeType} | Feels Like: ${weather.current.feelslike}°${degreeType}`, 350, 250);
+			ctx.fillText(`Humidity: ${weather.current.humidity}`, 350, 325);
+			ctx.fillText(`Wind: ${weather.current.winddisplay}`, 350, 400);
+
+			// Write the forecast
+			ctx.font = 'bold 25px comfortaa, sans-serif';
+			ctx.textAlign = 'left';
+			ctx.fillText('3-Day Forecast:', 725, 100);
+			ctx.fillText(`${weather.forecast[2].day}:`, 725, 150);
+			ctx.fillText(`${weather.forecast[2].skytextday}`, 725, 185);
+			ctx.fillText(`${weather.forecast[2].low} - ${weather.forecast[2].high}°${degreeType} | ${weather.forecast[2].precip}% Rain`, 725, 220);
+			ctx.fillText(`${weather.forecast[3].day}:`, 725, 270);
+			ctx.fillText(`${weather.forecast[3].skytextday}`, 725, 305);
+			ctx.fillText(`${weather.forecast[3].low} - ${weather.forecast[3].high}°${degreeType} | ${weather.forecast[3].precip}% Rain`, 725, 340);
+			ctx.fillText(`${weather.forecast[4].day}:`, 725, 390);
+			ctx.fillText(`${weather.forecast[4].skytextday}`, 725, 425);
+			ctx.fillText(`${weather.forecast[4].low} - ${weather.forecast[4].high}°${degreeType} | ${weather.forecast[4].precip}% Rain`, 725, 460);
 
 			let today = new Date().toLocaleDateString();
 
 			ctx.font = '12px comfortaa, sans-serif';
 			ctx.fillStyle = '#ffffff';
 			ctx.textAlign = 'left';
-			ctx.fillText(`Lat: ${weather.location.lat} | Long: ${weather.location.long}`, 5, canvas.height - 5);
+			ctx.fillText(`Lat.: ${weather.location.lat} | Long.: ${weather.location.long}`, 5, canvas.height - 5);
 			ctx.textAlign = 'right';
 			ctx.fillText(`Made by Elitebotix on ${today}`, canvas.width - 5, canvas.height - 5);
 
-			// ctx.font = 'bold 25px comfortaa, sans-serif';
-			// ctx.textAlign = 'left';
-			// ctx.fillText(argArray[0], 100, 150);
 
+			//Get a circle in the middle for inserting the map background
+			ctx.beginPath();
+			ctx.arc(75, 75, 50, 0, Math.PI * 2, true);
+			ctx.closePath();
+			ctx.clip();
+
+			//Draw a shape onto the main canvas in the top left
+			const weatherPic = await Canvas.loadImage(weather.current.imageUrl);
+			ctx.drawImage(weatherPic, 25, 25, 100, 100);
 
 			//Create as an attachment
 			const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'elitebotix-changelog.png');
 
-			msg.channel.send('**Elitebotix has been updated** - Please report any bugs by using `e!feedback bug <Description>`.', attachment);
+			msg.channel.send(`Weather for ${weather.location.name}`, attachment);
 		});
 	},
 };
