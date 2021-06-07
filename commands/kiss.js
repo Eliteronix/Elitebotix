a50e68672a7ba023ee4a02fe01632d3f04ccda2e
Eliteronix <zimmermann.mariomarvin@gmail.com>
2021-06-07 22:36:32 +0200

    Made it possible to mention multiple users at once

--- commands/kiss.js
+++ commands/kiss.js
@@ -17,13 +17,15 @@ module.exports = {
 	prefixCommand: true,
 	// eslint-disable-next-line no-unused-vars
 	async execute(msg, args) {
-		if(msg.mentions.users.first()){
-			// eslint-disable-next-line no-undef
-			let url = `https://g.tenor.com/v1/search?q=kiss%20anime&key=${process.env.TENORTOKEN}&contentfilter=high`;
-			let response = await fetch(url);
-			let json = await response.json();
-			const index = Math.floor(Math.random() * json.results.length);
-			msg.channel.send(`<@${msg.author.id}> has kissed <@${msg.mentions.users.first().id}>\n${json.results[index].url}`);
+		if (msg.mentions.users.first()) {
+			msg.mentions.users.forEach(async (user) => {
+				// eslint-disable-next-line no-undef
+				let url = `https://g.tenor.com/v1/search?q=kiss%20anime&key=${process.env.TENORTOKEN}&contentfilter=high`;
+				let response = await fetch(url);
+				let json = await response.json();
+				const index = Math.floor(Math.random() * json.results.length);
+				msg.channel.send(`<@${msg.author.id}> has kissed <@${user.id}>\n${json.results[index].url}`);
+			});
 		} else {
 			msg.reply('please mention a user.');
 		}
