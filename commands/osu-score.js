f5381a076607be9f2fff5c3525ee6a24bde55c34
Marvin Zimmermann <64707347+Eliteronix@users.noreply.github.com>
2021-07-12 18:01:39 +0200

    Update osu-score.js

--- commands/osu-score.js
+++ commands/osu-score.js
@@ -125,10 +125,19 @@ async function getScore(msg, beatmap, username, server, mode, noLinkedAccount, m
 			completeScores: false, // When fetching scores also fetch the beatmap they are for (Allows getting accuracy) (default: false)
 			parseNumeric: false // Parse numeric values into numbers/floats, excluding ids
 		});
+
+		const discordUser = await DBDiscordUsers.findOne({
+			where: { osuUserId: username }
+		});
+
+		if (discordUser && discordUser.osuUserId) {
+			username = discordUser.osuName;
+		}
+
 		osuApi.getScores({ b: beatmap.id, u: username, m: mode })
 			.then(async (scores) => {
 				if (!(scores[0])) {
-					return msg.channel.send(`Couldn't find any scores for \`${username.replace(/`/g, '')}\` on \`${beatmap.id}\`.`);
+					return msg.channel.send(`Couldn't find any scores for \`${username.replace(/`/g, '')}\` on \`${beatmap.artist} - ${beatmap.title} [${beatmap.version}] (${beatmap.id})\`.`);
 				}
 				let scoreHasBeenOutput = false;
 				for (let i = 0; i < scores.length; i++) {
