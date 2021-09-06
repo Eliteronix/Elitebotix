c834184dae53116409e1a1eac80336f2eeaa08c0
Marvin Zimmermann <64707347+Eliteronix@users.noreply.github.com>
2021-09-06 08:49:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -190,7 +190,7 @@ client.on('emojiUpdate', emojiUpdate);
 
 client.on('emojiDelete', emojiDelete);
 
-setInterval(() => executeNextProcessQueueTask(client), 1000);
+setInterval(() => executeNextProcessQueueTask(client), 800);
 
 setInterval(() => initializeMOTD(client, bancho, false, false), 60000);
 
