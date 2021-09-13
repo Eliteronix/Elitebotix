1d7a79c478376209dccaa1a665b8df87bc4f985b
Marvin Zimmermann <64707347+Eliteronix@users.noreply.github.com>
2021-09-13 13:14:06 +0200

    Updated invite link

--- guildCreate.js
+++ guildCreate.js
@@ -8,14 +8,14 @@ module.exports = async function (guild) {
 	try {
 		const systemChannel = await guild.channels.cache.get(guild.systemChannelId);
 		if (systemChannel) {
-			systemChannel.send('Thanks for adding me to the server!\nUse `e!help` to get a list of commands!\nTo provide feedback please use `e!feedback`\nTo stay informed about changes go to <https://discord.com/invite/Asz5Gfe> and follow <#804658828883787784>');
+			systemChannel.send('Thanks for adding me to the server!\nUse `e!help` to get a list of commands!\nTo provide feedback please use `e!feedback` or `/feedback`');
 		} else {
 			const generalChannel = await guild.channels.cache.find(channel => channel.name === 'general');
 			if (generalChannel) {
-				generalChannel.send('Thanks for adding me to the server!\nUse `e!help` to get a list of commands!\nTo provide feedback please use `e!feedback`\nTo stay informed about changes go to <https://discord.com/invite/Asz5Gfe> and follow <#804658828883787784>');
+				generalChannel.send('Thanks for adding me to the server!\nUse `e!help` to get a list of commands!\nTo provide feedback please use `e!feedback` or `/feedback`');
 			} else {
 				const otherChannel = guild.channels.cache.find(channel => channel.type === 'text' && channel.permissionsFor(guild.me).has('SEND_MESSAGES'));
-				otherChannel.send('Thanks for adding me to the server!\nUse `e!help` to get a list of commands!\nTo provide feedback please use `e!feedback`\nTo stay informed about changes go to <https://discord.com/invite/Asz5Gfe> and follow <#804658828883787784>');
+				otherChannel.send('Thanks for adding me to the server!\nUse `e!help` to get a list of commands!\nTo provide feedback please use `e!feedback` or `/feedback`');
 			}
 		}
 	} catch (error) {
