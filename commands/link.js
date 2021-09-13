0d30e40dab86633189880a78859a014d3033bc6c
Marvin Zimmermann <64707347+Eliteronix@users.noreply.github.com>
2021-09-13 13:19:34 +0200

    Update link.js

--- commands/link.js
+++ commands/link.js
@@ -16,7 +16,7 @@ module.exports = {
 	// eslint-disable-next-line no-unused-vars
 	execute(msg, args, interaction, additionalObjects) {
 		if (interaction) {
-			return interaction.reply('Here is a [link](https://discord.com/oauth2/authorize?client_id=784836063058329680&scope=bot+applications.commands&permissions=285256792) to add the bot to your server)');
+			return interaction.reply('Here is a [link](https://discord.com/oauth2/authorize?client_id=784836063058329680&scope=bot+applications.commands&permissions=285256792) to add the bot to your server');
 		}
 
 		//Link with permissions | Administrator, Manage Roles, Manage Channels, Read Messages, Send Messages, Manage Messages, Attach Files, View Channel, Move Members, Add Reactions
