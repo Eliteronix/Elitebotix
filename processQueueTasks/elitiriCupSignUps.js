39dfadedb998869c84d2abd419e6e8fbdf5a5a7e
Marvin Zimmermann <64707347+Eliteronix@users.noreply.github.com>
2021-09-17 11:38:29 +0200

    Added backup for fails

--- processQueueTasks/elitiriCupSignUps.js
+++ processQueueTasks/elitiriCupSignUps.js
@@ -18,103 +18,121 @@ module.exports = {
 	},
 };
 
-async function updateSheet(spreadsheetID, bracketName) {
+async function updateSheet(spreadsheetID, bracketName, processQueueEntry) {
 	// eslint-disable-next-line no-undef
 	if (process.env.SERVER !== 'Live') {
 		return;
 	}
 
-	// Initialize the sheet - doc ID is the long id in the sheets URL
-	const doc = new GoogleSpreadsheet(spreadsheetID);
+	try {
+		// Initialize the sheet - doc ID is the long id in the sheets URL
+		const doc = new GoogleSpreadsheet(spreadsheetID);
 
-	// Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
-	await doc.useServiceAccountAuth({
-		// eslint-disable-next-line no-undef
-		client_email: process.env.GOOGLESHEETSSERVICEACCOUNTMAIL,
-		// eslint-disable-next-line no-undef
-		private_key: process.env.GOOGLESHEETSSERVICEACCOUNTPRIVATEKEY.replace(/\\n/g, '\n'),
-	});
+		// Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
+		await doc.useServiceAccountAuth({
+			// eslint-disable-next-line no-undef
+			client_email: process.env.GOOGLESHEETSSERVICEACCOUNTMAIL,
+			// eslint-disable-next-line no-undef
+			private_key: process.env.GOOGLESHEETSSERVICEACCOUNTPRIVATEKEY.replace(/\\n/g, '\n'),
+		});
 
-	await doc.loadInfo(); // loads document properties and worksheet
+		await doc.loadInfo(); // loads document properties and worksheet
 
-	const sheet = doc.sheetsByTitle['Player List'];
+		const sheet = doc.sheetsByTitle['Player List'];
 
-	let bracketPlayers = await DBElitiriCupSignUp.findAll({
-		where: {
-			tournamentName: 'Elitiri Cup Summer 2021',
-			bracketName: bracketName
-		}
-	});
-
-	quicksort(bracketPlayers);
-
-	await sheet.loadCells('B4:J500');
-	for (let i = 0; i < 496; i++) {
-		let numberValue = '';
-		let playerName = '';
-		let playerID = '';
-		let playerRank = '';
-		let discordTag = '';
-		let time = '';
-		let connected = '';
-		let badges = '';
-		if (bracketPlayers[i]) {
-			numberValue = i + 1;
-			playerName = bracketPlayers[i].osuName;
-			playerID = bracketPlayers[i].osuUserId;
-			playerRank = bracketPlayers[i].osuRank;
-			discordTag = bracketPlayers[i].discordTag;
-			if (bracketPlayers[i].saturdayEarlyAvailability) {
-				time = `${bracketPlayers[i].saturdayEarlyAvailability} - ${bracketPlayers[i].saturdayLateAvailability} UTC | ${bracketPlayers[i].sundayEarlyAvailability} - ${bracketPlayers[i].sundayLateAvailability} UTC`;
-			} else {
-				time = 'No availabilities set';
+		let bracketPlayers = await DBElitiriCupSignUp.findAll({
+			where: {
+				tournamentName: 'Elitiri Cup Summer 2021',
+				bracketName: bracketName
 			}
-			connected = 'Yes';
-			badges = bracketPlayers[i].osuBadges;
-		}
+		});
 
-		const NumberCell = sheet.getCell(3 + i, 1); //getCell(row, column) zero-indexed
-		if (NumberCell.value !== numberValue) {
-			NumberCell.value = numberValue;
-		}
+		quicksort(bracketPlayers);
 
-		const PlayerNameCell = sheet.getCell(3 + i, 2); //getCell(row, column) zero-indexed
-		if (PlayerNameCell.value !== playerName) {
-			PlayerNameCell.value = playerName;
-		}
+		await sheet.loadCells('B4:J500');
+		for (let i = 0; i < 496; i++) {
+			let numberValue = '';
+			let playerName = '';
+			let playerID = '';
+			let playerRank = '';
+			let discordTag = '';
+			let time = '';
+			let connected = '';
+			let badges = '';
+			if (bracketPlayers[i]) {
+				numberValue = i + 1;
+				playerName = bracketPlayers[i].osuName;
+				playerID = bracketPlayers[i].osuUserId;
+				playerRank = bracketPlayers[i].osuRank;
+				discordTag = bracketPlayers[i].discordTag;
+				if (bracketPlayers[i].saturdayEarlyAvailability) {
+					time = `${bracketPlayers[i].saturdayEarlyAvailability} - ${bracketPlayers[i].saturdayLateAvailability} UTC | ${bracketPlayers[i].sundayEarlyAvailability} - ${bracketPlayers[i].sundayLateAvailability} UTC`;
+				} else {
+					time = 'No availabilities set';
+				}
+				connected = 'Yes';
+				badges = bracketPlayers[i].osuBadges;
+			}
 
-		const PlayerIDCell = sheet.getCell(3 + i, 3); //getCell(row, column) zero-indexed
-		if (PlayerIDCell.value !== playerID) {
-			PlayerIDCell.value = playerID;
-		}
+			const NumberCell = sheet.getCell(3 + i, 1); //getCell(row, column) zero-indexed
+			if (NumberCell.value !== numberValue) {
+				NumberCell.value = numberValue;
+			}
+
+			const PlayerNameCell = sheet.getCell(3 + i, 2); //getCell(row, column) zero-indexed
+			if (PlayerNameCell.value !== playerName) {
+				PlayerNameCell.value = playerName;
+			}
+
+			const PlayerIDCell = sheet.getCell(3 + i, 3); //getCell(row, column) zero-indexed
+			if (PlayerIDCell.value !== playerID) {
+				PlayerIDCell.value = playerID;
+			}
 
 
-		const RankCell = sheet.getCell(3 + i, 4); //getCell(row, column) zero-indexed
-		if (RankCell.value !== playerRank) {
-			RankCell.value = playerRank;
-		}
+			const RankCell = sheet.getCell(3 + i, 4); //getCell(row, column) zero-indexed
+			if (RankCell.value !== playerRank) {
+				RankCell.value = playerRank;
+			}
 
-		const DiscordTagCell = sheet.getCell(3 + i, 5); //getCell(row, column) zero-indexed
-		if (DiscordTagCell.value !== discordTag) {
-			DiscordTagCell.value = discordTag;
-		}
+			const DiscordTagCell = sheet.getCell(3 + i, 5); //getCell(row, column) zero-indexed
+			if (DiscordTagCell.value !== discordTag) {
+				DiscordTagCell.value = discordTag;
+			}
 
-		const TimeCell = sheet.getCell(3 + i, 6); //getCell(row, column) zero-indexed
-		if (TimeCell.value !== time) {
-			TimeCell.value = time;
-		}
+			const TimeCell = sheet.getCell(3 + i, 6); //getCell(row, column) zero-indexed
+			if (TimeCell.value !== time) {
+				TimeCell.value = time;
+			}
 
-		const ConnectedCell = sheet.getCell(3 + i, 8); //getCell(row, column) zero-indexed
-		if (ConnectedCell.value !== connected) {
-			ConnectedCell.value = connected;
-		}
+			const ConnectedCell = sheet.getCell(3 + i, 8); //getCell(row, column) zero-indexed
+			if (ConnectedCell.value !== connected) {
+				ConnectedCell.value = connected;
+			}
 
-		const BadgesCell = sheet.getCell(3 + i, 9); //getCell(row, column) zero-indexed
-		if (BadgesCell.value !== badges) {
-			BadgesCell.value = badges;
+			const BadgesCell = sheet.getCell(3 + i, 9); //getCell(row, column) zero-indexed
+			if (BadgesCell.value !== badges) {
+				BadgesCell.value = badges;
+			}
+			if (i % 150 === 0 || i === 495) {
+				await sheet.saveUpdatedCells();
+			}
 		}
-		if (i % 150 === 0 || i === 495) {
-			await sheet.saveUpdatedCells();
+	} catch (error) {
+		if (error.message === 'Google API error - [503] The service is currently unavailable.') {
+			console.log(error.message, 'New task is being created');
+			await processQueueEntry.destroy();
+			const task = await DBProcessQueue.findOne({
+				where: { guildId: 'None', task: 'elitiriCupSignUps', additions: bracketName }
+			});
+
+			if (!task) {
+				let date = new Date();
+				date.setUTCMinutes(date.getUTCMinutes() + 1);
+				DBProcessQueue.create({ guildId: 'None', task: 'elitiriCupSignUps', priority: 3, additions: bracketName, date: date });
+			}
+		} else {
+			console.log(error);
 		}
 	}
 
