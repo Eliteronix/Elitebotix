dcd8a621f4589cc0aff2fe2b821b12a1dfab946e
Eliteronix <zimmermann.mariomarvin@gmail.com>
2021-09-29 23:34:49 +0200

    Added a QA version of saveMultiMatches

--- gotMessage.js
+++ gotMessage.js
@@ -9,7 +9,7 @@ const { DBElitiriCupSignUp, DBTickets } = require('./dbObjects');
 
 module.exports = async function (msg, bancho) {
 	//check if the message wasn't sent by the bot itself or another bot
-	if (!(msg.author.bot)) {
+	if (!(msg.author.bot) || msg.channel.id === '892873577479692358') {
 		//Create a collection for the commands
 		msg.client.commands = new Discord.Collection();
 
--- processQueueTasks/saveMultiMatches.js
+++ processQueueTasks/saveMultiMatches.js
@@ -9,7 +9,16 @@ module.exports = {
 		let args = processQueueEntry.additions.split(';');
 
 		// eslint-disable-next-line no-undef
-		const osuApi = new osu.Api(process.env.OSUTOKENV1, {
+		let APItoken = process.env.OSUTOKENV1;
+
+		// eslint-disable-next-line no-undef
+		if (process.env.SERVER === 'QA') {
+			// eslint-disable-next-line no-undef
+			APItoken = process.env.OSUTOKENV1BACKUP;
+		}
+
+		// eslint-disable-next-line no-undef
+		const osuApi = new osu.Api(APItoken, {
 			// baseUrl: sets the base api url (default: https://osu.ppy.sh/api)
 			notFoundAsError: true, // Throw an error on not found instead of returning nothing. (default: true)
 			completeScores: false, // When fetching scores also fetch the beatmap they are for (Allows getting accuracy) (default: false)
@@ -19,7 +28,19 @@ module.exports = {
 		let matchID = args[0];
 
 		// eslint-disable-next-line no-undef
-		if (args[0] === '-1' || process.env.SERVER !== 'Live') {
+		if (process.env.SERVER === 'Live' && matchID === '50000000') {
+			matchID = '60000000';
+		}
+
+		// eslint-disable-next-line no-undef
+		if (process.env.SERVER === 'QA' && matchID === '60000000') {
+			// eslint-disable-next-line no-undef
+			console.log(`Manually deleted task for saving Multi Matches for ${matchID} ${process.env.SERVER}`);
+			return processQueueEntry.destroy();
+		}
+
+		// eslint-disable-next-line no-undef
+		if (args[0] === '-1' || process.env.SERVER === 'Dev') {
 			console.log(`Manually deleted task for saving Multi Matches for ${matchID}`);
 			return processQueueEntry.destroy();
 		}
@@ -35,7 +56,13 @@ module.exports = {
 						let minutesBehindToday = parseInt((now.getTime() - Date.parse(match.raw_start)) / 1000 / 60) % 60;
 						let hoursBehindToday = parseInt((now.getTime() - Date.parse(match.raw_start)) / 1000 / 60 / 60) % 24;
 						let daysBehindToday = parseInt((now.getTime() - Date.parse(match.raw_start)) / 1000 / 60 / 60 / 24);
-						const channel = await client.channels.fetch('891314445559676928');
+						let channel;
+						// eslint-disable-next-line no-undef
+						if (process.env.SERVER === 'Live') {
+							channel = await client.channels.fetch('891314445559676928');
+						} else {
+							channel = await client.channels.fetch('892873577479692358');
+						}
 						channel.send(`<https://osu.ppy.sh/mp/${matchID}> ${daysBehindToday}d ${hoursBehindToday}h ${minutesBehindToday}m ${match.name} done`);
 					} else {
 						// console.log(`${matchID} ${match.name} is not a tourney match`);
