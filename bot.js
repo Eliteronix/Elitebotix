2fd10bd3ff5693764f7f63751796fbca3a70faad
Marvin Zimmermann <64707347+Eliteronix@users.noreply.github.com>
2021-10-15 10:33:20 +0200

    Fixed activity not showing

--- bot.js
+++ bot.js
@@ -128,10 +128,10 @@ function readyDiscord() {
 	if (process.env.SERVER === 'Live') {
 		client.user.setPresence({
 			status: 'online',  //You can show online, idle....
-			activity: {
+			activities: [{
 				name: 'with e!help',  //The message shown
 				type: 'PLAYING' //PLAYING: WATCHING: LISTENING: STREAMING:
-			}
+			}]
 		});
 	}
 
