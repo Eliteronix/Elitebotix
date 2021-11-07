c6ef9351579cd3a2ad939e87d1ebdb18b08c8546
Eliteronix <zimmermann.mariomarvin@gmail.com>
2021-11-08 00:20:08 +0100

    Added osu-motd slash command and added custom motd option

--- commands/osu-motd.js
+++ commands/osu-motd.js
@@ -1,5 +1,5 @@
 const { DBDiscordUsers } = require('../dbObjects');
-const { getGuildPrefix } = require('../utils');
+const { getGuildPrefix, populateMsgFromInteraction } = require('../utils');
 
 module.exports = {
 	name: 'osu-motd',
@@ -16,9 +16,48 @@ module.exports = {
 	//noCooldownMessage: true,
 	tags: 'osu',
 	prefixCommand: true,
-	async execute(msg, args) {
+	async execute(msg, args, interaction) {
+		let years = 0;
+		let months = 0;
+		let weeks = 0;
+		let days = 0;
+		let hours = 0;
+		let minutes = 0;
+
+		if (interaction) {
+			msg = await populateMsgFromInteraction(interaction);
+
+			if (interaction.options._subcommand === 'custom-fixed-players') {
+				args = ['custom'];
+			} else {
+				args = [interaction.options._subcommand];
+			}
+
+			for (let i = 0; i < interaction.options._hoistedOptions.length; i++) {
+				if (interaction.options._hoistedOptions[i].name === 'years') {
+					years = interaction.options._hoistedOptions[i].value;
+				} else if (interaction.options._hoistedOptions[i].name === 'months') {
+					months = interaction.options._hoistedOptions[i].value;
+				} else if (interaction.options._hoistedOptions[i].name === 'weeks') {
+					weeks = interaction.options._hoistedOptions[i].value;
+				} else if (interaction.options._hoistedOptions[i].name === 'days') {
+					days = interaction.options._hoistedOptions[i].value;
+				} else if (interaction.options._hoistedOptions[i].name === 'hours') {
+					hours = interaction.options._hoistedOptions[i].value;
+				} else if (interaction.options._hoistedOptions[i].name === 'minutes') {
+					minutes = interaction.options._hoistedOptions[i].value;
+				} else if (interaction.options._hoistedOptions[i].name === 'message') {
+					args = [interaction.options._hoistedOptions[i].value];
+				}
+			}
+		}
+
+
 		if (args[0].toLowerCase() === 'server') {
-			sendMessage(msg, 'The discord server for the competition can be found here: <https://discord.com/invite/Asz5Gfe>\nAfter joining be sure to head to <#801000891750547496> and assign yourself the MOTD role!\nEverything else will be done automatically when you registered!');
+			if (msg.id) {
+				return sendMessage(msg, 'The discord server for the competition can be found here: <https://discord.com/invite/Asz5Gfe>\nAfter joining be sure to head to <#801000891750547496> and assign yourself the MOTD role!\nEverything else will be done automatically when you registered!');
+			}
+			return interaction.reply({ content: 'The discord server for the competition can be found here: <https://discord.com/invite/Asz5Gfe>\nAfter joining be sure to head to <#801000891750547496> and assign yourself the MOTD role!\nEverything else will be done automatically when you registered!', ephemeral: true });
 		} else if (args[0].toLowerCase() === 'register') {
 			const guildPrefix = await getGuildPrefix(msg);
 
@@ -29,7 +68,10 @@ module.exports = {
 
 			if (discordUser && discordUser.osuUserId) {
 				if (discordUser.osuMOTDRegistered) {
-					return sendMessage(msg, `You are already registered for the \`Maps of the Day\` competition.\nBe sure to join the server if you didn't already. (\`${guildPrefix}osu-motd server\`)\nOther than that be sure to have DMs open for me so that I can send you updates for the competition!`);
+					if (msg.id) {
+						return sendMessage(msg, `You are already registered for the \`Maps of the Day\` competition.\nBe sure to join the server if you didn't already. (\`${guildPrefix}osu-motd server\`)\nOther than that be sure to have DMs open for me so that I can send you updates for the competition!`);
+					}
+					return interaction.reply({ content: 'You are already registered for the `Maps of the Day` competition.\nBe sure to join the server if you didn\'t already. (`/osu-motd server`)\nOther than that be sure to have DMs open for me so that I can send you updates for the competition!', ephemeral: true });
 				}
 				if (discordUser.osuVerified) {
 					discordUser.osuMOTDRegistered = true;
@@ -38,12 +80,21 @@ module.exports = {
 					discordUser.osuMOTDerrorFirstOccurence = null;
 					discordUser.osuMOTDmutedUntil = null;
 					discordUser.save();
-					sendMessage(msg, `You successfully registered for the \`Maps of the Day\` competition.\nBe sure to join the server and read <#834833321438740490> if you didn't already. (\`${guildPrefix}osu-motd server\`)\nOther than that be sure to have DMs open for me so that I can send you updates for the competition!`);
+					if (msg.id) {
+						return sendMessage(msg, `You successfully registered for the \`Maps of the Day\` competition.\nBe sure to join the server and read <#834833321438740490> if you didn't already. (\`${guildPrefix}osu-motd server\`)\nOther than that be sure to have DMs open for me so that I can send you updates for the competition!`);
+					}
+					return interaction.reply({ content: 'You successfully registered for the `Maps of the Day` competition.\nBe sure to join the server and read <#834833321438740490> if you didn\'t already. (`/osu-motd server`)\nOther than that be sure to have DMs open for me so that I can send you updates for the competition!', ephemeral: true });
 				} else {
-					sendMessage(msg, `It seems like you don't have your connected osu! account verified.\nPlease use \`${guildPrefix}osu-link verify\` to send a verification code to your osu! dms, follow the instructions and try again afterwards.`);
+					if (msg.id) {
+						return sendMessage(msg, `It seems like you don't have your connected osu! account verified.\nPlease use \`${guildPrefix}osu-link verify\` to send a verification code to your osu! dms, follow the instructions and try again afterwards.`);
+					}
+					return interaction.reply({ content: 'It seems like you don\'t have your connected osu! account verified.\nPlease use `$/osu-link verify` to send a verification code to your osu! dms, follow the instructions and try again afterwards.', ephemeral: true });
 				}
 			} else {
-				sendMessage(msg, `It seems like you don't have your osu! account connected to the bot.\nPlease use \`${guildPrefix}osu-link osu-username\` to connect you account and verify it.`);
+				if (msg.id) {
+					return sendMessage(msg, `It seems like you don't have your osu! account connected to the bot.\nPlease use \`${guildPrefix}osu-link osu-username\` to connect you account and verify it.`);
+				}
+				return interaction.reply({ content: 'It seems like you don\'t have your osu! account connected to the bot.\nPlease use `/osu-link osu-username` to connect you account and verify it.', ephemeral: true });
 			}
 		} else if (args[0].toLowerCase() === 'unregister') {
 			//get discordUser from db
@@ -54,9 +105,15 @@ module.exports = {
 			if (discordUser && discordUser.osuMOTDRegistered) {
 				discordUser.osuMOTDRegistered = false;
 				discordUser.save();
-				sendMessage(msg, 'You have been unregistered from the `Maps of the Day` competition.\nStill thank you for showing interest!\nYou can always register again by using `e!osu-motd register`!');
+				if (msg.id) {
+					return sendMessage(msg, 'You have been unregistered from the `Maps of the Day` competition.\nStill thank you for showing interest!\nYou can always register again by using `e!osu-motd register`!');
+				}
+				return interaction.reply({ content: 'You have been unregistered from the `Maps of the Day` competition.\nStill thank you for showing interest!\nYou can always register again by using `/osu-motd register`!', ephemeral: true });
 			} else {
-				sendMessage(msg, 'You aren\'t signed up for the `Maps of the Day` competition at the moment.\nYou can always register by using `e!osu-motd register`!');
+				if (msg.id) {
+					return sendMessage(msg, 'You aren\'t signed up for the `Maps of the Day` competition at the moment.\nYou can always register by using `e!osu-motd register`!');
+				}
+				return interaction.reply({ content: 'You aren\'t signed up for the `Maps of the Day` competition at the moment.\nYou can always register by using `/osu-motd register`!', ephemeral: true });
 			}
 		} else if (args[0].toLowerCase() === 'mute') {
 			//get discordUser from db
@@ -67,13 +124,6 @@ module.exports = {
 			if (discordUser && discordUser.osuMOTDRegistered) {
 				if (discordUser.osuMOTDMuted) {
 					if (args[1]) {
-						let years = 0;
-						let months = 0;
-						let weeks = 0;
-						let days = 0;
-						let hours = 0;
-						let minutes = 0;
-
 						for (let i = 0; i < args.length; i++) {
 							let splice = true;
 							if (args[i].endsWith('y') && !isNaN(args[i].replace('y', ''))) {
@@ -110,7 +160,10 @@ module.exports = {
 							discordUser.save();
 						}
 					}
-					sendMessage(msg, 'The `Maps of the Day` competition has already been muted for you - the time has been update if a period was specified.\nTo receive messages and pings again use `e!osu-motd unmute`.');
+					if (msg.id) {
+						return sendMessage(msg, 'The `Maps of the Day` competition has already been muted for you - the time has been update if a period was specified.\nTo receive messages and pings again use `e!osu-motd unmute`.');
+					}
+					return interaction.reply({ content: 'The `Maps of the Day` competition has already been muted for you - the time has been update if a period was specified.\nTo receive messages and pings again use `/osu-motd unmute`.', ephemeral: true });
 				} else {
 					if (args[1]) {
 						let years = 0;
@@ -161,10 +214,16 @@ module.exports = {
 
 					discordUser.osuMOTDMuted = true;
 					discordUser.save();
-					sendMessage(msg, 'The `Maps of the Day` competition has been muted for you. You will not receive messages and pings anymore but will still appear on the leaderboard.\nTo receive messages and pings again use `e!osu-motd unmute`.');
+					if (msg.id) {
+						return sendMessage(msg, 'The `Maps of the Day` competition has been muted for you. You will not receive messages and pings anymore but will still appear on the leaderboard.\nTo receive messages and pings again use `e!osu-motd unmute`.');
+					}
+					return interaction.reply({ content: 'The `Maps of the Day` competition has been muted for you. You will not receive messages and pings anymore but will still appear on the leaderboard.\nTo receive messages and pings again use `/osu-motd unmute`.', ephemeral: true });
 				}
 			} else {
-				sendMessage(msg, 'You aren\'t signed up for the `Maps of the Day` competition at the moment.\nYou can always register by using `e!osu-motd register`!');
+				if (msg.id) {
+					return sendMessage(msg, 'You aren\'t signed up for the `Maps of the Day` competition at the moment.\nYou can always register by using `e!osu-motd register`!');
+				}
+				return interaction.reply({ content: 'You aren\'t signed up for the `Maps of the Day` competition at the moment.\nYou can always register by using `/osu-motd register`!', ephemeral: true });
 			}
 		} else if (args[0].toLowerCase() === 'unmute') {
 			//get discordUser from db
@@ -177,15 +236,104 @@ module.exports = {
 					discordUser.osuMOTDMuted = false;
 					discordUser.osuMOTDmutedUntil = null;
 					discordUser.save();
-					sendMessage(msg, 'The `Maps of the Day` competition has been unmuted for you. You will start receiving messages again.');
+					if (msg.id) {
+						return sendMessage(msg, 'The `Maps of the Day` competition has been unmuted for you. You will start receiving messages again.');
+					}
+					return interaction.reply({ content: 'The `Maps of the Day` competition has been unmuted for you. You will start receiving messages again.', ephemeral: true });
 				} else {
-					sendMessage(msg, 'The `Maps of the Day` competition was not muted for you.');
+					if (msg.id) {
+						return sendMessage(msg, 'The `Maps of the Day` competition was not muted for you.');
+					}
+					return interaction.reply({ content: 'The `Maps of the Day` competition was not muted for you.', ephemeral: true });
 				}
 			} else {
-				sendMessage(msg, 'You aren\'t signed up for the `Maps of the Day` competition at the moment.\nYou can always register by using `e!osu-motd register`!');
+				if (msg.id) {
+					return sendMessage(msg, 'You aren\'t signed up for the `Maps of the Day` competition at the moment.\nYou can always register by using `e!osu-motd register`!');
+				}
+				return interaction.reply({ content: 'You aren\'t signed up for the `Maps of the Day` competition at the moment.\nYou can always register by using `/osu-motd register`!', ephemeral: true });
+			}
+		} else if (args[0].toLowerCase() === 'custom') {
+			//Return if its not triggered by a slash command
+			if (msg.id) {
+				return msg.reply('Please use `/osu-motd custom` to set up the custom MOTD');
+			}
+			args.shift();
+			//Decide between provided player list or not
+			if (args[0]) { //Player list provided
+				if (!args[1]) {
+					//Send a message that not enough players were provided (min 2)
+					return interaction.reply('You need to provide at least 2 players to start a custom MOTD!');
+				}
+
+				//Defer the interaction
+				await interaction.deferReply();
+
+				// eslint-disable-next-line no-undef
+				const osuApi = new osu.Api(process.env.OSUTOKENV1, {
+					// baseUrl: sets the base api url (default: https://osu.ppy.sh/api)
+					notFoundAsError: true, // Throw an error on not found instead of returning nothing. (default: true)
+					completeScores: false, // When fetching scores also fetch the beatmap they are for (Allows getting accuracy) (default: false)
+					parseNumeric: false // Parse numeric values into numbers/floats, excluding ids
+				});
+
+				//Get all players from the osu! api and store them in the players array
+				let players = [];
+				let playersNotFound = [];
+				for (let i = 0; i < args.length; i++) {
+					let player = await osuApi.getUser({ u: args[i] });
+					if (player) {
+						players.push(player);
+					} else {
+						playersNotFound.push(args[i]);
+					}
+				}
+
+				//Return if at least one player wasn't found
+				if (playersNotFound.length > 0) {
+					return interaction.editReply(`The following players were not found: ${playersNotFound.join(', ')}\nThe custom MOTD has been aborted.`);
+				}
+
+				//Get the related discordUser from the db for each player
+				let discordUsers = [];
+				let playersNotConnected = [];
+
+				for (let i = 0; i < players.length; i++) {
+					let discordUser = await DBDiscordUsers.findOne({
+						where: { userId: players[i].id },
+					});
+
+					if (discordUser) {
+						discordUsers.push(discordUser);
+					} else {
+						playersNotConnected.push(players[i].username);
+					}
+				}
+
+				//Return if at least one player isn't connected to the bot
+				if (playersNotConnected.length > 0) {
+					return interaction.editReply(`The following players are not connected to the bot: ${playersNotConnected.join(', ')}\nThe custom MOTD has been aborted.`);
+				}
+
+				//Get the related user on discord for each discorduser
+				let users = [];
+				let unreachableUsers = [];
+
+				for (let i = 0; i < discordUsers.length; i++) {
+					let user = await interaction.guild.members.fetch(discordUsers[i].userId);
+					if (user) {
+						users.push(user);
+					} else {
+						unreachableUsers.push(discordUsers[i].userId);
+					}
+				}
+
+				//Return if at least one user isn't reachable
+				if (unreachableUsers.length > 0) {
+					return interaction.editReply(`The following users are not reachable: ${unreachableUsers.join(', ')}\nThe custom MOTD has been aborted.`);
+				}
 			}
 		} else {
-			msg.reply('Please specify what you want to do: `server`, `register`, `unregister`, `mute`, `unmute`');
+			msg.reply('Please specify what you want to do: `server`, `register`, `unregister`, `mute`, `unmute`, `custom`');
 		}
 	},
 };
