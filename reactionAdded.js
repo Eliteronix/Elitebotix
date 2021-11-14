57d1c9a326237390fd062151aaf1048c04effe46
Eliteronix <zimmermann.mariomarvin@gmail.com>
2021-11-14 15:55:59 +0100

    Added reaction to osu-profile and added shortcut logic

--- reactionAdded.js
+++ reactionAdded.js
@@ -294,6 +294,32 @@ module.exports = async function (reaction, user, additionalObjects) {
 		}
 	}
 
+	//Check if reacted for map information
+	if (reaction._emoji.name === '📈') {
+		//Check if it is actually a scorepost
+		if (reaction.message.attachments.first().name.startsWith('osu-profile')) {
+			//get the osuUserId used
+			const osuUserId = reaction.message.attachments.first().name.replace(/.+-/gm, '').replace('.png', '');
+
+			//Setup artificial arguments
+			let args = [osuUserId];
+
+			const command = require('./commands/osu-skills.js');
+
+			//Set author of the message to the reacting user to not break the commands
+			reaction.message.author = user;
+
+			try {
+				command.execute(reaction.message, args, null, additionalObjects);
+			} catch (error) {
+				console.error(error);
+				const eliteronixUser = await reaction.message.client.users.cache.find(user => user.id === '138273136285057025');
+				reaction.message.reply('There was an error trying to execute that command. The developers have been alerted.');
+				eliteronixUser.send(`There was an error trying to execute a command.\nReaction by ${user.username}#${user.discriminator}: \`Compare Reaction\`\n\n${error}`);
+			}
+		}
+	}
+
 	if (reaction.message.channel.type === 'DM') {
 		return;
 	}
