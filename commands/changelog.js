001da4a28b0362d9c9a82eaae3fe75ab14d91506
Eliteronix <zimmermann.mariomarvin@gmail.com>
2021-11-17 00:53:54 +0100

    Updated wording

--- commands/changelog.js
+++ commands/changelog.js
@@ -70,7 +70,7 @@ module.exports = {
 				// eslint-disable-next-line no-undef
 			} else if (process.env.SERVER === 'Live') {
 				const changelogChannel = await msg.client.channels.fetch('804658828883787784');
-				changelogChannel.send({ content: '**Elitebotix has been updated** - Please report any bugs by using `e!feedback bug <Description>`.', files: [attachment] });
+				changelogChannel.send({ content: '**Elitebotix has been updated** - Please report any bugs by using `e!feedback bug <Description>` / `/feedback`.', files: [attachment] });
 			}
 		}
 	},
