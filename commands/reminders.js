5fcedc9f23808bab068294fa3920360dad5b1177
Eliteronix <zimmermann.mariomarvin@gmail.com>
2021-11-17 21:57:57 +0100

    Fixed your issue agreeGeBusiness

--- commands/reminders.js
+++ commands/reminders.js
@@ -1,6 +1,5 @@
 const { Permissions } = require('discord.js');
 const { DBProcessQueue } = require('../dbObjects');
-const { populateMsgFromInteraction } = require('../utils');
 
 module.exports = {
 	name: 'reminders',
@@ -18,11 +17,7 @@ module.exports = {
 	tags: 'general',
 	prefixCommand: true,
 	// eslint-disable-next-line no-unused-vars
-	async execute(msg, interaction) {
-		if (interaction) {
-			msg = await populateMsgFromInteraction(interaction);
-		}
-
+	async execute(msg, args, interaction) {
 		const reminders = await DBProcessQueue.findAll({
 			where: { task: 'remind' }
 		});
@@ -35,36 +30,34 @@ module.exports = {
 		if (reminders.length == 0) {
 			if (msg) {
 				return msg.reply('No reminders set for you');
-			} else
-				return interaction.reply({ content: 'No reminders set for you', ephemeral: true });
-		} else {
-			for (let i = 0; i < reminders.length; i++) {
-				let args = reminders[i].additions.split(';');
-				date = reminders[i].date;
-
-				setReminders.push(args[1]);
-				reminderTime.push(date.toLocaleTimeString('en-UK', {
-					day: 'numeric', // numeric, 2-digit
-					year: 'numeric', // numeric, 2-digit
-					month: '2-digit', // numeric, 2-digit, long, short, narrow
-					hour: 'numeric', // numeric, 2-digit
-					minute: 'numeric', // numeric, 2-digit
-					second: 'numeric', // numeric, 2-digit
-				}));
 			}
-
-			for (let i = 0; i < setReminders.length; i++) {
-				message += `"${setReminders[i]}"  -  will be send on ${reminderTime[i]}\n`;
-			}
-
-			if (msg) {
-				if (msg.channel.type !== 'DM') {
-					msg.reply(' Check your DMs');
-					msg.author.send(message);
-				}
-			} else {
-				await interaction.reply({ content: message, ephemeral: true });
-			}
+			return interaction.reply({ content: 'No reminders set for you', ephemeral: true });
 		}
+
+		for (let i = 0; i < reminders.length; i++) {
+			let args = reminders[i].additions.split(';');
+			date = reminders[i].date;
+
+			setReminders.push(args[1]);
+			reminderTime.push(date.toLocaleTimeString('en-UK', {
+				day: 'numeric', // numeric, 2-digit
+				year: 'numeric', // numeric, 2-digit
+				month: '2-digit', // numeric, 2-digit, long, short, narrow
+				hour: 'numeric', // numeric, 2-digit
+				minute: 'numeric', // numeric, 2-digit
+				second: 'numeric', // numeric, 2-digit
+			}));
+		}
+
+		for (let i = 0; i < setReminders.length; i++) {
+			message += `"${setReminders[i]}"  -  will be send on ${reminderTime[i]}\n`;
+		}
+
+		if (msg && msg.channel.type !== 'DM') {
+			msg.reply(' Check your DMs');
+			return msg.author.send(message);
+		}
+		return interaction.reply({ content: message, ephemeral: true });
+
 	}
 };
