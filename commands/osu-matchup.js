2954b0453a80053c0a2435e511a79138af8da104
Eliteronix <zimmermann.mariomarvin@gmail.com>
2021-11-26 00:19:22 +0100

    Added osu-matchup command

---
+++ commands/osu-matchup.js
@@ -0,0 +1,173 @@
+const Discord = require('discord.js');
+const osu = require('node-osu');
+const { DBDiscordUsers, DBOsuMultiScores } = require('../dbObjects');
+const { getGuildPrefix, getOsuUserServerMode, getIDFromPotentialOsuLink, getMessageUserDisplayname, populateMsgFromInteraction, logDatabaseQueries } = require('../utils');
+const { Permissions } = require('discord.js');
+
+module.exports = {
+	name: 'osu-matchup',
+	// aliases: ['os', 'o-s'],
+	description: 'Sends an info card about the matchups between the specified players',
+	usage: '<username> [username]',
+	//permissions: 'MANAGE_GUILD',
+	//permissionsTranslated: 'Manage Server',
+	botPermissions: [Permissions.FLAGS.SEND_MESSAGES, Permissions.FLAGS.ATTACH_FILES],
+	botPermissionsTranslated: 'Send Messages and Attach Files',
+	//guildOnly: true,
+	args: true,
+	cooldown: 5,
+	//noCooldownMessage: true,
+	tags: 'osu',
+	prefixCommand: true,
+	async execute(msg, args, interaction) {
+		if (interaction) {
+			msg = await populateMsgFromInteraction(interaction);
+
+			await interaction.reply('Matchup is being processed');
+
+			args = [];
+
+			if (interaction.options._hoistedOptions) {
+				for (let i = 0; i < interaction.options._hoistedOptions.length; i++) {
+					args.push(interaction.options._hoistedOptions[i].value);
+				}
+			}
+		}
+		const guildPrefix = await getGuildPrefix(msg);
+
+		const commandConfig = await getOsuUserServerMode(msg, args);
+		const commandUser = commandConfig[0];
+		let users = [];
+
+		if (!args[1]) {//Get profile by author if no argument
+			if (commandUser && commandUser.osuUserId) {
+				users.push(commandUser.osuUserId);
+			} else {
+				const userDisplayName = await getMessageUserDisplayname(msg);
+				users.push(userDisplayName);
+			}
+		}
+
+		//Get profiles by arguments
+		for (let i = 0; i < args.length; i++) {
+			if (args[i].startsWith('<@') && args[i].endsWith('>')) {
+				logDatabaseQueries(4, 'commands/osu-matchup.js DBDiscordUsers');
+				const discordUser = await DBDiscordUsers.findOne({
+					where: { userId: args[i].replace('<@', '').replace('>', '').replace('!', '') },
+				});
+
+				if (discordUser && discordUser.osuUserId) {
+					users.push(discordUser.osuUserId);
+				} else {
+					msg.channel.send(`\`${args[i].replace(/`/g, '')}\` doesn't have their osu! account connected.\nPlease use their username or wait until they connected their account by using \`${guildPrefix}osu-link <username>\`.`);
+					users.push(args[i]);
+				}
+			} else {
+				users.push(getIDFromPotentialOsuLink(args[i]));
+			}
+		}
+
+		// eslint-disable-next-line no-undef
+		const osuApi = new osu.Api(process.env.OSUTOKENV1, {
+			// baseUrl: sets the base api url (default: https://osu.ppy.sh/api)
+			notFoundAsError: true, // Throw an error on not found instead of returning nothing. (default: true)
+			completeScores: false, // When fetching scores also fetch the beatmap they are for (Allows getting accuracy) (default: false)
+			parseNumeric: false // Parse numeric values into numbers/floats, excluding ids
+		});
+
+		let usersReadable = [];
+		for (let i = 0; i < users.length; i++) {
+			await osuApi.getUser({ u: users[i] })
+				.then(user => {
+					users[i] = user.id;
+					usersReadable.push(user.name);
+				})
+				.catch(err => {
+					if (err.message === 'Not found') {
+						msg.channel.send(`Could not find user \`${users[i].replace(/`/g, '')}\`. (Use "_" instead of spaces)`);
+						users.splice(i, 1);
+						i--;
+					} else {
+						console.log(err);
+					}
+				});
+		}
+
+		if (users.length < 2) {
+			return msg.channel.send('Not enough users left for the matchup.');
+		}
+
+		console.log(usersReadable, users);
+
+		//Add all multiscores from both players to an array
+		let scores = [];
+		scores.push(await DBOsuMultiScores.findAll({
+			where: { osuUserId: users[0] }
+		}));
+		scores.push(await DBOsuMultiScores.findAll({
+			where: { osuUserId: users[1] }
+		}));
+
+		//Create arrays of standings for each player/Mod/Score
+		//[ScoreV1[User1Wins, User2Wins], ScoreV2[User1Wins, User2Wins]]
+		let directNoModsWins = [[0, 0], [0, 0]];
+		let directHiddenWins = [[0, 0], [0, 0]];
+		let directHardRockWins = [[0, 0], [0, 0]];
+		let directDoubleTimeWins = [[0, 0], [0, 0]];
+		let directFreeModWins = [[0, 0], [0, 0]];
+
+		let indirectNoModsWins = [[0, 0], [0, 0]];
+		let indirectHiddenWins = [[0, 0], [0, 0]];
+		let indirectHardRockWins = [[0, 0], [0, 0]];
+		let indirectDoubleTimeWins = [[0, 0], [0, 0]];
+		let indirectFreeModWins = [[0, 0], [0, 0]];
+
+		let matchesPlayed = [];
+		for (let i = 0; i < scores[0].length; i++) {
+			for (let j = 0; j < scores[1].length; j++) {
+				//Find a score from the same game
+				if (scores[0][i].gameId === scores[1][j].gameId) {
+					//Push matches for the history txt
+					if (!matchesPlayed.includes(`${(scores[0][i].matchStartDate.getUTCMonth() + 1).toString().padStart(2, '0')}-${scores[0][i].matchStartDate.getUTCFullYear()} - ${scores[0][i].matchName} ----- https://osu.ppy.sh/community/matches/${scores[0][i].matchId}`)) {
+						matchesPlayed.push(`${(scores[0][i].matchStartDate.getUTCMonth() + 1).toString().padStart(2, '0')}-${scores[0][i].matchStartDate.getUTCFullYear()} - ${scores[0][i].matchName} ----- https://osu.ppy.sh/community/matches/${scores[0][i].matchId}`);
+					}
+
+					//Evaluate if it was played with Score v2 or not (0 = v1, 1 = v2)
+					let scoreVersion = 0;
+					if (scores[0][i].scoringType === 'Score v2') {
+						scoreVersion = 1;
+					}
+
+					//Evaluate which score is better (0 = user1; 1 = user2)
+					let winner = 0;
+					if (parseInt(scores[0][i].score) < parseInt(scores[1][j].score)) {
+						winner = 1;
+					}
+
+					//Evaluate with which mods the game was played
+					if (!scores[0][i].freeMod && scores[0][i].rawMods === '0' && (scores[0][i].gameRawMods === '0' || scores[0][i].gameRawMods === '1')) {
+						directNoModsWins[scoreVersion][winner]++;
+					} else if (scores[0][i].rawMods === '0' && (scores[0][i].gameRawMods === '8' || scores[0][i].gameRawMods === '9')) {
+						directHiddenWins[scoreVersion][winner]++;
+					} else if (scores[0][i].rawMods === '0' && (scores[0][i].gameRawMods === '16' || scores[0][i].gameRawMods === '17')) {
+						directHardRockWins[scoreVersion][winner]++;
+					} else if (scores[0][i].rawMods === '0' && (scores[0][i].gameRawMods === '64' || scores[0][i].gameRawMods === '65' || scores[0][i].gameRawMods === '576' || scores[0][i].gameRawMods === '577')) {
+						directDoubleTimeWins[scoreVersion][winner]++;
+					} else {
+						directFreeModWins[scoreVersion][winner]++;
+					}
+				}
+			}
+		}
+
+		console.log(directNoModsWins, directHiddenWins, directHardRockWins, directDoubleTimeWins, directFreeModWins);
+
+		// eslint-disable-next-line no-undef
+		matchesPlayed = new Discord.MessageAttachment(Buffer.from(matchesPlayed.join('\n'), 'utf-8'), `multi-matches-${users[0]}-vs-${users[1]}.txt`);
+
+		if (msg.id) {
+			return msg.reply({ content: `Matchup analysis for \`${usersReadable[0]}\` vs \`${usersReadable[1]}\``, files: [matchesPlayed] });
+		}
+		return interaction.followUp({ content: `Matchup analysis for \`${usersReadable[0]}\` vs \`${usersReadable[1]}\``, files: [matchesPlayed] });
+	},
+};
