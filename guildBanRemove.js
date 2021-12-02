51eaf3643b7c629d649c1dd9dae33470dc77d448
Eliteronix <zimmermann.mariomarvin@gmail.com>
2021-12-02 01:16:11 +0100

    Fixed guildBanAdd and guildBanRemove not working

--- guildBanRemove.js
+++ guildBanRemove.js
@@ -2,39 +2,39 @@ const Discord = require('discord.js');
 const { DBGuilds } = require('./dbObjects');
 const { isWrongSystem, logDatabaseQueries } = require('./utils');
 
-module.exports = async function (discordGuild, user) {
-	if (isWrongSystem(discordGuild.id, false)) {
+module.exports = async function (guildBan) {
+	if (isWrongSystem(guildBan.guild.id, false)) {
 		return;
 	}
 
 	logDatabaseQueries(2, 'guildBanRemove.js DBGuilds');
 	//Get the guild dataset from the db
 	const guild = await DBGuilds.findOne({
-		where: { guildId: discordGuild.id, loggingBanRemove: true },
+		where: { guildId: guildBan.guild.id, loggingBanRemove: true },
 	});
 
 	//check if a guild was found in the db
 	if (guild && guild.loggingChannel) {
 		let channel;
 		try {
-			channel = await discordGuild.client.channels.fetch(guild.loggingChannel);
+			channel = await guildBan.guild.client.channels.fetch(guild.loggingChannel);
 		} catch (error) {
 			if (error.message === 'Unknown Channel') {
 				guild.loggingChannel = null;
 				guild.save();
-				const owner = await discordGuild.client.users.fetch(discordGuild.ownerId);
-				return owner.send(`It seems like the logging channel on the guild \`${discordGuild.name}\` has been deleted.\nThe logging has been deactivated.`);
+				const owner = await guildBan.guild.client.users.fetch(guildBan.guild.ownerId);
+				return owner.send(`It seems like the logging channel on the guild \`${guildBan.guild.name}\` has been deleted.\nThe logging has been deactivated.`);
 			}
 			console.log(error);
 		}
 
 		const changeEmbed = new Discord.MessageEmbed()
 			.setColor('#0099ff')
-			.setAuthor(`${user.username}#${user.discriminator}`, user.displayAvatarURL())
-			.setDescription(`<@${user.id}> was unbanned from the server!`)
-			.setThumbnail(user.displayAvatarURL())
+			.setAuthor(`${guildBan.user.username}#${guildBan.user.discriminator}`, guildBan.user.displayAvatarURL())
+			.setDescription(`<@${guildBan.user.id}> was unbanned from the server!`)
+			.setThumbnail(guildBan.user.displayAvatarURL())
 			.addFields(
-				{ name: 'Unbanned from the server', value: `<@${user.id}>` },
+				{ name: 'Unbanned from the server', value: `<@${guildBan.user.id}>` },
 			)
 			.setTimestamp()
 			.setFooter('Eventname: banremove');
