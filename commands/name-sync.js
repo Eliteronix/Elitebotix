ca89ac0bd7ee2049affa273caf5cd37a1bebe6f9
Marvin Zimmermann <64707347+Eliteronix@users.noreply.github.com>
2021-12-07 09:45:47 +0100

    Finished osu! name sync to discord

--- commands/name-sync.js
+++ commands/name-sync.js
@@ -9,8 +9,8 @@ module.exports = {
 	usage: '<disable/osuname/osunameandrank>',
 	permissions: 'MANAGE_GUILD',
 	permissionsTranslated: 'Manage Server',
-	botPermissions: Permissions.FLAGS.SEND_MESSAGES,
-	botPermissionsTranslated: 'Send Messages',
+	botPermissions: [Permissions.FLAGS.SEND_MESSAGES, Permissions.FLAGS.MANAGE_NICKNAMES],
+	botPermissionsTranslated: 'Send Messages and Manage Nicknames',
 	//guildOnly: true,
 	args: true,
 	cooldown: 10,
@@ -33,14 +33,14 @@ module.exports = {
 			if (task) {
 				task.destroy();
 				if (msg.id) {
-					return msg.reply('Name Sync Disabled');
+					return msg.reply('Name Sync Disabled.');
 				}
-				return interaction.reply('Name Sync Disabled');
+				return interaction.reply({ content: 'Name Sync Disabled.', ephemeral: true });
 			} else {
 				if (msg.id) {
-					return msg.reply('Name Sync is already disabled');
+					return msg.reply('Name Sync is already disabled.');
 				}
-				return interaction.reply('Name Sync is already disabled');
+				return interaction.reply({ content: 'Name Sync is already disabled.', ephemeral: true });
 			}
 		} else if (args[0].toLowerCase() === 'osuname') {
 			logDatabaseQueries(4, 'commands/name-sync.js DBProcessQueue osuname');
@@ -50,16 +50,19 @@ module.exports = {
 
 			if (task && task.additions === 'osuname') {
 				if (msg.id) {
-					return msg.reply('Name Sync is already set to osu! name.');
+					return msg.reply('Name Sync is already set to osu! name.\nBe sure to have the bot role above all other roles or else the bot won\'t be able to edit the nicknames.');
 				}
-				return interaction.reply('Name Sync is already set to osu! name.');
+				return interaction.reply({ content: 'Name Sync is already set to osu! name.\nBe sure to have the bot role above all other roles or else the bot won\'t be able to edit the nicknames.', ephemeral: true });
 			} else if (task) {
+				let date = new Date();
+				task.date = date;
 				task.additions = 'osuname';
+				task.beingExecuted = false;
 				task.save();
 				if (msg.id) {
-					return msg.reply('Name Sync has been changed to osu! name.');
+					return msg.reply('Name Sync has been changed to osu! name.\nBe sure to have the bot role above all other roles or else the bot won\'t be able to edit the nicknames.');
 				}
-				return interaction.reply('Name Sync has been changed to osu! name.');
+				return interaction.reply({ content: 'Name Sync has been changed to osu! name.\nBe sure to have the bot role above all other roles or else the bot won\'t be able to edit the nicknames.', ephemeral: true });
 			}
 
 			let date = new Date();
@@ -68,12 +71,13 @@ module.exports = {
 				task: 'nameSync',
 				additions: 'osuname',
 				date: date,
+				priority: 5
 			});
 
 			if (msg.id) {
-				return msg.reply('Name Sync is now set to osu! name.');
+				return msg.reply('Name Sync is now set to osu! name.\nBe sure to have the bot role above all other roles or else the bot won\'t be able to edit the nicknames.');
 			}
-			return interaction.reply('Name Sync is now set to osu! name.');
+			return interaction.reply({ content: 'Name Sync is now set to osu! name.\nBe sure to have the bot role above all other roles or else the bot won\'t be able to edit the nicknames.', ephemeral: true });
 		} else if (args[0].toLowerCase() === 'osunameandrank') {
 			logDatabaseQueries(4, 'commands/name-sync.js DBProcessQueue osunameandrank');
 			let task = await DBProcessQueue.findOne({
@@ -82,16 +86,19 @@ module.exports = {
 
 			if (task && task.additions === 'osunameandrank') {
 				if (msg.id) {
-					return msg.reply('Name Sync is already set to osu! name and rank.');
+					return msg.reply('Name Sync is already set to osu! name and rank.\nBe sure to have the bot role above all other roles or else the bot won\'t be able to edit the nicknames.');
 				}
-				return interaction.reply('Name Sync is already set to osu! name and rank.');
+				return interaction.reply({ content: 'Name Sync is already set to osu! name and rank.\nBe sure to have the bot role above all other roles or else the bot won\'t be able to edit the nicknames.', ephemeral: true });
 			} else if (task) {
+				let date = new Date();
+				task.date = date;
 				task.additions = 'osunameandrank';
+				task.beingExecuted = false;
 				task.save();
 				if (msg.id) {
-					return msg.reply('Name Sync has been changed to osu! name and rank.');
+					return msg.reply('Name Sync has been changed to osu! name and rank.\nBe sure to have the bot role above all other roles or else the bot won\'t be able to edit the nicknames.');
 				}
-				return interaction.reply('Name Sync has been changed to osu! name and rank.');
+				return interaction.reply({ content: 'Name Sync has been changed to osu! name and rank.\nBe sure to have the bot role above all other roles or else the bot won\'t be able to edit the nicknames.', ephemeral: true });
 			}
 
 			let date = new Date();
@@ -100,18 +107,19 @@ module.exports = {
 				task: 'nameSync',
 				additions: 'osunameandrank',
 				date: date,
+				priority: 5
 			});
 
 			if (msg.id) {
-				return msg.reply('Name Sync is now set to osu! name and rank.');
+				return msg.reply('Name Sync is now set to osu! name and rank.\nBe sure to have the bot role above all other roles or else the bot won\'t be able to edit the nicknames.');
 			}
-			return interaction.reply('Name Sync is now set to osu! name and rank.');
+			return interaction.reply({ content: 'Name Sync is now set to osu! name and rank.\nBe sure to have the bot role above all other roles or else the bot won\'t be able to edit the nicknames.', ephemeral: true });
 		} else {
 			let guildPrefix = await getGuildPrefix(msg);
 			if (msg.id) {
 				return msg.reply(`Please specify what the names should be synced to.\nCorrect usage: ${guildPrefix}${this.name} ${this.usage}`);
 			}
-			return interaction.reply(`Please specify what the names should be synced to.\nCorrect usage: ${guildPrefix}${this.name} ${this.usage}`);
+			return interaction.reply({ content: `Please specify what the names should be synced to.\nCorrect usage: ${guildPrefix}${this.name} ${this.usage}`, ephemeral: true });
 		}
 	},
 };
---
+++ processQueueTasks/nameSync.js
@@ -0,0 +1,79 @@
+const { DBDiscordUsers } = require('../dbObjects');
+const { logDatabaseQueries } = require('../utils');
+
+module.exports = {
+	async execute(client, bancho, processQueueEntry) {
+		let guild;
+		try {
+			guild = await client.guilds.fetch(processQueueEntry.guildId);
+		} catch (e) {
+			if (e.message === 'Missing Access') {
+				processQueueEntry.destroy();
+				return;
+			} else {
+				return console.log('processQueueTasks/nameSync.js fetch guild', e);
+			}
+		}
+
+		//Fetch all members
+		await guild.members.fetch()
+			.then(async (guildMembers) => {
+				const members = [];
+				guildMembers.filter(member => member.user.bot !== true).each(member => members.push(member));
+				for (let i = 0; i < members.length; i++) {
+					//Get the user
+					logDatabaseQueries(2, 'processQueueTasks/nameSync.js DBDiscordUsers');
+					let discordUser = await DBDiscordUsers.findOne({
+						where: {
+							userId: members[i].user.id,
+						}
+					});
+
+					if (members[i].user.id !== guild.ownerId && discordUser && discordUser.osuUserId && discordUser.osuVerified) {
+						try {
+							//Get the users displayname
+							let userDisplayName = members[i].user.username;
+
+							if (members[i].nickname) {
+								userDisplayName = members[i].nickname;
+							}
+
+							//Set what the user's nickname should be
+							let nickname = '';
+							if (processQueueEntry.additions === 'osuname') {
+								nickname = discordUser.osuName;
+							} else if (processQueueEntry.additions === 'osunameandrank') {
+								let rank = discordUser.osuRank;
+								if (rank.length > 4) {
+									rank = `${rank.substring(0, rank.length - 3)}k`;
+								}
+
+								nickname = `${discordUser.osuName} [${rank}]`;
+							}
+
+							//Set nickname if needed
+							if (userDisplayName !== nickname) {
+								await members[i].setNickname(nickname);
+							}
+						} catch (e) {
+							if (!e.message === 'Missing Permissions') {
+								let date = new Date();
+								date.setMinutes(date.getMinutes() + 10);
+								processQueueEntry.date = date;
+								processQueueEntry.beingExecuted = false;
+								processQueueEntry.save();
+								return console.log('processQueueTasks/nameSync.js setNickname', e);
+							}
+						}
+					}
+				}
+			});
+
+		//Set the date and prepare for new execution
+		let date = new Date();
+		date.setMinutes(date.getMinutes() + 10);
+		processQueueEntry.date = date;
+		processQueueEntry.beingExecuted = false;
+		processQueueEntry.save();
+	},
+};
