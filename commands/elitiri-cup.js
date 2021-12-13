fdff4e6e45ec432343fa2dd63f318c9111fa0879
Eliteronix <zimmermann.mariomarvin@gmail.com>
2021-12-13 19:47:45 +0100

    Added responses to slash command

--- commands/elitiri-cup.js
+++ commands/elitiri-cup.js
@@ -57,7 +57,7 @@ module.exports = {
 			}
 		}
 		if (args[0].toLowerCase() === 'server') {
-			sendMessage(msg, 'The discord server for the competition can be found here: <https://discord.com/invite/Asz5Gfe>\nAfter joining be sure to head to <#727987472772104272> and assign yourself the Elitiri Cup role!\nEverything else will be done automatically when you registered!');
+			sendMessage(msg, interaction, 'The discord server for the competition can be found here: <https://discord.com/invite/Asz5Gfe>\nAfter joining be sure to head to <#727987472772104272> and assign yourself the Elitiri Cup role!\nEverything else will be done automatically when you registered!');
 		} else if (args[0].toLowerCase() === 'register') {
 			let now = new Date();
 			let endOfRegs = new Date();
@@ -69,7 +69,10 @@ module.exports = {
 			endOfRegs.setUTCMonth(5); //Zero Indexed
 			endOfRegs.setUTCFullYear(2021);
 			if (now > endOfRegs) {
-				return msg.reply('The registration period has ended.');
+				if (msg.id) {
+					return msg.reply('The registration period has ended.');
+				}
+				return interaction.reply({ content: 'The registration period has ended.', ephemeral: true });
 			}
 
 			const guildPrefix = await getGuildPrefix(msg);
@@ -80,7 +83,7 @@ module.exports = {
 			});
 
 			if (elitiriSignUp) {
-				return sendMessage(msg, `You are already registered for the \`Elitiri Cup Summer 2021\` tournament.\nBe sure to join the server if you didn't already. (\`${guildPrefix}${this.name} server\`)\nOther than that be sure to have DMs open for me so that I can send you updates for the tournament!\n\nAlso please **be sure to set your availabilities** by using \`${guildPrefix}${this.name} availability\``);
+				return sendMessage(msg, interaction, `You are already registered for the \`Elitiri Cup Summer 2021\` tournament.\nBe sure to join the server if you didn't already. (\`${guildPrefix}${this.name} server\`)\nOther than that be sure to have DMs open for me so that I can send you updates for the tournament!\n\nAlso please **be sure to set your availabilities** by using \`${guildPrefix}${this.name} availability\``);
 			}
 
 			//get discordUser from db
@@ -105,19 +108,19 @@ module.exports = {
 					}
 
 					if (bracketName === '') {
-						return sendMessage(msg, `Your BWS rank is #${BWSRank} and you are not able to join any of the brackets because of that.`);
+						return sendMessage(msg, interaction, `Your BWS rank is #${BWSRank} and you are not able to join any of the brackets because of that.`);
 					}
 
 					if (!args[2]) {
-						return sendMessage(msg, `You didn't provide valid difficulty boundaries for your sign up. Please provide a number between 0 and 10 for your desired difficulties of the mappools.\nUsage: \`${guildPrefix}${this.name} register <lower SR between 0-10> <upper SR between 0-10>\``);
+						return sendMessage(msg, interaction, `You didn't provide valid difficulty boundaries for your sign up. Please provide a number between 0 and 10 for your desired difficulties of the mappools.\nUsage: \`${guildPrefix}${this.name} register <lower SR between 0-10> <upper SR between 0-10>\``);
 					}
 
 					if (isNaN(args[1]) || args[1] < 0 || args[1] > 10) {
-						return sendMessage(msg, `${args[1]} is not a valid number. Please provide a number between 0 and 10 for your desired lower difficulty of the mappools.\nUsage: \`${guildPrefix}${this.name} register <lower SR between 0-10> <upper SR between 0-10>\``);
+						return sendMessage(msg, interaction, `${args[1]} is not a valid number. Please provide a number between 0 and 10 for your desired lower difficulty of the mappools.\nUsage: \`${guildPrefix}${this.name} register <lower SR between 0-10> <upper SR between 0-10>\``);
 					}
 
 					if (isNaN(args[2]) || args[2] < 0 || args[2] > 10) {
-						return sendMessage(msg, `${args[2]} is not a valid number. Please provide a number between 0 and 10 for your desired upper difficulty of the mappools.\nUsage: \`${guildPrefix}${this.name} register <lower SR between 0-10> <upper SR between 0-10>\``);
+						return sendMessage(msg, interaction, `${args[2]} is not a valid number. Please provide a number between 0 and 10 for your desired upper difficulty of the mappools.\nUsage: \`${guildPrefix}${this.name} register <lower SR between 0-10> <upper SR between 0-10>\``);
 					}
 
 					await DBElitiriCupSignUp.create({
@@ -137,13 +140,13 @@ module.exports = {
 						sundayLateAvailability: null,
 						tournamentName: 'Elitiri Cup Summer 2021'
 					});
-					sendMessage(msg, `You successfully registered for the \`Elitiri Cup Summer 2021\` tournament.\nBe sure to join the server and read <#727987472772104272> if you didn't already. (\`${guildPrefix}${this.name} server\`)\nOther than that be sure to have DMs open for me so that I can send you updates for the tournament!\n\nAlso please **be sure to set your availabilities** by using \`${guildPrefix}${this.name} availability\``);
+					sendMessage(msg, interaction, `You successfully registered for the \`Elitiri Cup Summer 2021\` tournament.\nBe sure to join the server and read <#727987472772104272> if you didn't already. (\`${guildPrefix}${this.name} server\`)\nOther than that be sure to have DMs open for me so that I can send you updates for the tournament!\n\nAlso please **be sure to set your availabilities** by using \`${guildPrefix}${this.name} availability\``);
 					createProcessQueueTask(bracketName);
 				} else {
-					sendMessage(msg, `It seems like you don't have your connected osu! account verified.\nPlease use \`${guildPrefix}osu-link verify\` to send a verification code to your osu! dms, follow the instructions and try again afterwards.`);
+					sendMessage(msg, interaction, `It seems like you don't have your connected osu! account verified.\nPlease use \`${guildPrefix}osu-link verify\` to send a verification code to your osu! dms, follow the instructions and try again afterwards.`);
 				}
 			} else {
-				sendMessage(msg, `It seems like you don't have your osu! account connected to the bot.\nPlease use \`${guildPrefix}osu-link osu-username\` to connect you account and verify it.`);
+				sendMessage(msg, interaction, `It seems like you don't have your osu! account connected to the bot.\nPlease use \`${guildPrefix}osu-link osu-username\` to connect you account and verify it.`);
 			}
 		} else if (args[0].toLowerCase() === 'unregister') {
 			let now = new Date();
@@ -168,10 +171,10 @@ module.exports = {
 
 			if (elitiriSignUp) {
 				elitiriSignUp.destroy();
-				sendMessage(msg, `You have been unregistered from the \`Elitiri Cup Summer 2021\` tournament.\nStill thank you for showing interest!\nYou can register again by using \`${guildPrefix}${this.name} register\`!`);
+				sendMessage(msg, interaction, `You have been unregistered from the \`Elitiri Cup Summer 2021\` tournament.\nStill thank you for showing interest!\nYou can register again by using \`${guildPrefix}${this.name} register\`!`);
 				createProcessQueueTask(elitiriSignUp.bracketName);
 			} else {
-				sendMessage(msg, `You aren't signed up for the \`Elitiri Cup Summer 2021\` tournament at the moment.\nYou can register by using \`${guildPrefix}${this.name} register\`!`);
+				sendMessage(msg, interaction, `You aren't signed up for the \`Elitiri Cup Summer 2021\` tournament at the moment.\nYou can register by using \`${guildPrefix}${this.name} register\`!`);
 			}
 		} else if (args[0].toLowerCase() === 'availability') {
 			const guildPrefix = await getGuildPrefix(msg);
@@ -184,12 +187,12 @@ module.exports = {
 			if (elitiriSignUp) {
 				if (!args[1]) {
 					if (elitiriSignUp.saturdayEarlyAvailability === null) {
-						return sendMessage(msg, `You currently don't have any availabilities set.\nUsage: \`${guildPrefix}${this.name} availability xx-xx xx-xx\`\nExample: \`${guildPrefix}${this.name} availability 14-21 16-20\``);
+						return sendMessage(msg, interaction, `You currently don't have any availabilities set.\nUsage: \`${guildPrefix}${this.name} availability xx-xx xx-xx\`\nExample: \`${guildPrefix}${this.name} availability 14-21 16-20\``);
 					}
-					return sendMessage(msg, `Your current \`Elitiri Cup Summer 2021\` availabilities are:\nSaturday: ${elitiriSignUp.saturdayEarlyAvailability} - ${elitiriSignUp.saturdayLateAvailability} UTC\nSunday: ${elitiriSignUp.sundayEarlyAvailability} - ${elitiriSignUp.sundayLateAvailability} UTC\nUsage: \`${guildPrefix}${this.name} availability xx-xx xx-xx\`\nExample: \`${guildPrefix}${this.name} availability 14-21 16-20\``);
+					return sendMessage(msg, interaction, `Your current \`Elitiri Cup Summer 2021\` availabilities are:\nSaturday: ${elitiriSignUp.saturdayEarlyAvailability} - ${elitiriSignUp.saturdayLateAvailability} UTC\nSunday: ${elitiriSignUp.sundayEarlyAvailability} - ${elitiriSignUp.sundayLateAvailability} UTC\nUsage: \`${guildPrefix}${this.name} availability xx-xx xx-xx\`\nExample: \`${guildPrefix}${this.name} availability 14-21 16-20\``);
 				}
 				if (!args[2]) {
-					return sendMessage(msg, `You used the wrong format for submitting your availability in UTC.\nUsage: \`${guildPrefix}${this.name} availability xx-xx xx-xx\`\nExample: \`${guildPrefix}${this.name} availability 14-21 16-20\``);
+					return sendMessage(msg, interaction, `You used the wrong format for submitting your availability in UTC.\nUsage: \`${guildPrefix}${this.name} availability xx-xx xx-xx\`\nExample: \`${guildPrefix}${this.name} availability 14-21 16-20\``);
 				}
 				let saturdayAvailability = args[1].split('-');
 				let sundayAvailability = args[2].split('-');
@@ -198,17 +201,17 @@ module.exports = {
 					|| isNaN(saturdayAvailability[1]) || saturdayAvailability[1] < 0 || saturdayAvailability[1] > 24
 					|| isNaN(sundayAvailability[0]) || sundayAvailability[0] < 0 || sundayAvailability[0] > 24
 					|| isNaN(sundayAvailability[1]) || sundayAvailability[1] < 0 || sundayAvailability[1] > 24) {
-					return sendMessage(msg, `You used the wrong format for submitting your availability in UTC.\nUsage: \`${guildPrefix}${this.name} availability xx-xx xx-xx\`\nExample: \`${guildPrefix}${this.name} availability 14-21 16-20\``);
+					return sendMessage(msg, interaction, `You used the wrong format for submitting your availability in UTC.\nUsage: \`${guildPrefix}${this.name} availability xx-xx xx-xx\`\nExample: \`${guildPrefix}${this.name} availability 14-21 16-20\``);
 				}
 				elitiriSignUp.saturdayEarlyAvailability = parseInt(saturdayAvailability[0]);
 				elitiriSignUp.saturdayLateAvailability = parseInt(saturdayAvailability[1]);
 				elitiriSignUp.sundayEarlyAvailability = parseInt(sundayAvailability[0]);
 				elitiriSignUp.sundayLateAvailability = parseInt(sundayAvailability[1]);
 				await elitiriSignUp.save();
-				sendMessage(msg, `Your \`Elitiri Cup Summer 2021\` availabilities have been updated.\nYour new availabilities are:\nSaturday: ${elitiriSignUp.saturdayEarlyAvailability} - ${elitiriSignUp.saturdayLateAvailability} UTC\nSunday: ${elitiriSignUp.sundayEarlyAvailability} - ${elitiriSignUp.sundayLateAvailability} UTC`);
+				sendMessage(msg, interaction, `Your \`Elitiri Cup Summer 2021\` availabilities have been updated.\nYour new availabilities are:\nSaturday: ${elitiriSignUp.saturdayEarlyAvailability} - ${elitiriSignUp.saturdayLateAvailability} UTC\nSunday: ${elitiriSignUp.sundayEarlyAvailability} - ${elitiriSignUp.sundayLateAvailability} UTC`);
 				createProcessQueueTask(elitiriSignUp.bracketName);
 			} else {
-				sendMessage(msg, `You are not yet registered for the \`Elitiri Cup Summer 2021\` tournament.\nYou can register by using \`${guildPrefix}${this.name} register\`!`);
+				sendMessage(msg, interaction, `You are not yet registered for the \`Elitiri Cup Summer 2021\` tournament.\nYou can register by using \`${guildPrefix}${this.name} register\`!`);
 			}
 		} else {
 			msg.reply('Please specify what you want to do: `server`, `register`, `unregister`, `availability`');
@@ -216,15 +219,18 @@ module.exports = {
 	},
 };
 
-function sendMessage(msg, content) {
-	msg.author.send(content)
-		.then(() => {
-			if (msg.channel.type === 'DM') return;
-			msg.reply('I\'ve sent you a DM with some info!');
-		})
-		.catch(() => {
-			msg.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
-		});
+function sendMessage(msg, interaction, content) {
+	if (msg.id) {
+		return msg.author.send(content)
+			.then(() => {
+				if (msg.channel.type === 'DM') return;
+				msg.reply('I\'ve sent you a DM with some info!');
+			})
+			.catch(() => {
+				msg.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+			});
+	}
+	return interaction.reply({ content: content, ephemeral: true });
 }
 
 async function createProcessQueueTask(bracketName) {
