be64c3b091b60c1153d97bc3b1c86fc5e505271a
Eliteronix <zimmermann.mariomarvin@gmail.com>
2021-12-21 16:12:24 +0100

    Updated mod reactions on osu-beatmap

--- commands/osu-beatmap.js
+++ commands/osu-beatmap.js
@@ -100,13 +100,16 @@ async function getBeatmap(msg, beatmap) {
 	const sentMessage = await msg.channel.send({ content: `Website: <https://osu.ppy.sh/b/${beatmap.beatmapId}>\nosu! direct: <osu://b/${beatmap.beatmapId}>`, files: [attachment] });
 	processingMessage.delete();
 	sentMessage.react('<:COMPARE:827974793365159997>');
+	sentMessage.react('<:EZ:918920760586805259>');
+	sentMessage.react('<:HT:918921193426411544>');
+	sentMessage.react('<:HD:918922015182827531>');
+	if (beatmap.mode === 'Mania') {
+		sentMessage.react('<:FI:918922047994880010>');
+	}
+	sentMessage.react('<:DT:918920670023397396>');
+	sentMessage.react('<:HR:918938816377671740>');
+	sentMessage.react('<:FL:918920836755382343>');
 	if (beatmap.mode === 'Standard') {
-		sentMessage.react('<:EZ:918920760586805259>');
-		sentMessage.react('<:HT:918921193426411544>');
-		sentMessage.react('<:HD:918922015182827531>');
-		sentMessage.react('<:DT:918920670023397396>');
-		sentMessage.react('<:HR:918938816377671740>');
-		sentMessage.react('<:FL:918920836755382343>');
 		sentMessage.react('<:HDHR:918935327215861760>');
 		sentMessage.react('<:HDDT:918935350125142036>');
 	}
