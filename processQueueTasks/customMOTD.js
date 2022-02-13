5014bdb299ec34183c37a8896d94df4bafbe0624
Eliteronix <zimmermann.mariomarvin@gmail.com>
2022-02-13 19:33:34 +0100

    Finished duel option

--- processQueueTasks/customMOTD.js
+++ processQueueTasks/customMOTD.js
@@ -4,6 +4,10 @@ module.exports = {
 	async execute(client, bancho, processQueueEntry) {
 		//used to block new osu-referee matches to be scheduled while customMOTDs are blocking it
 		await pause(parseInt(processQueueEntry.additions) * 1000);
-		processQueueEntry.destroy();
+		try {
+			processQueueEntry.destroy();
+		} catch (error) {
+			//Nothing
+		}
 	},
 };
