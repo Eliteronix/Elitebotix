14069893c6a8248b0abc650893afa7ba01e871c8
Roddyyyy <38702439+Roddyyyy@users.noreply.github.com>
2022-02-20 16:54:40 +0200

    fuckery

--- commands/elitiri-claim-lobby.js
+++ commands/elitiri-claim-lobby.js
@@ -5,15 +5,12 @@ const { currentElitiriCup } = require('../config.json');
 
 // To-Do (it is 5:27 right now i havent slept yet my code sucks ass as always thanks)
 // what to do if lobby is full
-// user is not registered
 // lobbyId regex
-// lobby change (i actually have no clue how we can search for the osuName, as far as i know you cant find values of the range of cells with sheets api)
-// process queue fuckery :hypers:
 
 module.exports = {
     name: 'elitiri-claim-lobby',
     //aliases: ['developer'],
-    description: `Claim your qualifiers lobby for ${currentElitiriCup}`,
+    description: `Choose your qualifier lobby for ${currentElitiriCup}`,
     usage: '<LobbyID>',
     //permissions: 'KICK_MEMBERS',
     //permissionsTranslated: 'Manage Server',
@@ -27,6 +24,12 @@ module.exports = {
     prefixCommand: true,
     // eslint-disable-next-line no-unused-vars
     async execute(msg, args, interaction, client) {
+        // UNCOMMENT THIS LATER
+        // eslint-disable-next-line no-undef
+        // if (process.env.SERVER !== 'Live') {
+        //     return;
+        // }
+
         // Elite you need to use your regex power and get rid of the possible lobby abbreviation (AQ-1 => 1)
         let lobbyId = Number(args[0]);
 
@@ -37,48 +40,42 @@ module.exports = {
             } else {
                 return interaction.reply({ content: 'Please make sure your lobby ID is correct' });
             }
-        } else {
-            await updateSheet(lobbyId);
-            msg.reply('Done.');
         }
-    }
-};
-
-async function updateSheet(lobbyId) {
-    // UNCOMMENT THIS LATER
-
-    // eslint-disable-next-line no-undef
-    // if (process.env.SERVER !== 'Live') {
-    //     return;
-    // }
-
-    try {
         // STUFF NEEDS TESTING
+        let player = await DBElitiriCupSignUp.findAll({
+            where: {
+                tournamentName: currentElitiriCup,
+                userId: msg.author.id,
+            }
+        });
 
-        // let bracketPlayers = await DBElitiriCupSignUp.findAll({
-        //     where: {
-        //         tournamentName: currentElitiriCup,
-        //         bracketName: bracketName,
-        //         osuName: osuName
-        //     }
-        // });
-
-
-        // players bracket check
-        if (bracketName == 'Top Bracket') {
-            scheduleSheetId = 'Qualifiers Schedules-Top';
-        } else if (bracketName == 'Middle Bracket') {
-            scheduleSheetId = 'Qualifiers Schedules-Middle';
-        } else if (bracketName == 'Lower Bracket') {
-            scheduleSheetId = 'Qualifiers Schedules-Lower';
-        } else if (bracketName == 'Beginner Bracket') {
-            scheduleSheetId = 'Qualifiers Schedules-Beginner';
+        if (!player) {
+            if (msg && msg.id) {
+                return msg.reply(`Seems like you're not registered for ${currentElitiriCup}`);
+            } else {
+                return interaction.reply({ content: `Seems like you're not registered for ${currentElitiriCup}` });
+            }
         }
 
+        let scheduleSheetId;
+        let lobbyAbbreviation;
+        // players bracket check
+        if (player[0].bracketName == 'Top Bracket') {
+            scheduleSheetId = 'Qualifiers Schedules-Top';
+            lobbyAbbreviation = 'DQ-';
+        } else if (player[0].bracketName == 'Middle Bracket') {
+            scheduleSheetId = 'Qualifiers Schedules-Middle';
+            lobbyAbbreviation = 'CQ-';
+        } else if (player[0].bracketName == 'Lower Bracket') {
+            scheduleSheetId = 'Qualifiers Schedules-Lower';
+            lobbyAbbreviation = 'BQ-';
+        } else if (player[0].bracketName == 'Beginner Bracket') {
+            scheduleSheetId = 'Qualifiers Schedules-Beginner';
+            lobbyAbbreviation = 'AQ-';
+        }
 
         // Initialize the sheet - doc ID is the long id in the sheets URL
         const doc = new GoogleSpreadsheet('1FPr133dAROYGUpJOaQPGTvGq5hjk8B-Ik82rmZsa9NM');
-
         // Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
         await doc.useServiceAccountAuth({
             // eslint-disable-next-line no-undef
@@ -90,27 +87,25 @@ async function updateSheet(lobbyId) {
         const sheet = doc.sheetsByTitle[scheduleSheetId];
         await sheet.loadCells('A1:U29');
 
+
         //we need to skip 17th row
         if (lobbyId > 12) {
             lobbyId++;
         }
 
-        for (let i = 0; i < 15; i++) {
-            // let playerName = 'Roddy';
-            if (bracketPlayers[i]) {
-                playerName = bracketPlayers[i].osuName;
-            }
-
-            // i couldnt make loadCells function work properly so had to load cells from A1
-            const PlayerNameCell = sheet.getCell(3 + lobbyId, 6 + i); //getCell(row, column) zero-indexed
-            if (PlayerNameCell.value !== playerName) {
-                PlayerNameCell.value = playerName;
+        let PlayerNameCell = sheet.getCell(3 + lobbyId, 6);
+        for (let i = 0; PlayerNameCell !== ''; i++) {
+            PlayerNameCell = sheet.getCell(3 + lobbyId, 6 + i); //getCell(row, column) zero-indexed
+            if (PlayerNameCell.value !== player[0].osuName) {
+                PlayerNameCell.value = player[0].osuName;
             }
         }
         await sheet.saveUpdatedCells();
-    } catch (error) {
-        if (error.message === 'Google API error - [503] The service is currently unavailable.'
-            || error.message === 'Request failed with status code 502'
-            || error.message === 'Google API error - [500] Internal error encountered.') { console.log(error); }
+
+        if (msg && msg.id) {
+            return msg.reply(`You have successfully claimed lobby ${lobbyAbbreviation}${args[0]}`);
+        } else {
+            return interaction.reply({ content: `You have successfully claimed lobby ${lobbyAbbreviation}${args[0]}` });
+        }
     }
-}
+};
