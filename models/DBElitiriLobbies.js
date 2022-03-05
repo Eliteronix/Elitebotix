9379b749c6b10b3126bc8df223d4f932b09f37fb
Roddyyyy <38702439+Roddyyyy@users.noreply.github.com>
2022-03-06 00:16:35 +0300

    camelcase fix
    Co-authored-by: Marvin Zimmermann <64707347+Eliteronix@users.noreply.github.com>

--- models/DBElitiriLobbies.js
+++ models/DBElitiriLobbies.js
@@ -17,7 +17,7 @@ module.exports = (sequelize, DataTypes) => {
 		bracketName: {
 			type: DataTypes.STRING,
 		},
-		refdiscordTag: {
+		refDiscordTag: {
 			type: DataTypes.STRING,
 		},
 		refOsuUserId: {
