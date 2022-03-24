0434367bbc0cd3f690fd19e91a638b31293426d6
Roddyyyy <38702439+Roddyyyy@users.noreply.github.com>
2022-03-24 02:38:34 +0200

    Added sorting

--- commands/osu-top.js
+++ commands/osu-top.js
@@ -54,13 +54,37 @@ module.exports = {
 		const server = commandConfig[1];
 		const mode = commandConfig[2];
 
-		let recentScores = false;
 		let limit = 10;
 		let tracking = false;
+		let sorting = null;
 
 		for (let i = 0; i < args.length; i++) {
 			if (args[i] === '--new' || args[i] === '--recent' || args[i] === '--n') {
-				recentScores = true;
+				sorting = 'recent';
+				args.splice(i, 1);
+				i--;
+			} else if (args[i] === '--ar' || args[i] === '--approach' || args[i] === '--approachrate') {
+				sorting = 'ar';
+				args.splice(i, 1);
+				i--;
+			} else if (args[i] === '--cs' || args[i] === '--size' || args[i] === '--circlesize') {
+				sorting = 'cs';
+				args.splice(i, 1);
+				i--;
+			} else if (args[i] === '--od' || args[i] === '--diff' || args[i] === '--overalldifficulty') {
+				sorting = 'od';
+				args.splice(i, 1);
+				i--;
+			} else if (args[i] === '--hp' || args[i] === '--hpdrain' || args[i] === '--drain') {
+				sorting = 'drain';
+				args.splice(i, 1);
+				i--;
+			} else if (args[i] === '--length') {
+				sorting = 'length';
+				args.splice(i, 1);
+				i--;
+			} else if (args[i] === '--bpm' || args[i] === '--beatsperminute') { // who the fuck would use beats per fucking minute smh
+				sorting = 'bpm';
 				args.splice(i, 1);
 				i--;
 			} else if (args[i].startsWith('--') && !isNaN(args[i].replace('--', ''))) {
@@ -82,10 +106,10 @@ module.exports = {
 		if (!args[0]) {
 			//Get profile by author if no argument
 			if (commandUser && commandUser.osuUserId) {
-				getTopPlays(msg, commandUser.osuUserId, server, mode, false, recentScores, limit, tracking);
+				getTopPlays(msg, commandUser.osuUserId, server, mode, false, sorting, limit, tracking);
 			} else {
 				const userDisplayName = await getMessageUserDisplayname(msg);
-				getTopPlays(msg, userDisplayName, server, mode, false, recentScores, limit, tracking);
+				getTopPlays(msg, userDisplayName, server, mode, false, sorting, limit, tracking);
 			}
 		} else {
 			//Get profiles by arguments
@@ -97,21 +121,21 @@ module.exports = {
 					});
 
 					if (discordUser && discordUser.osuUserId) {
-						getTopPlays(msg, discordUser.osuUserId, server, mode, false, recentScores, limit, tracking);
+						getTopPlays(msg, discordUser.osuUserId, server, mode, false, sorting, limit, tracking);
 					} else {
 						msg.channel.send(`\`${args[i].replace(/`/g, '')}\` doesn't have their osu! account connected.\nPlease use their username or wait until they connected their account by using \`${guildPrefix}osu-link <username>\`.`);
-						getTopPlays(msg, args[i], server, mode, false, recentScores, limit, tracking);
+						getTopPlays(msg, args[i], server, mode, false, sorting, limit, tracking);
 					}
 				} else {
 
 					if (args.length === 1 && !(args[0].startsWith('<@')) && !(args[0].endsWith('>'))) {
 						if (!(commandUser) || commandUser && !(commandUser.osuUserId)) {
-							getTopPlays(msg, getIDFromPotentialOsuLink(args[i]), server, mode, true, recentScores, limit, tracking);
+							getTopPlays(msg, getIDFromPotentialOsuLink(args[i]), server, mode, true, sorting, limit, tracking);
 						} else {
-							getTopPlays(msg, getIDFromPotentialOsuLink(args[i]), server, mode, false, recentScores, limit, tracking);
+							getTopPlays(msg, getIDFromPotentialOsuLink(args[i]), server, mode, false, sorting, limit, tracking);
 						}
 					} else {
-						getTopPlays(msg, getIDFromPotentialOsuLink(args[i]), server, mode, false, recentScores, limit, tracking);
+						getTopPlays(msg, getIDFromPotentialOsuLink(args[i]), server, mode, false, sorting, limit, tracking);
 					}
 				}
 			}
@@ -119,7 +143,7 @@ module.exports = {
 	}
 };
 
-async function getTopPlays(msg, username, server, mode, noLinkedAccount, recentScores, limit, tracking) {
+async function getTopPlays(msg, username, server, mode, noLinkedAccount, sorting, limit, tracking) {
 	if (server === 'bancho') {
 		// eslint-disable-next-line no-undef
 		const osuApi = new osu.Api(process.env.OSUTOKENV1, {
@@ -154,9 +178,9 @@ async function getTopPlays(msg, username, server, mode, noLinkedAccount, recentS
 
 				let elements = [canvas, ctx, user];
 
-				elements = await drawTitle(elements, server, mode, recentScores);
+				elements = await drawTitle(elements, server, mode, sorting);
 
-				elements = await drawTopPlays(elements, server, mode, msg, recentScores, limit);
+				elements = await drawTopPlays(elements, server, mode, msg, sorting, limit);
 
 				await drawFooter(elements);
 
@@ -216,9 +240,9 @@ async function getTopPlays(msg, username, server, mode, noLinkedAccount, recentS
 
 				let elements = [canvas, ctx, user];
 
-				elements = await drawTitle(elements, server, mode, recentScores);
+				elements = await drawTitle(elements, server, mode, sorting);
 
-				elements = await drawTopPlays(elements, server, mode, msg, recentScores);
+				elements = await drawTopPlays(elements, server, mode, msg, sorting);
 
 				await drawFooter(elements);
 
@@ -239,7 +263,7 @@ async function getTopPlays(msg, username, server, mode, noLinkedAccount, recentS
 	}
 }
 
-async function drawTitle(input, server, mode, recentScores) {
+async function drawTitle(input, server, mode, sorting) {
 	let canvas = input[0];
 	let ctx = input[1];
 	let user = input[2];
@@ -250,17 +274,40 @@ async function drawTitle(input, server, mode, recentScores) {
 		serverDisplay = `[${server}] `;
 	}
 
-	let recent = '';
+	let sortingText;
 
-	if (recentScores) {
-		recent = 'most recent ';
+	if (sorting !== null) {
+		sortingText = 'sorted by ';
+		if (sorting == 'recent') {
+			sortingText += 'date';
+		} else if (sorting == 'ar') {
+			sortingText += 'AR';
+		} else if (sorting == 'cs') {
+			sortingText += 'CS';
+		} else if (sorting == 'drain') {
+			sortingText += 'HP'; 
+		} else if (sorting == 'od') {
+			sortingText += 'OD';
+		} else if (sorting == 'bpm') {
+			sortingText += 'BPM';
+		} else if (sorting == 'length') {
+			sortingText += 'length';
+		}
 	}
 
 	let gameMode = getGameModeName(mode);
+	let title;
 
-	let title = `✰ ${serverDisplay}${user.name}'s ${recent}${gameMode} top plays ✰`;
-	if (user.name.endsWith('s') || user.name.endsWith('x')) {
-		title = `✰ ${serverDisplay}${user.name}' ${recent}${gameMode} top plays ✰`;
+	if(sorting !== null){
+		title = `✰ ${serverDisplay}${user.name}'s ${gameMode} top plays ${sortingText} ✰`;
+		if (user.name.endsWith('s') || user.name.endsWith('x')) {
+			title = `✰ ${serverDisplay}${user.name}' ${gameMode} top plays ${sortingText} ✰`;
+		}
+	} else {
+		title = `✰ ${serverDisplay}${user.name}'s ${gameMode} top plays ✰`;
+		if (user.name.endsWith('s') || user.name.endsWith('x')) {
+			title = `✰ ${serverDisplay}${user.name}' ${gameMode} top plays ✰`;
+		}
 	}
 
 	roundedRect(ctx, canvas.width / 2 - title.length * 8.5, 500 / 50, title.length * 17, 500 / 12, 5, '28', '28', '28', 0.75);
@@ -291,7 +338,7 @@ async function drawFooter(input) {
 	return output;
 }
 
-async function drawTopPlays(input, server, mode, msg, recentScores, showLimit) {
+async function drawTopPlays(input, server, mode, msg, sorting, showLimit) {
 	let canvas = input[0];
 	let ctx = input[1];
 	let user = input[2];
@@ -308,7 +355,7 @@ async function drawTopPlays(input, server, mode, msg, recentScores, showLimit) {
 
 	let limit = showLimit;
 
-	if (recentScores) {
+	if (sorting) {
 		limit = 100;
 	}
 
@@ -329,23 +376,54 @@ async function drawTopPlays(input, server, mode, msg, recentScores, showLimit) {
 		}
 	}
 
-	if (recentScores) {
-		quicksort(scores);
+	let sortedScores = [];
+	let beatmaps = [];
+	
+	if (sorting && sorting == 'recent') {
+		quicksortRecent(scores);
+	} 
+
+	for (let i = 0; i < showLimit; i++) {
+		let dbBeatmap = await getOsuBeatmap(scores[i].beatmapId, scores[i].raw_mods);
+		beatmaps.push(dbBeatmap);
 	}
 
-	for (let i = 0; i < scores.length && i < showLimit; i++) {
+
+
+	if (sorting) {
+		if (sorting == 'ar') {
+			quicksortAR(beatmaps);
+		} else if (sorting == 'cs') {
+			quicksortCS(beatmaps);
+		} else if (sorting == 'od') {
+			quicksortOD(beatmaps);
+		} else if (sorting == 'drain') {
+			quicksortHP(beatmaps);
+		} else if (sorting == 'bpm') {
+			quicksortBPM(beatmaps);
+		} else if (sorting == 'length') {
+			quicksortLength(beatmaps);
+		}
+	}
+
+	for (let i = 0; i < beatmaps.length && i < showLimit; i++) {
+		let scores = await osuApi.getScores({ u: user.name, b: beatmaps[i].beatmapId, m: mode });
+		sortedScores.push(scores);
+	}
+
+
+	for (let i = 0; i < sortedScores.length && i < showLimit; i++) {
 		roundedRect(ctx, canvas.width / 70, 500 / 8 + (500 / 12) * i, canvas.width - canvas.width / 35, 500 / 13, 500 / 70, '70', '57', '63', 0.75);
 
-		const rankImage = await Canvas.loadImage(getRankImage(scores[i].rank));
+		const rankImage = await Canvas.loadImage(getRankImage(sortedScores[i][0].rank));
 		ctx.drawImage(rankImage, canvas.width / 35, 500 / 8 + (500 / 12) * i + 500 / 13 / 2 - 500 / 31.25 / 2, canvas.width / 31.25, 500 / 31.25);
 
 		ctx.font = 'bold 18px comfortaa, sans-serif';
 		ctx.fillStyle = '#FF66AB';
 		ctx.textAlign = 'right';
-		ctx.fillText(humanReadable(Math.round(scores[i].pp)) + 'pp', (canvas.width / 35) * 34, 500 / 8 + (500 / 12) * i + 500 / 13 / 2 + 500 / 70);
+		ctx.fillText(humanReadable(Math.round(sortedScores[i][0].pp)) + 'pp', (canvas.width / 35) * 34, 500 / 8 + (500 / 12) * i + 500 / 13 / 2 + 500 / 70);
 
-		const dbBeatmap = await getOsuBeatmap(scores[i].beatmapId, 0);
-		let beatmapTitle = `${dbBeatmap.title} by ${dbBeatmap.artist}`;
+		let beatmapTitle = `${beatmaps[i].title} by ${beatmaps[i].artist}`;
 		const maxSize = canvas.width / 250 * 19;
 		if (beatmapTitle.length > maxSize) {
 			beatmapTitle = beatmapTitle.substring(0, maxSize - 3) + '...';
@@ -361,13 +439,13 @@ async function drawTopPlays(input, server, mode, msg, recentScores, showLimit) {
 		ctx.font = 'bold 10px comfortaa, sans-serif';
 		ctx.fillStyle = '#FFCC22';
 		ctx.textAlign = 'left';
-		ctx.fillText(dbBeatmap.difficulty, (canvas.width / 35) * 3, 500 / 8 + (500 / 12) * i + 500 / 12 / 2 + 500 / 35);
+		ctx.fillText(beatmaps[i].difficulty, (canvas.width / 35) * 3, 500 / 8 + (500 / 12) * i + 500 / 12 / 2 + 500 / 35);
 
 		let today = new Date();
 
 		const todayMilliseconds = today.getTime();	//Get the time (milliseconds since January 1, 1970)
 
-		const scoreMilliseconds = Date.parse(scores[i].raw_date); //Get the time (milliseconds since January 1, 1970)
+		const scoreMilliseconds = Date.parse(sortedScores[i][0].raw_date); //Get the time (milliseconds since January 1, 1970)
 
 		let timeDifference = todayMilliseconds - scoreMilliseconds;
 
@@ -393,16 +471,15 @@ async function drawTopPlays(input, server, mode, msg, recentScores, showLimit) {
 		ctx.font = 'bold 10px comfortaa, sans-serif';
 		ctx.fillStyle = '#A08C95';
 		ctx.textAlign = 'left';
-		ctx.fillText(achievedTime, (canvas.width / 35) * 3 + parseInt(dbBeatmap.difficulty.length) * 6 + canvas.width / 50, 500 / 8 + (500 / 12) * i + 500 / 12 / 2 + 500 / 35);
-
-		let accuracy = getAccuracy(scores[i], mode) * 100;
+		ctx.fillText(achievedTime, (canvas.width / 35) * 3 + parseInt(beatmaps[i].difficulty.length) * 6 + canvas.width / 50, 500 / 8 + (500 / 12) * i + 500 / 12 / 2 + 500 / 35);
+		let accuracy = getAccuracy(sortedScores[i][0], mode) * 100;
 
 		let combo;
 
 		if (mode === 3) {
-			combo = `(${scores[i].maxCombo}x)`;
+			combo = `(${sortedScores[i][0].maxCombo}x)`;
 		} else {
-			combo = `(${scores[i].maxCombo}/${dbBeatmap.maxCombo})`;
+			combo = `(${sortedScores[i][0].maxCombo}/${beatmaps[i].maxCombo})`;
 		}
 
 		ctx.font = 'bold 10px comfortaa, sans-serif';
@@ -411,7 +488,7 @@ async function drawTopPlays(input, server, mode, msg, recentScores, showLimit) {
 		ctx.fillText(combo, (canvas.width / 28) * 23.4, 500 / 8 + (500 / 12) * i + 500 / 12 / 2 + 500 / 35);
 		ctx.fillText(Math.round(accuracy * 100) / 100 + '%', (canvas.width / 28) * 24.75, 500 / 8 + (500 / 12) * i + 500 / 12 / 2 + 500 / 35);
 
-		const mods = getMods(scores[i].raw_mods);
+		const mods = getMods(sortedScores[i][0].raw_mods);
 		for (let j = 0; j < mods.length; j++) {
 			const modImage = await Canvas.loadImage(getModImage(mods[mods.length - j - 1]));
 			ctx.drawImage(modImage, (canvas.width / 28) * 24.75 - (canvas.width / 1000 * 23) * (j + 1), 500 / 8 + (500 / 12) * i + (500 / 12) / 5, canvas.width / 1000 * 23, 500 / 125 * 4);
@@ -422,7 +499,7 @@ async function drawTopPlays(input, server, mode, msg, recentScores, showLimit) {
 	return output;
 }
 
-function partition(list, start, end) {
+function partitionRecent(list, start, end) {
 	const pivot = list[end];
 	let i = start;
 	for (let j = start; j < end; j += 1) {
@@ -435,14 +512,156 @@ function partition(list, start, end) {
 	return i;
 }
 
-function quicksort(list, start = 0, end = undefined) {
+function quicksortRecent(list, start = 0, end = undefined) {
 	if (end === undefined) {
 		end = list.length - 1;
 	}
 	if (start < end) {
-		const p = partition(list, start, end);
-		quicksort(list, start, p - 1);
-		quicksort(list, p + 1, end);
+		const p = partitionRecent(list, start, end);
+		quicksortRecent(list, start, p - 1);
+		quicksortRecent(list, p + 1, end);
+	}
+	return list;
+}
+function partitionAR(list, start, end) {
+	const pivot = list[end];
+	let i = start;
+	for (let j = start; j < end; j += 1) {
+		if (parseFloat(list[j].approachRate) >= parseFloat(pivot.approachRate)) {
+			[list[j], list[i]] = [list[i], list[j]];
+			i++;
+		}
+	}
+	[list[i], list[end]] = [list[end], list[i]];
+	return i;
+}
+function quicksortAR(list, start = 0, end = undefined) {
+	if (end === undefined) {
+		end = list.length - 1;
+	}
+	if (start < end) {
+		const p = partitionAR(list, start, end);
+		quicksortAR(list, start, p - 1);
+		quicksortAR(list, p + 1, end);
+	}
+	return list;
+}
+function partitionCS(list, start, end) {
+	const pivot = list[end];
+	let i = start;
+	for (let j = start; j < end; j += 1) {
+		if (parseFloat(list[j].circleSize) >= parseFloat(pivot.circleSize)) {
+			[list[j], list[i]] = [list[i], list[j]];
+			i++;
+		}
+	}
+	[list[i], list[end]] = [list[end], list[i]];
+	return i;
+}
+
+function quicksortCS(list, start = 0, end = undefined) {
+	if (end === undefined) {
+		end = list.length - 1;
+	}
+	if (start < end) {
+		const p = partitionCS(list, start, end);
+		quicksortCS(list, start, p - 1);
+		quicksortCS(list, p + 1, end);
+	}
+	return list;
+}
+function partitionOD(list, start, end) {
+	const pivot = list[end];
+	let i = start;
+	for (let j = start; j < end; j += 1) {
+		if (parseFloat(list[j].overallDifficulty) >= parseFloat(pivot.overallDifficulty)) {
+			[list[j], list[i]] = [list[i], list[j]];
+			i++;
+		}
+	}
+	[list[i], list[end]] = [list[end], list[i]];
+	return i;
+}
+
+function quicksortOD(list, start = 0, end = undefined) {
+	if (end === undefined) {
+		end = list.length - 1;
+	}
+	if (start < end) {
+		const p = partitionOD(list, start, end);
+		quicksortOD(list, start, p - 1);
+		quicksortOD(list, p + 1, end);
+	}
+	return list;
+}
+function partitionHP(list, start, end) {
+	const pivot = list[end];
+	let i = start;
+	for (let j = start; j < end; j += 1) {
+		if (parseFloat(list[j].hpDrain) >= parseFloat(pivot.hpDrain)) {
+			[list[j], list[i]] = [list[i], list[j]];
+			i++;
+		}
+	}
+	[list[i], list[end]] = [list[end], list[i]];
+	return i;
+}
+
+function quicksortHP(list, start = 0, end = undefined) {
+	if (end === undefined) {
+		end = list.length - 1;
+	}
+	if (start < end) {
+		const p = partitionHP(list, start, end);
+		quicksortHP(list, start, p - 1);
+		quicksortHP(list, p + 1, end);
+	}
+	return list;
+}
+function partitionBPM(list, start, end) {
+	const pivot = list[end];
+	let i = start;
+	for (let j = start; j < end; j += 1) {
+		if (parseFloat(list[j].bpm) >= parseFloat(pivot.bpm)) {
+			[list[j], list[i]] = [list[i], list[j]];
+			i++;
+		}
+	}
+	[list[i], list[end]] = [list[end], list[i]];
+	return i;
+}
+
+function quicksortBPM(list, start = 0, end = undefined) {
+	if (end === undefined) {
+		end = list.length - 1;
+	}
+	if (start < end) {
+		const p = partitionBPM(list, start, end);
+		quicksortBPM(list, start, p - 1);
+		quicksortBPM(list, p + 1, end);
+	}
+	return list;
+}
+function partitionLength(list, start, end) {
+	const pivot = list[end];
+	let i = start;
+	for (let j = start; j < end; j += 1) {
+		if (parseFloat(list[j].totalLength) >= parseFloat(pivot.totalLength)) {
+			[list[j], list[i]] = [list[i], list[j]];
+			i++;
+		}
+	}
+	[list[i], list[end]] = [list[end], list[i]];
+	return i;
+}
+function quicksortLength(list, start = 0, end = undefined) {
+	if (end === undefined) {
+		end = list.length - 1;
+	}
+	if (start < end) {
+		const p = partitionLength(list, start, end);
+		quicksortLength(list, start, p - 1);
+		quicksortLength(list, p + 1, end);
 	}
 	return list;
 }
