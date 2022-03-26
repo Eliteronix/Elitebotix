b495d946104edd005cf0c626cb0e485a79b6e7ad
Roddyyyy <38702439+Roddyyyy@users.noreply.github.com>
2022-03-27 01:23:01 +0200

    fixed notifications

--- processQueueTasks/tourneyLobbyRemind.js
+++ processQueueTasks/tourneyLobbyRemind.js
@@ -6,8 +6,14 @@ module.exports = {
 			//Nothing
 		});
 
+		let players = args[4].split(',');
+		let playerNames = '';
+		for (let i = 0; i < players.length; i++) {
+			playerNames += `${players[i]} `;
+		}
+
 		if (user) {
-			user.send(`The lobby \`${args[1]}\` in ${args[2]} is going to happen <t:${args[3]}:R>`);
+			user.send(`The lobby \`${args[1]}\` in ${args[2]} is going to happen <t:${args[3]}:R>\nThe players are: ${playerNames}`);
 		}
 		processQueueEntry.destroy();
 	},
