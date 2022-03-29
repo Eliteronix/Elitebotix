1c9134f50d92f9ed9cfdbc70073755383616571d
Roddyyyy <38702439+Roddyyyy@users.noreply.github.com>
2022-03-29 19:13:40 +0300

    Did stuff i guess but still looks like shit

--- commands/osu-top.js
+++ commands/osu-top.js
@@ -566,6 +566,8 @@ async function drawTopPlays(input, server, mode, msg, sorting, showLimit, proces
 	}
 
 	for (let i = 0; i < sortedScores.length && i < showLimit; i++) {
+		ctx.imageSmoothingEnabled = true;
+		ctx.imageSmoothingQuality = 'high';
 		roundedRect(ctx, canvas.width / 70, 500 / 8 + (500 / 12) * i, canvas.width - canvas.width / 35, 500 / 13, 500 / 70, '70', '57', '63', 0.75);
 
 		const rankImage = await Canvas.loadImage(getRankImage(sortedScores[i].rank));
@@ -639,6 +641,7 @@ async function drawTopPlays(input, server, mode, msg, sorting, showLimit, proces
 			combo = `(${sortedScores[i].maxCombo}/${beatmaps[i].maxCombo})`;
 		}
 
+		// Write accuracy and combo per map
 		ctx.font = 'bold 10px comfortaa, sans-serif';
 		ctx.fillStyle = '#FFCC22';
 		ctx.textAlign = 'right';
@@ -646,6 +649,40 @@ async function drawTopPlays(input, server, mode, msg, sorting, showLimit, proces
 		ctx.fillText(Math.round(accuracy * 100) / 100 + '%', (canvas.width / 28) * 24.75, 500 / 8 + (500 / 12) * i + 500 / 12 / 2 + 500 / 35);
 
 		const mods = getMods(sortedScores[i].raw_mods);
+		
+		let arrow = '';
+		if (mods.includes('DT') || mods.includes('NC')) {
+			arrow = '^'; 
+		}
+
+		let sortingText;
+		if (sorting !== null) {
+			if (sorting == 'ar') {
+				sortingText = 'AR: ' + beatmaps[i].approachRate + arrow;
+			} else if (sorting == 'cs') {
+				sortingText = 'CS: ' + beatmaps[i].circleSize;
+			} else if (sorting == 'drain') {
+				sortingText = 'HP: ' + beatmaps[i].drain + arrow;
+			} else if (sorting == 'od') {
+				sortingText = 'OD: ' + beatmaps[i].overallDifficulty + arrow;
+			} else if (sorting == 'bpm') {
+				sortingText = 'BPM: ' + beatmaps[i].bpm;
+			} else if (sorting == 'length') {
+				const totalLengthSeconds = (beatmaps[i].totalLength % 60) + '';
+				const totalLengthMinutes = (beatmaps[i].totalLength - beatmaps[i].totalLength % 60) / 60;
+				const totalLength = totalLengthMinutes + ':' + Math.round(totalLengthSeconds).toString().padStart(2, '0');
+				sortingText = 'Length: ' + totalLength;
+			}
+		}
+		ctx.font = 'bold 10px comfortaa, sans-serif';
+		ctx.fillStyle = '#FFFFFF';
+		ctx.textAlign = 'right';
+
+		if (sorting){
+			ctx.fillText(sortingText, (canvas.width / 28) * 21.5, 500 / 8 + (500 / 12) * i + 500 / 12 / 2);
+		}
+
+		//Write mods per map
 		for (let j = 0; j < mods.length; j++) {
 			const modImage = await Canvas.loadImage(getModImage(mods[mods.length - j - 1]));
 			ctx.drawImage(modImage, (canvas.width / 28) * 24.75 - (canvas.width / 1000 * 23) * (j + 1), 500 / 8 + (500 / 12) * i + (500 / 12) / 5, canvas.width / 1000 * 23, 500 / 125 * 4);
