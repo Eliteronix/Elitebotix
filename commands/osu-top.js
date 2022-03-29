d78816da477ce5d12aa6045de3a5d243ab1b798e
Roddyyyy <38702439+Roddyyyy@users.noreply.github.com>
2022-03-29 19:44:02 +0300

    merged title and sorting text, but this upwards arrow still looks like shit

--- commands/osu-top.js
+++ commands/osu-top.js
@@ -584,12 +584,6 @@ async function drawTopPlays(input, server, mode, msg, sorting, showLimit, proces
 			beatmapTitle = beatmapTitle.substring(0, maxSize - 3) + '...';
 		}
 
-		//Write title per map
-		ctx.font = 'bold 15px comfortaa, sans-serif';
-		ctx.fillStyle = '#FFFFFF';
-		ctx.textAlign = 'left';
-		ctx.fillText(beatmapTitle, (canvas.width / 35) * 3, 500 / 8 + (500 / 12) * i + 500 / 12 / 2);
-
 		//Write Difficulty per map
 		ctx.font = 'bold 10px comfortaa, sans-serif';
 		ctx.fillStyle = '#FFCC22';
@@ -649,7 +643,6 @@ async function drawTopPlays(input, server, mode, msg, sorting, showLimit, proces
 		ctx.fillText(Math.round(accuracy * 100) / 100 + '%', (canvas.width / 28) * 24.75, 500 / 8 + (500 / 12) * i + 500 / 12 / 2 + 500 / 35);
 
 		const mods = getMods(sortedScores[i].raw_mods);
-		
 		let arrow = '';
 		if (mods.includes('DT') || mods.includes('NC')) {
 			arrow = '^'; 
@@ -658,29 +651,28 @@ async function drawTopPlays(input, server, mode, msg, sorting, showLimit, proces
 		let sortingText;
 		if (sorting !== null) {
 			if (sorting == 'ar') {
-				sortingText = 'AR: ' + beatmaps[i].approachRate + arrow;
+				sortingText = ` (AR: ${beatmaps[i].approachRate + arrow})`;
 			} else if (sorting == 'cs') {
-				sortingText = 'CS: ' + beatmaps[i].circleSize;
+				sortingText = ` (CS: ${beatmaps[i].circleSize})`;
 			} else if (sorting == 'drain') {
-				sortingText = 'HP: ' + beatmaps[i].drain + arrow;
+				sortingText = ` (HP: ${beatmaps[i].drain + arrow})`;
 			} else if (sorting == 'od') {
-				sortingText = 'OD: ' + beatmaps[i].overallDifficulty + arrow;
+				sortingText = ` (OD: ${beatmaps[i].overallDifficulty + arrow})`;
 			} else if (sorting == 'bpm') {
-				sortingText = 'BPM: ' + beatmaps[i].bpm;
+				sortingText = ` (BPM: ${beatmaps[i].bpm})`;
 			} else if (sorting == 'length') {
 				const totalLengthSeconds = (beatmaps[i].totalLength % 60) + '';
 				const totalLengthMinutes = (beatmaps[i].totalLength - beatmaps[i].totalLength % 60) / 60;
 				const totalLength = totalLengthMinutes + ':' + Math.round(totalLengthSeconds).toString().padStart(2, '0');
-				sortingText = 'Length: ' + totalLength;
+				sortingText = ` (Length: ${totalLength})`;
 			}
 		}
-		ctx.font = 'bold 10px comfortaa, sans-serif';
-		ctx.fillStyle = '#FFFFFF';
-		ctx.textAlign = 'right';
 
-		if (sorting){
-			ctx.fillText(sortingText, (canvas.width / 28) * 21.5, 500 / 8 + (500 / 12) * i + 500 / 12 / 2);
-		}
+		//Write title and sortingText per map
+		ctx.font = 'bold 15px comfortaa, sans-serif';
+		ctx.fillStyle = '#FFFFFF';
+		ctx.textAlign = 'left';
+		ctx.fillText(beatmapTitle + sortingText, (canvas.width / 35) * 3, 500 / 8 + (500 / 12) * i + 500 / 12 / 2);
 
 		//Write mods per map
 		for (let j = 0; j < mods.length; j++) {
