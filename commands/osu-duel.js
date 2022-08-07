02421c31200ef3962909ee5fcce7215c0201439b
Eliteronix <zimmermann.mariomarvin@gmail.com>
2022-08-07 18:15:10 +0200

    More untested code go brrrrr

--- commands/osu-duel.js
+++ commands/osu-duel.js
@@ -70,6 +70,29 @@ module.exports = {
 					return await interaction.editReply('You don\'t have your osu! account connected and verified.\nPlease connect your account by using `/osu-link connect <username>`.');
 				}
 
+				let now = new Date();
+				let rapidPlayers = [
+					'17116333', // Husmor
+					'3148418', // Terces
+					'2166373', // Rubgish
+					'15264880', // Laan_c
+					'1983942', // Obi-nyan
+					'17151706', // Mark Rober
+					'11155994', // megahello
+					'7516377', // Baiterzz
+					'12079158', // cherry sweet
+					'13326023', // LordYuto
+					'23473167', // Julius Novacron
+					'4280363', // x3Lydiia
+					'10805810', // hexencoda
+					'10497897', // Kaitiri
+					'2844779', // boterham
+				];
+
+				if (now.getDate() === 7 && now.getMonth() === 7 && now.getUTCHours() > 16 && !rapidPlayers.includes(commandUser.osuUserId)) {
+					return await interaction.editReply('The duel slots are currently reserved for a rapid tournament hosted by the Dev, Eliteronix. As soon as the tournament is over, the slots will be available again.');
+				}
+
 				if (opponentId && commandUser.userId === opponentId || firstOpponentId && commandUser.userId === firstOpponentId || secondOpponentId && commandUser.userId === secondOpponentId) {
 					return await interaction.editReply('You cannot play against yourself.');
 				}
