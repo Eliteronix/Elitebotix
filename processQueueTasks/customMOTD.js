58dc1183b6938ecbd24cc001067cffe8f41dab0c
Eliteronix <zimmermann.mariomarvin@gmail.com>
2022-08-14 16:00:23 +0200

    Update customMOTD.js

--- processQueueTasks/customMOTD.js
+++ processQueueTasks/customMOTD.js
@@ -2,7 +2,7 @@ const { pause } = require('../utils');
 
 module.exports = {
 	async execute(client, bancho, processQueueEntry) {
-		console.log('closeMOTD');
+		console.log('customMOTD');
 		//used to block new osu-referee matches to be scheduled while customMOTDs are blocking it
 		await pause(parseInt(processQueueEntry.additions) * 1000);
 		try {
