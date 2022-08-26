8eb15b13d438696aa13a80a6a881c2a912430a77
Eliteronix <zimmermann.mariomarvin@gmail.com>
2022-08-26 23:45:08 +0200

    Added a space

--- gotBanchoPrivateMessage.js
+++ gotBanchoPrivateMessage.js
@@ -353,7 +353,7 @@ module.exports = async function (client, bancho, message) {
 			modeText = ' [Taiko]';
 		}
 
-		message.user.sendMessage(`[https://osu.ppy.sh/b/${beatmap.beatmapId} ${beatmap.artist} - ${beatmap.title} [${beatmap.difficulty}]]${modeText} + ${mod} | Beatmap ★: ${Math.floor(beatmap.starRating * 100) / 100}${hdBuff}| Your${specifiedRating ? 'specified' : ''} ${mod} duel ★: ${Math.floor(userStarRating * 100) / 100} | ${totalLength}  ♫${beatmap.bpm}  AR${beatmap.approachRate}  OD${beatmap.overallDifficulty}`);
+		message.user.sendMessage(`[https://osu.ppy.sh/b/${beatmap.beatmapId} ${beatmap.artist} - ${beatmap.title} [${beatmap.difficulty}]]${modeText} + ${mod} | Beatmap ★: ${Math.floor(beatmap.starRating * 100) / 100}${hdBuff}| Your${specifiedRating ? ' specified' : ''} ${mod} duel ★: ${Math.floor(userStarRating * 100) / 100} | ${totalLength}  ♫${beatmap.bpm}  AR${beatmap.approachRate}  OD${beatmap.overallDifficulty}`);
 
 
 		logDatabaseQueries(4, 'commands/osu-beatmap.js DBOsuMultiScores');
