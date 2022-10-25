3cb876f79e3fec1cdd13780073f7c0ddc75778a2
Eliteronix <zimmermann.mariomarvin@gmail.com>
2022-10-25 21:05:28 +0200

    Fixed modpools

--- utils.js
+++ utils.js
@@ -1430,9 +1430,9 @@ module.exports = {
 
 		//Fill as much as needed in groups
 		while (modPools.length < bestOf - 1) {
-			let modsToAdd = ['FreeMod', 'FreeMod', 'FreeMod', 'FreeMod', 'FreeMod'];
+			let modsToAdd = ['NM', 'HD', 'HR', 'DT', 'FreeMod'];
 			shuffle(modsToAdd);
-			modsToAdd.push('FreeMod');
+			modsToAdd.push('NM');
 
 			while (modsToAdd.length) {
 				modPools.push(modsToAdd.shift());
