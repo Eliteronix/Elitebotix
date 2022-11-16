f112fa885f277cfc84198475306b2e3b3a0584a7
Eliteronix <zimmermann.mariomarvin@gmail.com>
2022-11-16 17:03:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { ShardingManager } = require('discord.js');
 require('dotenv').config();
 
 // eslint-disable-next-line no-undef
-let manager = new ShardingManager('./bot.js', { token: process.env.BOTTOKEN });
+let manager = new ShardingManager('./bot.js', { token: process.env.BOTTOKEN, totalShards: 6 });
 
 // eslint-disable-next-line no-undef
 if (process.env.SERVER === 'Dev') {
