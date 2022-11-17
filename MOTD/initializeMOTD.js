fee51c18dbd3f5e459e97edb1ada49d2fe5d7dca
Eliteronix <zimmermann.mariomarvin@gmail.com>
2022-11-17 21:32:12 +0100

    Added more logging

--- MOTD/initializeMOTD.js
+++ MOTD/initializeMOTD.js
@@ -195,6 +195,7 @@ module.exports = {
 			// eslint-disable-next-line no-undef
 		} else if (process.env.SERVER === 'Live' && today.getUTCMinutes() % 10 === 0) {
 			//Assign roles to all players currently registered and remove unneeded roles
+			console.log('assigning roles');
 			await assignPlayerRoles(client);
 		}
 	}
