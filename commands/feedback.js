fe2039e580c08201c823be2dd46593913e246336
Eliteronix <zimmermann.mariomarvin@gmail.com>
2022-12-13 02:58:32 +0100

    Fixed feedback command v2

--- commands/feedback.js
+++ commands/feedback.js
@@ -32,25 +32,25 @@ module.exports = {
 		let message = interaction.options.getString('feedback');
 
 		if (message.length > 200) {
-			return interaction.reply(`Your message is too long. Please shorten it. (Max: 200 characters, currently: ${message.length})`);
+			return interaction.editReply(`Your message is too long. Please shorten it. (Max: 200 characters, currently: ${message.length})`);
 		}
 
 		if (type === 'bug') {
 			createJiraIssue('10006', `[BUG] ${message} - ${interaction.user.username}#${interaction.user.discriminator}`);
 
-			return interaction.reply('Your bug report was sent to the developers.');
+			return interaction.editReply('Your bug report was sent to the developers.');
 		} else if (type === 'feature') {
 			createJiraIssue('10007', `[FEATURE] ${message} - ${interaction.user.username}#${interaction.user.discriminator}`);
 
-			return interaction.reply('Your feature-request was sent to the developers.');
+			return interaction.editReply('Your feature-request was sent to the developers.');
 		} else if (type === 'feedback') {
 			createJiraIssue('10005', `[FEEDBACK] ${message} - ${interaction.user.username}#${interaction.user.discriminator}`);
 
-			return interaction.reply('Your feedback has been sent to the developers.');
+			return interaction.editReply('Your feedback has been sent to the developers.');
 		} else if (type === 'question') {
 			createJiraIssue('10008', `[QUESTION] ${message} - ${interaction.user.username}#${interaction.user.discriminator}`);
 
-			return interaction.reply('Your question has been sent to the developers. They will respond to you as soon as possible.');
+			return interaction.editReply('Your question has been sent to the developers. They will respond to you as soon as possible.');
 		}
 	},
 };
