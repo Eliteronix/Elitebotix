015a2f4a1898c13086de1283dff625279e9ba735
Eliteronix <zimmermann.mariomarvin@gmail.com>
2023-01-11 23:25:25 +0100

    Restructured dbObjects

--- dbObjects.js
+++ dbObjects.js
@@ -17,25 +17,33 @@ const DBReactionRoles = require('./models/DBReactionRoles')(sequelize, Sequelize
 const DBReactionRolesHeader = require('./models/DBReactionRolesHeader')(sequelize, Sequelize.DataTypes);
 const DBAutoRoles = require('./models/DBAutoRoles')(sequelize, Sequelize.DataTypes);
 const DBTemporaryVoices = require('./models/DBTemporaryVoices')(sequelize, Sequelize.DataTypes);
-const DBDiscordUsers = require('./models/DBDiscordUsers')(sequelize, Sequelize.DataTypes);
-const DBServerUserActivity = require('./models/DBServerUserActivity')(sequelize, Sequelize.DataTypes);
-const DBProcessQueue = require('./models/DBProcessQueue')(sequelize, Sequelize.DataTypes);
 const DBActivityRoles = require('./models/DBActivityRoles')(sequelize, Sequelize.DataTypes);
-const DBMOTDPoints = require('./models/DBMOTDPoints')(sequelize, Sequelize.DataTypes);
-const DBElitiriCupSignUp = require('./models/DBElitiriCupSignUp')(sequelize, Sequelize.DataTypes);
-const DBElitiriCupStaff = require('./models/DBElitiriCupStaff')(sequelize, Sequelize.DataTypes);
-const DBElitiriCupSubmissions = require('./models/DBElitiriCupSubmissions')(sequelize, Sequelize.DataTypes);
 const DBStarBoardMessages = require('./models/DBStarBoardMessages')(sequelize, Sequelize.DataTypes);
 const DBTickets = require('./models/DBTickets')(sequelize, Sequelize.DataTypes);
-const DBOsuMultiScores = require('./models/DBOsuMultiScores')(sequelize, Sequelize.DataTypes);
-const DBOsuBeatmaps = require('./models/DBOsuBeatmaps')(sequelize, Sequelize.DataTypes);
-const DBElitiriCupLobbies = require('./models/DBElitiriCupLobbies')(sequelize, Sequelize.DataTypes);
 const DBBirthdayGuilds = require('./models/DBBirthdayGuilds')(sequelize, Sequelize.DataTypes);
+const DBOsuGuildTrackers = require('./models/DBOsuGuildTrackers')(sequelize, Sequelize.DataTypes);
+
+const DBDiscordUsers = require('./models/DBDiscordUsers')(sequelize, Sequelize.DataTypes);
+
+const DBServerUserActivity = require('./models/DBServerUserActivity')(sequelize, Sequelize.DataTypes);
+
+const DBProcessQueue = require('./models/DBProcessQueue')(sequelize, Sequelize.DataTypes);
+
+const DBMOTDPoints = require('./models/DBMOTDPoints')(sequelize, Sequelize.DataTypes);
 const DBOsuTourneyFollows = require('./models/DBOsuTourneyFollows')(sequelize, Sequelize.DataTypes);
 const DBDuelRatingHistory = require('./models/DBDuelRatingHistory')(sequelize, Sequelize.DataTypes);
 const DBOsuForumPosts = require('./models/DBOsuForumPosts')(sequelize, Sequelize.DataTypes);
 const DBOsuTrackingUsers = require('./models/DBOsuTrackingUsers')(sequelize, Sequelize.DataTypes);
-const DBOsuGuildTrackers = require('./models/DBOsuGuildTrackers')(sequelize, Sequelize.DataTypes);
+
+const DBElitiriCupSignUp = require('./models/DBElitiriCupSignUp')(sequelize, Sequelize.DataTypes);
+const DBElitiriCupStaff = require('./models/DBElitiriCupStaff')(sequelize, Sequelize.DataTypes);
+const DBElitiriCupSubmissions = require('./models/DBElitiriCupSubmissions')(sequelize, Sequelize.DataTypes);
+const DBElitiriCupLobbies = require('./models/DBElitiriCupLobbies')(sequelize, Sequelize.DataTypes);
+
+const DBOsuMultiScores = require('./models/DBOsuMultiScores')(sequelize, Sequelize.DataTypes);
+
+const DBOsuBeatmaps = require('./models/DBOsuBeatmaps')(sequelize, Sequelize.DataTypes);
+
 
 module.exports = {
 	DBGuilds,
