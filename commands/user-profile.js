a360a9f809a942aaf39765bf952bb7e86f2cde7f
Eliteronix <zimmermann.mariomarvin@gmail.com>
2023-01-11 22:09:16 +0100

    Removed todo

--- commands/user-profile.js
+++ commands/user-profile.js
@@ -28,7 +28,6 @@ module.exports = {
 	prefixCommand: true,
 	// eslint-disable-next-line no-unused-vars
 	async execute(msg, args, interaction, additionalObjects) {
-		//TODO: Remove message code and replace with interaction code
 		try {
 			await interaction.deferReply();
 		} catch (error) {
