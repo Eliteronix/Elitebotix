63db01418f931ca18ab3c2647a9f38856a3ba604
Eliteronix <zimmermann.mariomarvin@gmail.com>
2023-01-12 20:50:19 +0100

    Removed todo

--- commands/osu-wrapped.js
+++ commands/osu-wrapped.js
@@ -25,7 +25,6 @@ module.exports = {
 	// eslint-disable-next-line no-unused-vars
 	async execute(msg, args, interaction) {
 		//TODO: Remove message code and replace with interaction code
-		//TODO: deferReply
 		try {
 			await interaction.deferReply();
 		} catch (error) {
