0b57b96893a8b8a91e30ee3fef860af50a87dc04
Marvin Zimmermann <64707347+Eliteronix@users.noreply.github.com>
2023-01-17 09:11:37 +0100

    Fixed error

--- commands/osu-matchup.js
+++ commands/osu-matchup.js
@@ -31,7 +31,7 @@ module.exports = {
 
 			try {//TODO: Deferreply
 				//await interaction.deferReply();
-				interaction.editReply('Processing...');
+				await interaction.editReply('Processing...');
 			} catch (error) {
 				if (error.message === 'Unknown interaction' && showUnknownInteractionError || error.message !== 'Unknown interaction') {
 					console.error(error);
