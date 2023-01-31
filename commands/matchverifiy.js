9bb22bc2984674ae2ab49d5ad57e99014667fc90
Eliteronix <zimmermann.mariomarvin@gmail.com>
2023-01-31 13:00:25 +0100

    Update matchverifiy.js

--- commands/matchverifiy.js
+++ commands/matchverifiy.js
@@ -141,7 +141,7 @@ module.exports = {
 
 			await interaction.editReply({ embeds: [unverifiedScoresEmbed] });
 		} else if (interaction.options._subcommand === 'update') {
-			let matchId = getIDFromPotentialOsuLink(interaction.options.getString('id'));
+			let matchIds = interaction.options.getString('id').split(/ +/);
 			let valid = interaction.options.getBoolean('valid');
 			let comment = interaction.options.getString('comment');
 
@@ -159,88 +159,97 @@ module.exports = {
 				return await interaction.editReply('You need to connect and verify your osu! account first to use this command.');
 			}
 
-			let scores = await DBOsuMultiScores.findAll({
-				where: {
-					matchId: matchId,
-				},
-			});
+			await interaction.editReply('Processing...');
 
-			if (!developers.includes(interaction.user.id)) {
+			for (let matchIndex = 0; matchIndex < matchIds.length; matchIndex++) {
+				let matchId = getIDFromPotentialOsuLink(matchIds[matchIndex]);
+
+				let scores = await DBOsuMultiScores.findAll({
+					where: {
+						matchId: matchId,
+					},
+				});
+
+				if (!developers.includes(interaction.user.id)) {
+					for (let i = 0; i < scores.length; i++) {
+						let score = scores[i];
+						if (score.osuUserId === discordUser.osuUserId) {
+							await interaction.followUp('You cannot verify your own scores.');
+							continue;
+						}
+					}
+				}
+
+				let updatedUsers = [];
+				let tourneyMatchChanged = false;
+				let tourneyMatchChangedString = '';
+
 				for (let i = 0; i < scores.length; i++) {
 					let score = scores[i];
-					if (score.osuUserId === discordUser.osuUserId) {
-						return await interaction.editReply('You cannot verify your own scores.');
+
+					if (!updatedUsers.includes(score.osuUserId)) {
+						updatedUsers.push(score.osuUserId);
 					}
-				}
-			}
 
-			let updatedUsers = [];
-			let tourneyMatchChanged = false;
-			let tourneyMatchChangedString = '';
-
-			for (let i = 0; i < scores.length; i++) {
-				let score = scores[i];
-
-				if (!updatedUsers.includes(score.osuUserId)) {
-					updatedUsers.push(score.osuUserId);
-				}
-
-				if (score.tourneyMatch !== valid) {
-					tourneyMatchChanged = true;
-					tourneyMatchChangedString = '**Changed**: ';
-				}
-			}
-
-			await DBOsuMultiScores.update({
-				tourneyMatch: valid,
-				verifiedAt: new Date(),
-				verifiedBy: discordUser.osuUserId,
-				verificationComment: comment,
-			}, {
-				where: {
-					matchId: matchId,
-				},
-			});
-
-			if (tourneyMatchChanged) {
-				let ratingHistories = await DBDuelRatingHistory.findAll({
-					where: {
-						osuUserId: {
-							[Op.in]: updatedUsers,
-						},
-					},
-				});
-
-				for (let i = 0; i < ratingHistories.length; i++) {
-					let ratingHistory = ratingHistories[i];
-
-					let ratingHistoryDate = new Date();
-					ratingHistoryDate.setUTCDate(ratingHistory.date);
-					ratingHistoryDate.setUTCMonth(ratingHistory.month - 1);
-					ratingHistoryDate.setUTCFullYear(ratingHistory.year);
-
-					if (ratingHistoryDate > new Date(scores[0].matchStartDate)) {
-						await ratingHistory.destroy();
+					if (score.tourneyMatch !== valid) {
+						tourneyMatchChanged = true;
+						tourneyMatchChangedString = '**Changed**: ';
 					}
 				}
 
-				await DBDiscordUsers.update({
-					lastDuelRatingUpdate: null,
+				await DBOsuMultiScores.update({
+					tourneyMatch: valid,
+					verifiedAt: new Date(),
+					verifiedBy: discordUser.osuUserId,
+					verificationComment: comment,
 				}, {
 					where: {
-						osuUserId: {
-							[Op.in]: updatedUsers,
-						},
+						matchId: matchId,
 					},
 				});
+
+				if (tourneyMatchChanged) {
+					let ratingHistories = await DBDuelRatingHistory.findAll({
+						where: {
+							osuUserId: {
+								[Op.in]: updatedUsers,
+							},
+						},
+					});
+
+					for (let i = 0; i < ratingHistories.length; i++) {
+						let ratingHistory = ratingHistories[i];
+
+						let ratingHistoryDate = new Date();
+						ratingHistoryDate.setUTCDate(ratingHistory.date);
+						ratingHistoryDate.setUTCMonth(ratingHistory.month - 1);
+						ratingHistoryDate.setUTCFullYear(ratingHistory.year);
+
+						if (ratingHistoryDate > new Date(scores[0].matchStartDate)) {
+							await ratingHistory.destroy();
+						}
+					}
+
+					await DBDiscordUsers.update({
+						lastDuelRatingUpdate: null,
+					}, {
+						where: {
+							osuUserId: {
+								[Op.in]: updatedUsers,
+							},
+						},
+					});
+				}
+
+				// eslint-disable-next-line no-undef
+				if (process.env.SERVER === 'Live') {
+					interaction.guild.channels.cache.get('1068905937219362826').send(`${tourneyMatchChangedString} - Valid: ${valid} | Comment: ${comment} | https://osu.ppy.sh/mp/${matchId} was verified by ${interaction.user.username}#${interaction.user.discriminator} (<@${interaction.user.id}> | <https://osu.ppy.sh/users/${discordUser.osuUserId}>)`);
+				}
+
+				await interaction.followUp(`Updated ${scores.length} scores for https://osu.ppy.sh/mp/${matchId}`);
 			}
 
-			// eslint-disable-next-line no-undef
-			if (process.env.SERVER === 'Live') {
-				interaction.guild.channels.cache.get('1068905937219362826').send(`${tourneyMatchChangedString} - Valid: ${valid} | Comment: ${comment} | https://osu.ppy.sh/mp/${matchId} was verified by ${interaction.user.username}#${interaction.user.discriminator} (<@${interaction.user.id}> | <https://osu.ppy.sh/users/${discordUser.osuUserId}>)`);
-			}
-
-			await interaction.editReply(`Updated ${scores.length} scores.`);
+			await interaction.editReply('Done processing.');
 		}
 	}
 };
