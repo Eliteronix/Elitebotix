50239b3cec98263751cd5a3ad5574ce9b7c638ca
Eliteronix <zimmermann.mariomarvin@gmail.com>
2023-02-01 22:22:58 +0100

    Update activityrole.js

--- commands/activityrole.js
+++ commands/activityrole.js
@@ -13,142 +13,142 @@ module.exports = {
 	botPermissionsTranslated: 'Send Messages and Manage Roles',
 	cooldown: 5,
 	tags: 'server-admin',
-	data: new SlashCommandBuilder()
-		.setName('activityrole')
-		.setNameLocalizations({
-			'de': 'aktivitätsrolle',
-			'en-GB': 'activityrole',
-			'en-US': 'activityrole',
-		})
-		.setDescription('Assigns roles depending on how active your users are')
-		.setDescriptionLocalizations({
-			'de': 'Weist Rollen je nach Aktivität deiner Benutzer zu',
-			'en-GB': 'Assigns roles depending on how active your users are',
-			'en-US': 'Assigns roles depending on how active your users are',
-		})
-		.setDefaultMemberPermissions([PermissionsBitField.Flags.ManageRoles])
-		.setDMPermission(false)
-		.addSubcommand(subcommand =>
-			subcommand.setName('add')
-				.setNameLocalizations({
-					'de': 'hinzufügen',
-					'en-GB': 'add',
-					'en-US': 'add',
-				})
-				.setDescription('Lets you add a new role which will be assigned based on useractivity')
-				.setDescriptionLocalizations({
-					'de': 'Lässt dich eine neue Rolle hinzufügen, die je nach Aktivität des Benutzers zugewiesen wird',
-					'en-GB': 'Lets you add a new role which will be assigned based on useractivity',
-					'en-US': 'Lets you add a new role which will be assigned based on useractivity',
-				})
-				.addRoleOption(option =>
-					option.setName('role')
-						.setNameLocalizations({
-							'de': 'rolle',
-							'en-GB': 'role',
-							'en-US': 'role',
-						})
-						.setDescription('The role that should be an activityrole')
-						.setDescriptionLocalizations({
-							'de': 'Die Rolle, die als Aktivitätsrolle zugewiesen werden soll',
-							'en-GB': 'The role that should be an activityrole',
-							'en-US': 'The role that should be an activityrole',
-						})
-						.setRequired(true)
-				)
-				.addIntegerOption(option =>
-					option.setName('rank')
-						.setNameLocalizations({
-							'de': 'rang',
-							'en-GB': 'rank',
-							'en-US': 'rank',
-						})
-						.setDescription('The required rank for getting the role')
-						.setDescriptionLocalizations({
-							'de': 'Der erforderliche Rang für die Rolle',
-							'en-GB': 'The required rank for getting the role',
-							'en-US': 'The required rank for getting the role',
-						})
-						.setRequired(false)
-						.setMinValue(1)
-				)
-				.addIntegerOption(option =>
-					option.setName('percentage')
-						.setNameLocalizations({
-							'de': 'prozent',
-							'en-GB': 'percentage',
-							'en-US': 'percentage',
-						})
-						.setDescription('The required topx% rank for getting the role')
-						.setDescriptionLocalizations({
-							'de': 'Der erforderliche topx% Rang für die Rolle',
-							'en-GB': 'The required topx% rank for getting the role',
-							'en-US': 'The required topx% rank for getting the role',
-						})
-						.setRequired(false)
-						.setMinValue(1)
-						.setMaxValue(99)
-				)
-				.addIntegerOption(option =>
-					option.setName('points')
-						.setNameLocalizations({
-							'de': 'punkte',
-							'en-GB': 'points',
-							'en-US': 'points',
-						})
-						.setDescription('The required points for getting the role')
-						.setDescriptionLocalizations({
-							'de': 'Die erforderlichen Punkte für die Rolle',
-							'en-GB': 'The required points for getting the role',
-							'en-US': 'The required points for getting the role',
-						})
-						.setRequired(false)
-						.setMinValue(1)
-				)
-		)
-		.addSubcommand(subcommand =>
-			subcommand.setName('remove')
-				.setNameLocalizations({
-					'de': 'entfernen',
-					'en-GB': 'remove',
-					'en-US': 'remove',
-				})
-				.setDescription('Remove an existing activityrole')
-				.setDescriptionLocalizations({
-					'de': 'Entfernt eine bestehende Aktivitätsrolle',
-					'en-GB': 'Remove an existing activityrole',
-					'en-US': 'Remove an existing activityrole',
-				})
-				.addRoleOption(option =>
-					option.setName('role')
-						.setNameLocalizations({
-							'de': 'rolle',
-							'en-GB': 'role',
-							'en-US': 'role',
-						})
-						.setDescription('The role that should no longer be an activityrole')
-						.setDescriptionLocalizations({
-							'de': 'Die Rolle, die nicht mehr als Aktivitätsrolle zugewiesen werden soll',
-							'en-GB': 'The role that should no longer be an activityrole',
-							'en-US': 'The role that should no longer be an activityrole',
-						})
-						.setRequired(true)
-				)
-		)
-		.addSubcommand(subcommand =>
-			subcommand.setName('list')
-				.setNameLocalizations({
-					'de': 'liste',
-					'en-GB': 'list',
-					'en-US': 'list',
-				})
-				.setDescription('Show which activityroles are set up')
-				.setDescriptionLocalizations({
-					'de': 'Zeigt welche Aktivitätsrollen existieren sind',
-					'en-GB': 'Show which activityroles are set up',
-					'en-US': 'Show which activityroles are set up',
-				})
-		),
+	// data: new SlashCommandBuilder()
+	// 	.setName('activityrole')
+	// 	.setNameLocalizations({
+	// 		'de': 'aktivitätsrolle',
+	// 		'en-GB': 'activityrole',
+	// 		'en-US': 'activityrole',
+	// 	})
+	// 	.setDescription('Assigns roles depending on how active your users are')
+	// 	.setDescriptionLocalizations({
+	// 		'de': 'Weist Rollen je nach Aktivität deiner Benutzer zu',
+	// 		'en-GB': 'Assigns roles depending on how active your users are',
+	// 		'en-US': 'Assigns roles depending on how active your users are',
+	// 	})
+	// 	.setDefaultMemberPermissions([PermissionsBitField.Flags.ManageRoles])
+	// 	.setDMPermission(false)
+	// 	.addSubcommand(subcommand =>
+	// 		subcommand.setName('add')
+	// 			.setNameLocalizations({
+	// 				'de': 'hinzufügen',
+	// 				'en-GB': 'add',
+	// 				'en-US': 'add',
+	// 			})
+	// 			.setDescription('Lets you add a new role which will be assigned based on useractivity')
+	// 			.setDescriptionLocalizations({
+	// 				'de': 'Lässt dich eine neue Rolle hinzufügen, die je nach Aktivität des Benutzers zugewiesen wird',
+	// 				'en-GB': 'Lets you add a new role which will be assigned based on useractivity',
+	// 				'en-US': 'Lets you add a new role which will be assigned based on useractivity',
+	// 			})
+	// 			.addRoleOption(option =>
+	// 				option.setName('role')
+	// 					.setNameLocalizations({
+	// 						'de': 'rolle',
+	// 						'en-GB': 'role',
+	// 						'en-US': 'role',
+	// 					})
+	// 					.setDescription('The role that should be an activityrole')
+	// 					.setDescriptionLocalizations({
+	// 						'de': 'Die Rolle, die als Aktivitätsrolle zugewiesen werden soll',
+	// 						'en-GB': 'The role that should be an activityrole',
+	// 						'en-US': 'The role that should be an activityrole',
+	// 					})
+	// 					.setRequired(true)
+	// 			)
+	// 			.addIntegerOption(option =>
+	// 				option.setName('rank')
+	// 					.setNameLocalizations({
+	// 						'de': 'rang',
+	// 						'en-GB': 'rank',
+	// 						'en-US': 'rank',
+	// 					})
+	// 					.setDescription('The required rank for getting the role')
+	// 					.setDescriptionLocalizations({
+	// 						'de': 'Der erforderliche Rang für die Rolle',
+	// 						'en-GB': 'The required rank for getting the role',
+	// 						'en-US': 'The required rank for getting the role',
+	// 					})
+	// 					.setRequired(false)
+	// 					.setMinValue(1)
+	// 			)
+	// 			.addIntegerOption(option =>
+	// 				option.setName('percentage')
+	// 					.setNameLocalizations({
+	// 						'de': 'prozent',
+	// 						'en-GB': 'percentage',
+	// 						'en-US': 'percentage',
+	// 					})
+	// 					.setDescription('The required topx% rank for getting the role')
+	// 					.setDescriptionLocalizations({
+	// 						'de': 'Der erforderliche topx% Rang für die Rolle',
+	// 						'en-GB': 'The required topx% rank for getting the role',
+	// 						'en-US': 'The required topx% rank for getting the role',
+	// 					})
+	// 					.setRequired(false)
+	// 					.setMinValue(1)
+	// 					.setMaxValue(99)
+	// 			)
+	// 			.addIntegerOption(option =>
+	// 				option.setName('points')
+	// 					.setNameLocalizations({
+	// 						'de': 'punkte',
+	// 						'en-GB': 'points',
+	// 						'en-US': 'points',
+	// 					})
+	// 					.setDescription('The required points for getting the role')
+	// 					.setDescriptionLocalizations({
+	// 						'de': 'Die erforderlichen Punkte für die Rolle',
+	// 						'en-GB': 'The required points for getting the role',
+	// 						'en-US': 'The required points for getting the role',
+	// 					})
+	// 					.setRequired(false)
+	// 					.setMinValue(1)
+	// 			)
+	// 	)
+	// 	.addSubcommand(subcommand =>
+	// 		subcommand.setName('remove')
+	// 			.setNameLocalizations({
+	// 				'de': 'entfernen',
+	// 				'en-GB': 'remove',
+	// 				'en-US': 'remove',
+	// 			})
+	// 			.setDescription('Remove an existing activityrole')
+	// 			.setDescriptionLocalizations({
+	// 				'de': 'Entfernt eine bestehende Aktivitätsrolle',
+	// 				'en-GB': 'Remove an existing activityrole',
+	// 				'en-US': 'Remove an existing activityrole',
+	// 			})
+	// 			.addRoleOption(option =>
+	// 				option.setName('role')
+	// 					.setNameLocalizations({
+	// 						'de': 'rolle',
+	// 						'en-GB': 'role',
+	// 						'en-US': 'role',
+	// 					})
+	// 					.setDescription('The role that should no longer be an activityrole')
+	// 					.setDescriptionLocalizations({
+	// 						'de': 'Die Rolle, die nicht mehr als Aktivitätsrolle zugewiesen werden soll',
+	// 						'en-GB': 'The role that should no longer be an activityrole',
+	// 						'en-US': 'The role that should no longer be an activityrole',
+	// 					})
+	// 					.setRequired(true)
+	// 			)
+	// 	)
+	// 	.addSubcommand(subcommand =>
+	// 		subcommand.setName('list')
+	// 			.setNameLocalizations({
+	// 				'de': 'liste',
+	// 				'en-GB': 'list',
+	// 				'en-US': 'list',
+	// 			})
+	// 			.setDescription('Show which activityroles are set up')
+	// 			.setDescriptionLocalizations({
+	// 				'de': 'Zeigt welche Aktivitätsrollen existieren sind',
+	// 				'en-GB': 'Show which activityroles are set up',
+	// 				'en-US': 'Show which activityroles are set up',
+	// 			})
+	// 	),
 	async execute(msg, args, interaction) {
 		try {
 			await interaction.deferReply({ ephemeral: true });
