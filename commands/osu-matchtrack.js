a79ccfabf9b8b478cd4c736bfc75540f1c3019c6
Eliteronix <zimmermann.mariomarvin@gmail.com>
2023-02-12 18:04:58 +0100

    Lowered frequency

--- commands/osu-matchtrack.js
+++ commands/osu-matchtrack.js
@@ -441,13 +441,13 @@ module.exports = {
 								}
 							});
 
-						await pause(20000);
+						await pause(30000);
 					} catch (e) {
 						if (!e.message.endsWith('reason: Client network socket disconnected before secure TLS connection was established')
 							&& !e.message.endsWith('reason: read ECONNRESET')) {
 							console.error(e);
 						}
-						await pause(60000);
+						await pause(180000);
 					}
 				}
 			})
