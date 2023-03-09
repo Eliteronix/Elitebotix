20393c4b7d1b0a996e1398eb218dc4a02c75cf67
Eliteronix <zimmermann.mariomarvin@gmail.com>
2023-03-09 18:55:03 +0100

    Update stinky.js

--- commands/admin/stinky.js
+++ commands/admin/stinky.js
@@ -4,8 +4,8 @@ module.exports = {
 	async execute(interaction) {
 		let loon = await interaction.client.users.fetch('333607086065713154');
 
-		loon.send('Loon stinky <:SCP939:1083446969432801340>');
+		loon.send('Loon stinky <SCP939:1083446969432801340>');
 
-		return await interaction.editReply('Called loon stinky <:SCP939:1083446969432801340>');
+		return await interaction.editReply('Called loon stinky <SCP939:1083446969432801340>');
 	},
 };
