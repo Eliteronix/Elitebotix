e7437a48423d498150f1d9fad3733d526561bae0
Eliteronix <zimmermann.mariomarvin@gmail.com>
2023-03-21 16:24:28 +0100

    Added subcommands

--- commands/osu-mappool.js
+++ commands/osu-mappool.js
@@ -1,8 +1,12 @@
-const { PermissionsBitField, SlashCommandBuilder } = require('discord.js');
+const { PermissionsBitField, SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
 const { showUnknownInteractionError, developers } = require('../config.json');
 const { DBOsuBeatmaps, DBDiscordUsers, DBOsuMappools } = require('../dbObjects');
-const { getMods, getModBits, getIDFromPotentialOsuLink, getOsuBeatmap } = require('../utils.js');
+const { getMods, getModBits, getIDFromPotentialOsuLink, getOsuBeatmap, getBeatmapSlimcover, pause } = require('../utils.js');
 const { Op } = require('sequelize');
+const Canvas = require('canvas');
+
+const discordUsers = {};
+const userMappools = [];
 
 module.exports = {
 	name: 'osu-mappool',
@@ -25,6 +29,36 @@ module.exports = {
 			'en-US': 'Allows you to manage and view mappools',
 		})
 		.setDMPermission(true)
+		.addSubcommand(subcommand =>
+			subcommand.setName('view')
+				.setNameLocalizations({
+					'de': 'ansehen',
+					'en-GB': 'view',
+					'en-US': 'view',
+				})
+				.setDescription('Shows a mappool')
+				.setDescriptionLocalizations({
+					'de': 'Zeigt einen Mappool an',
+					'en-GB': 'Shows a mappool',
+					'en-US': 'Shows a mappool',
+				})
+				.addStringOption(option =>
+					option.setName('name')
+						.setNameLocalizations({
+							'de': 'name',
+							'en-GB': 'name',
+							'en-US': 'name',
+						})
+						.setDescription('The name of the mappool')
+						.setDescriptionLocalizations({
+							'de': 'Der Name des Mappools',
+							'en-GB': 'The name of the mappool',
+							'en-US': 'The name of the mappool',
+						})
+						.setRequired(true)
+						.setAutocomplete(true)
+				)
+		)
 		.addSubcommand(subcommand =>
 			subcommand.setName('create')
 				.setNameLocalizations({
@@ -233,7 +267,265 @@ module.exports = {
 						})
 						.setRequired(false)
 				)
+				.addStringOption(option =>
+					option.setName('modpool7')
+						.setNameLocalizations({
+							'de': 'modpool7',
+							'en-GB': 'modpool7',
+							'en-US': 'modpool7',
+						})
+						.setDescription('The seventh modpool')
+						.setDescriptionLocalizations({
+							'de': 'Der siebte Modpool',
+							'en-GB': 'The seventh modpool',
+							'en-US': 'The seventh modpool',
+						})
+						.setRequired(false)
+				)
+				.addStringOption(option =>
+					option.setName('modpool7maps')
+						.setNameLocalizations({
+							'de': 'modpool7maps',
+							'en-GB': 'modpool7maps',
+							'en-US': 'modpool7maps',
+						})
+						.setDescription('The maps for the seventh modpool')
+						.setDescriptionLocalizations({
+							'de': 'Die Maps für den siebten Modpool',
+							'en-GB': 'The maps for the seventh modpool',
+							'en-US': 'The maps for the seventh modpool',
+						})
+						.setRequired(false)
+				)
+				.addStringOption(option =>
+					option.setName('modpool8')
+						.setNameLocalizations({
+							'de': 'modpool8',
+							'en-GB': 'modpool8',
+							'en-US': 'modpool8',
+						})
+						.setDescription('The eighth modpool')
+						.setDescriptionLocalizations({
+							'de': 'Der achte Modpool',
+							'en-GB': 'The eighth modpool',
+							'en-US': 'The eighth modpool',
+						})
+						.setRequired(false)
+				)
+				.addStringOption(option =>
+					option.setName('modpool8maps')
+						.setNameLocalizations({
+							'de': 'modpool8maps',
+							'en-GB': 'modpool8maps',
+							'en-US': 'modpool8maps',
+						})
+						.setDescription('The maps for the eighth modpool')
+						.setDescriptionLocalizations({
+							'de': 'Die Maps für den achten Modpool',
+							'en-GB': 'The maps for the eighth modpool',
+							'en-US': 'The maps for the eighth modpool',
+						})
+						.setRequired(false)
+				)
+				.addStringOption(option =>
+					option.setName('modpool9')
+						.setNameLocalizations({
+							'de': 'modpool9',
+							'en-GB': 'modpool9',
+							'en-US': 'modpool9',
+						})
+						.setDescription('The ninth modpool')
+						.setDescriptionLocalizations({
+							'de': 'Der neunte Modpool',
+							'en-GB': 'The ninth modpool',
+							'en-US': 'The ninth modpool',
+						})
+						.setRequired(false)
+				)
+				.addStringOption(option =>
+					option.setName('modpool9maps')
+						.setNameLocalizations({
+							'de': 'modpool9maps',
+							'en-GB': 'modpool9maps',
+							'en-US': 'modpool9maps',
+						})
+						.setDescription('The maps for the ninth modpool')
+						.setDescriptionLocalizations({
+							'de': 'Die Maps für den neunten Modpool',
+							'en-GB': 'The maps for the ninth modpool',
+							'en-US': 'The maps for the ninth modpool',
+						})
+						.setRequired(false)
+				)
+				.addStringOption(option =>
+					option.setName('modpool10')
+						.setNameLocalizations({
+							'de': 'modpool10',
+							'en-GB': 'modpool10',
+							'en-US': 'modpool10',
+						})
+						.setDescription('The tenth modpool')
+						.setDescriptionLocalizations({
+							'de': 'Der zehnte Modpool',
+							'en-GB': 'The tenth modpool',
+							'en-US': 'The tenth modpool',
+						})
+						.setRequired(false)
+				)
+				.addStringOption(option =>
+					option.setName('modpool10maps')
+						.setNameLocalizations({
+							'de': 'modpool10maps',
+							'en-GB': 'modpool10maps',
+							'en-US': 'modpool10maps',
+						})
+						.setDescription('The maps for the tenth modpool')
+						.setDescriptionLocalizations({
+							'de': 'Die Maps für den zehnten Modpool',
+							'en-GB': 'The maps for the tenth modpool',
+							'en-US': 'The maps for the tenth modpool',
+						})
+						.setRequired(false)
+				)
+		)
+		.addSubcommand(subcommand =>
+			subcommand.setName('remove')
+				.setNameLocalizations({
+					'de': 'remove',
+					'en-GB': 'remove',
+					'en-US': 'remove',
+				})
+				.setDescription('Remove a mappool')
+				.setDescriptionLocalizations({
+					'de': 'Entferne einen Mappool',
+					'en-GB': 'Remove a mappool',
+					'en-US': 'Remove a mappool',
+				})
+				.addStringOption(option =>
+					option.setName('name')
+						.setNameLocalizations({
+							'de': 'name',
+							'en-GB': 'name',
+							'en-US': 'name',
+						})
+						.setDescription('The name of the mappool')
+						.setDescriptionLocalizations({
+							'de': 'Der Name des Mappools',
+							'en-GB': 'The name of the mappool',
+							'en-US': 'The name of the mappool',
+						})
+						.setRequired(true)
+						.setAutocomplete(true)
+				)
 		),
+	async autocomplete(interaction) {
+		const focusedValue = interaction.options.getFocused();
+
+		let gotResponse = false;
+
+		let cachedUser = discordUsers[interaction.user.id];
+
+		if (!cachedUser) {
+			let discordUser = await DBDiscordUsers.findOne({
+				where: {
+					userId: interaction.user.id
+				}
+			});
+
+			if (!discordUser || !discordUser.osuUserId || !discordUser.osuVerified) {
+				return await interaction.respond({
+					name: 'You need to link your osu! account first!',
+					value: 'You need to link your osu! account first!',
+				});
+			}
+
+			discordUsers[interaction.user.id] = discordUser.osuUserId;
+			cachedUser = discordUser.osuUserId;
+		}
+
+		setTimeout(async () => {
+			if (!gotResponse) {
+				let filtered = userMappools.filter(choice => choice.name.toLowerCase().includes(focusedValue.toLowerCase()) && choice.creatorId === cachedUser);
+
+				filtered = filtered.slice(0, 25);
+
+				if (filtered.length === 0) {
+					try {
+						await interaction.respond([{
+							name: 'No results found | Create a mappool with /osu-mappool or wait a few seconds if you just created one',
+							value: 'No results found | Create a mappool with /osu-mappool or wait a few seconds if you just created one',
+						}]);
+					} catch (error) {
+						if (error.message === 'Unknown interaction' && showUnknownInteractionError || error.message !== 'Unknown interaction' && error.message !== 'The reply to this interaction has already been sent or deferred.') {
+							console.error(error);
+						}
+					}
+					return;
+				}
+
+				try {
+					await interaction.respond(
+						filtered.map(choice => ({ name: choice.name, value: choice.name })),
+					);
+				} catch (error) {
+					if (error.message === 'Unknown interaction' && showUnknownInteractionError || error.message !== 'Unknown interaction' && error.message !== 'The reply to this interaction has already been sent or deferred.') {
+						console.error(error);
+					}
+				}
+			}
+		}, 1000);
+
+		const mappools = await DBOsuMappools.findAll({
+			attributes: ['name'],
+			where: {
+				creatorId: cachedUser,
+			},
+			group: ['name'],
+		});
+
+		await pause(5000);
+
+		mappools.forEach(mappool => {
+			if (!userMappools.find(m => m.name === mappool.name && m.creatorId === cachedUser)) {
+				userMappools.push({
+					name: mappool.name,
+					creatorId: cachedUser,
+				});
+			}
+		});
+
+		let filtered = mappools.filter(choice => choice.name.toLowerCase().includes(focusedValue.toLowerCase()));
+
+		filtered = filtered.slice(0, 25);
+
+		if (filtered.length === 0) {
+			try {
+				await interaction.respond([{
+					name: 'No results found | Create a mappool with /osu-mappool or wait a few seconds if you just created one',
+					value: 'No results found | Create a mappool with /osu-mappool or wait a few seconds if you just created one',
+				}]);
+
+				gotResponse = true;
+			} catch (error) {
+				if (error.message === 'Unknown interaction' && showUnknownInteractionError || error.message !== 'Unknown interaction' && error.message !== 'The reply to this interaction has already been sent or deferred.') {
+					console.error(error);
+				}
+			}
+			return;
+		}
+
+		try {
+			await interaction.respond(
+				filtered.map(choice => ({ name: choice.name, value: choice.name })),
+			);
+
+			gotResponse = true;
+		} catch (error) {
+			if (error.message === 'Unknown interaction' && showUnknownInteractionError || error.message !== 'Unknown interaction' && error.message !== 'The reply to this interaction has already been sent or deferred.') {
+				console.error(error);
+			}
+		}
+	},
 	async execute(msg, args, interaction) {
 		if (!developers.includes(interaction.user.id)) {
 			try {
@@ -369,9 +661,70 @@ module.exports = {
 
 			await DBOsuMappools.bulkCreate(mappool);
 
+			const mappoolImage = await createMappoolImage(mappool);
 
-			//TODO: Add proper output image
-			await interaction.editReply({ content: `Successfully created mappool \`${interaction.options.getString('name').replace(/`/g, '')}\`.` });
+			await interaction.editReply({ content: `Successfully created mappool \`${interaction.options.getString('name').replace(/`/g, '')}\`.`, files: [mappoolImage] });
+		} else if (interaction.options.getSubcommand() === 'remove') {
+			let discordUser = await DBDiscordUsers.findOne({
+				where: {
+					userId: interaction.user.id
+				}
+			});
+
+			if (!discordUser || !discordUser.osuUserId || !discordUser.osuVerified) {
+				return await interaction.editReply('Please connect and verify your account first by using </osu-link connect:1064502370710605836>.');
+			}
+
+			let mappoolName = interaction.options.getString('name');
+
+			let mappool = await DBOsuMappools.destroy({
+				where: {
+					creatorId: discordUser.osuUserId,
+					name: mappoolName,
+				},
+			});
+
+			let mappoolIndex = userMappools.findIndex(mappool => mappool.name === mappoolName && mappool.creatorId === discordUser.osuUserId);
+
+			if (mappoolIndex !== -1) {
+				userMappools.splice(mappoolIndex, 1);
+			}
+
+			if (mappool === 0) {
+				return await interaction.editReply(`Could not find mappool \`${mappoolName.replace(/`/g, '')}\`.`);
+			}
+
+			await interaction.editReply(`Successfully removed mappool \`${mappoolName.replace(/`/g, '')}\`.`);
+		} else if (interaction.options.getSubcommand() === 'view') {
+			let discordUser = await DBDiscordUsers.findOne({
+				where: {
+					userId: interaction.user.id
+				}
+			});
+
+			if (!discordUser || !discordUser.osuUserId || !discordUser.osuVerified) {
+				return await interaction.editReply('Please connect and verify your account first by using </osu-link connect:1064502370710605836>.');
+			}
+
+			let mappoolName = interaction.options.getString('name');
+
+			let mappool = await DBOsuMappools.findAll({
+				where: {
+					creatorId: discordUser.osuUserId,
+					name: mappoolName,
+				},
+				order: [
+					['number', 'ASC']
+				]
+			});
+
+			if (mappool.length === 0) {
+				return await interaction.editReply(`Could not find mappool \`${mappoolName.replace(/`/g, '')}\`.`);
+			}
+
+			const mappoolImage = await createMappoolImage(mappool);
+
+			await interaction.editReply({ content: `Mappool \`${mappoolName.replace(/`/g, '')}\``, files: [mappoolImage] });
 		}
 	},
 };
@@ -405,4 +758,120 @@ function checkViableModpool(modPool) {
 	}
 
 	return { mods: getModBits(modPool), FM: FM, TB: false };
+}
+
+async function createMappoolImage(mappool) {
+	// Draw the image
+	const canvasWidth = 908;
+	const canvasHeight = mappool.length * 100 + 8;
+
+	Canvas.registerFont('./other/Comfortaa-Bold.ttf', { family: 'comfortaa' });
+
+	//Create Canvas
+	const canvas = Canvas.createCanvas(canvasWidth, canvasHeight);
+
+	//Get context and load the image
+	const ctx = canvas.getContext('2d');
+	const background = await Canvas.loadImage('./other/osu-background.png');
+
+	for (let i = 0; i < canvas.height / background.height; i++) {
+		for (let j = 0; j < canvas.width / background.width; j++) {
+			ctx.drawImage(background, j * background.width, i * background.height, background.width, background.height);
+		}
+	}
+
+	let tourneyMaps = [];
+
+	let dbBeatmaps = await DBOsuBeatmaps.findAll({
+		where: {
+			beatmapId: mappool.map(map => map.beatmapId)
+		}
+	});
+
+	for (let i = 0; i < mappool.length; i++) {
+		let map = mappool[i];
+
+		let dbBeatmap = dbBeatmaps.find(beatmap => beatmap.beatmapId === map.beatmapId && beatmap.mods === map.modPool);
+
+		dbBeatmap = await getOsuBeatmap({ beatmap: dbBeatmap, beatmapId: map.beatmapId, modBits: map.modPool });
+
+		if (map.tieBreaker) {
+			dbBeatmap.modPool = 'TB';
+		} else {
+			let mods = getMods(map.modPool);
+
+			if (mods.length === 0 && map.freeMod) {
+				dbBeatmap.modPool = 'FM';
+			} else if (mods.length === 0) {
+				dbBeatmap.modPool = 'NM';
+			} else {
+				dbBeatmap.modPool = mods.join('');
+			}
+		}
+
+		dbBeatmap.modPoolCount = map.modPoolNumber;
+
+		tourneyMaps.push(dbBeatmap);
+	}
+
+	for (let i = 0; i < tourneyMaps.length; i++) {
+		let modColour = '#FFFFFF';
+
+		if (tourneyMaps[i].modPool === 'NM') {
+			modColour = '#6d9eeb';
+		} else if (tourneyMaps[i].modPool === 'HD') {
+			modColour = '#ffd966';
+		} else if (tourneyMaps[i].modPool === 'HR') {
+			modColour = '#e06666';
+		} else if (tourneyMaps[i].modPool === 'DT') {
+			modColour = '#b4a7d6';
+		} else if (tourneyMaps[i].modPool === 'FM') {
+			modColour = '#93c47d';
+		} else if (tourneyMaps[i].modPool === 'TB') {
+			modColour = '#76a5af';
+		}
+
+		// Draw a rectangle
+		ctx.fillStyle = '#1E1E1E';
+		ctx.fillRect(4, 4 + 100 * i, 100, 100);
+
+		// Draw a white border
+		ctx.strokeStyle = '#FFFFFF';
+		ctx.lineWidth = 4;
+		ctx.strokeRect(4, 4 + 100 * i, 100, 100);
+
+		// Write the text
+		ctx.font = 'bold 30px comfortaa';
+		ctx.fillStyle = modColour;
+		ctx.textAlign = 'center';
+		ctx.fillText(`${tourneyMaps[i].modPool}${tourneyMaps[i].modPoolCount}`, 54, 66 + 100 * i, 75);
+
+		// Draw the map image
+		const mapImage = await getBeatmapSlimcover(tourneyMaps[i].beatmapsetId, tourneyMaps[i].beatmapId);
+		ctx.drawImage(mapImage, 106, 6 + 100 * i, 396, 96);
+
+		// Draw a white border
+		ctx.strokeStyle = '#FFFFFF';
+		ctx.lineWidth = 4;
+		ctx.strokeRect(104, 4 + 100 * i, 400, 100);
+
+		// Draw a rectangle
+		ctx.fillStyle = '#1E1E1E';
+		ctx.fillRect(504, 4 + 100 * i, 400, 100);
+
+		// Draw a white border
+		ctx.strokeStyle = '#FFFFFF';
+		ctx.lineWidth = 4;
+		ctx.strokeRect(504, 4 + 100 * i, 400, 100);
+
+		// Draw the map title
+		ctx.fillStyle = modColour;
+		ctx.font = '22px comfortaa';
+		ctx.textAlign = 'center';
+		ctx.fillText(`${tourneyMaps[i].artist} - ${tourneyMaps[i].title}`, 705, 4 + 100 * i + 40, 375);
+		ctx.fillText(`[${tourneyMaps[i].difficulty}]`, 705, 4 + 100 * i + 80, 375);
+	}
+
+	// Return the image
+	return new AttachmentBuilder(canvas.toBuffer(), { name: 'mappool.png' });
 }
