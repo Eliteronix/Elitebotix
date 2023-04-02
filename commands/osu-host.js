484dff201117f7b8ea810374db1567b2fd7b9554
Eliteronix <zimmermann.mariomarvin@gmail.com>
2023-04-03 01:03:39 +0200

    Added new option to osu-host

--- commands/osu-host.js
+++ commands/osu-host.js
@@ -1,10 +1,12 @@
 const { showUnknownInteractionError } = require('../config.json');
 const { PermissionsBitField, SlashCommandBuilder } = require('discord.js');
-const { getOsuPlayerName, logDatabaseQueries, getMods, multiToBanchoScore, getOsuBeatmap, getUserDuelStarRating } = require('../utils');
-const { DBOsuMultiScores } = require('../dbObjects');
+const { getOsuPlayerName, logDatabaseQueries, getMods, multiToBanchoScore, getOsuBeatmap, getUserDuelStarRating, getGameModeName } = require('../utils');
+const { DBOsuMultiScores, DBOsuBeatmaps, DBDiscordUsers } = require('../dbObjects');
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const Discord = require('discord.js');
 const ObjectsToCsv = require('objects-to-csv');
+const { Op } = require('sequelize');
+const osu = require('node-osu');
 
 module.exports = {
 	name: 'osu-host',
@@ -120,38 +122,37 @@ module.exports = {
 						.setRequired(true)
 				)
 		)
-	// 	.addSubcommand(subcommand =>
-	// 		subcommand
-	// 			.setName('ppwithtournamenttopplays')
-	// 			.setNameLocalizations({
-	// 				'de': 'ppmitturniertopplays',
-	// 				'en-GB': 'ppwithtournamenttopplays',
-	// 				'en-US': 'ppwithtournamenttopplays',
-	// 			})
-	// 			.setDescription('Provides the pp with bancho and tournament top plays of the provided tournament players')
-	// 			.setDescriptionLocalizations({
-	// 				'de': 'Liefert die pp mit Bancho und Turnier Top Plays der bereitgestellten Turnierspieler',
-	// 				'en-GB': 'Provides the pp with bancho and tournament top plays of the provided tournament players',
-	// 				'en-US': 'Provides the pp with bancho and tournament top plays of the provided tournament players',
-	// 			})
-	// 			.addAttachmentOption(option =>
-	// 				option
-	// 					.setName('file')
-	// 					.setNameLocalizations({
-	// 						'de': 'datei',
-	// 						'en-GB': 'file',
-	// 						'en-US': 'file',
-	// 					})
-	// 					.setDescription('The .txt file containing the tournament players, one player id per line')
-	// 					.setDescriptionLocalizations({
-	// 						'de': 'Die .txt Datei, die die Turnierspieler enthält, eine Spieler ID pro Zeile',
-	// 						'en-GB': 'The .txt file containing the tournament players, one player id per line',
-	// 						'en-US': 'The .txt file containing the tournament players, one player id per line',
-	// 					})
-	// 					.setRequired(true)
-	// 			)
-	// )
-	,
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('ppwithtournamenttopplays')
+				.setNameLocalizations({
+					'de': 'ppmitturniertopplays',
+					'en-GB': 'ppwithtournamenttopplays',
+					'en-US': 'ppwithtournamenttopplays',
+				})
+				.setDescription('Provides the pp with bancho and tournament top plays of the provided tournament players')
+				.setDescriptionLocalizations({
+					'de': 'Liefert die pp mit Bancho und Turnier Top Plays der bereitgestellten Turnierspieler',
+					'en-GB': 'Provides the pp with bancho and tournament top plays of the provided tournament players',
+					'en-US': 'Provides the pp with bancho and tournament top plays of the provided tournament players',
+				})
+				.addAttachmentOption(option =>
+					option
+						.setName('file')
+						.setNameLocalizations({
+							'de': 'datei',
+							'en-GB': 'file',
+							'en-US': 'file',
+						})
+						.setDescription('The .txt file containing the tournament players, one player id per line')
+						.setDescriptionLocalizations({
+							'de': 'Die .txt Datei, die die Turnierspieler enthält, eine Spieler ID pro Zeile',
+							'en-GB': 'The .txt file containing the tournament players, one player id per line',
+							'en-US': 'The .txt file containing the tournament players, one player id per line',
+						})
+						.setRequired(true)
+				)
+		),
 	// eslint-disable-next-line no-unused-vars
 	async execute(msg, args, interaction, additionalObjects) {
 		try {
@@ -503,6 +504,285 @@ module.exports = {
 			if (interaction.client.hostCommands.includes(randomString)) {
 				interaction.client.hostCommands.splice(interaction.client.hostCommands.indexOf(randomString), 1);
 			}
+		} else if (interaction.options.getSubcommand() === 'ppwithtournamenttopplays') {
+			let attachedFile = interaction.options.getAttachment('file');
+
+			if (!attachedFile.contentType.startsWith('text/plain')) {
+				return await interaction.editReply({ content: 'The attached file is not a .txt file.' });
+			}
+
+			// fetch the file
+			let file = await fetch(attachedFile.url);
+
+			// parse the file
+			file = await file.text();
+
+			// split the file into an array
+			file = file.split('\n');
+
+			// remove empty lines
+			file = file.filter(line => line !== '');
+
+			// remove duplicates
+			file = [...new Set(file)];
+
+			// check if the file contains only numbers
+			if (file.some(line => isNaN(line))) {
+				return await interaction.editReply({ content: 'The attached file contains invalid player ids. Be sure to only provide playerIds; one for each line.' });
+			}
+
+			await interaction.editReply({ content: 'A proper file has been provided. Processing may take a while, depending on how many scores haven\'t been calculated since they have been set or the last pp update...' });
+
+			let processingMessage = await interaction.channel.send('Processing...');
+
+			let randomString = Math.random().toString(36);
+
+			interaction.client.hostCommands.push(randomString);
+
+			let csvData = [];
+
+			let lastUpdate = new Date();
+
+			// eslint-disable-next-line no-undef
+			const osuApi = new osu.Api(process.env.OSUTOKENV1, {
+				// baseUrl: sets the base api url (default: https://osu.ppy.sh/api)
+				notFoundAsError: true, // Throw an error on not found instead of returning nothing. (default: true)
+				completeScores: false, // When fetching scores also fetch the beatmap they are for (Allows getting accuracy) (default: false)
+				parseNumeric: false // Parse numeric values into numbers/floats, excluding ids
+			});
+
+			for (let i = 0; i < file.length; i++) {
+				let osuUserId = file[i].trim();
+
+				if (new Date() - lastUpdate > 15000) {
+					let osuName = await getOsuPlayerName(osuUserId);
+					processingMessage.edit(`Processing ${osuName} (Account ${i + 1}/${file.length})...`);
+					lastUpdate = new Date();
+				}
+
+				let scores = [];
+
+				let user = await osuApi.getUser({ u: osuUserId, m: 0 });
+
+				let banchoTopPlays = await osuApi.getUserBest({ u: osuUserId, m: 0, limit: 100 });
+
+				let topPlayData = await getTournamentTopPlayData(osuUserId, 0);
+
+				let tournamentTopPlays = topPlayData.scores;
+
+				scores = banchoTopPlays.concat(tournamentTopPlays);
+
+				scores.sort((a, b) => {
+					return parseFloat(b.pp) - parseFloat(a.pp);
+				});
+
+				// Remove duplicates
+				for (let i = 0; i < scores.length; i++) {
+					for (let j = i + 1; j < scores.length; j++) {
+						if (scores[i].beatmapId == scores[j].beatmapId) {
+							scores.splice(j, 1);
+							j--;
+						}
+					}
+				}
+
+				// Calculate total pp
+				let banchoTotalPP = parseFloat(user.pp.raw);
+
+				// Calculate the part of the pp that is coming from the bancho top 100
+				let banchoTopPlaysPP = 0;
+
+				for (let i = 0; i < banchoTopPlays.length; i++) {
+					banchoTopPlaysPP += parseFloat(banchoTopPlays[i].pp) * Math.pow(0.95, (i));
+				}
+
+				let additionalPP = banchoTotalPP - banchoTopPlaysPP;
+
+				// Calculate the pp with the new scores
+				let newScoresPP = 0;
+
+				for (let i = 0; i < scores.length && i < 100; i++) {
+					newScoresPP += parseFloat(scores[i].pp) * Math.pow(0.95, (i));
+				}
+
+				logDatabaseQueries(4, 'commands/osu-host.js DBDiscordUsers');
+				let discordUsers = await DBDiscordUsers.findAll({
+					attributes: ['osuPP', 'osuRank'],
+				});
+
+				let totalRankedPP = newScoresPP + additionalPP;
+
+				//Find the closest users to the PP values
+				let closestRankedPPUser = discordUsers[0];
+				for (let i = 0; i < discordUsers.length; i++) {
+					let currentDiscordUserPP = discordUsers[i].osuPP;
+					let closestRankedPP = closestRankedPPUser.osuPP;
+
+					if (Math.abs(currentDiscordUserPP - totalRankedPP) < Math.abs(closestRankedPP - totalRankedPP)) {
+						closestRankedPPUser = discordUsers[i];
+					}
+				}
+
+				csvData.push({
+					osuUserId: user.id,
+					banchoPP: banchoTotalPP,
+					banchoRank: user.pp.rank,
+					adaptedPP: totalRankedPP,
+					adaptedRank: closestRankedPPUser.osuRank,
+				});
+			}
+
+			processingMessage.delete();
+
+			let data = [];
+			for (let i = 0; i < csvData.length; i++) {
+				data.push(csvData[i]);
+
+				if (i % 10000 === 0 && i > 0 || csvData.length - 1 === i) {
+					let csv = new ObjectsToCsv(data);
+					csv = await csv.toString();
+					// eslint-disable-next-line no-undef
+					const buffer = Buffer.from(csv);
+					//Create as an attachment
+					const attachment = new Discord.AttachmentBuilder(buffer, { name: 'tournamentpp.csv' });
+
+					await interaction.channel.send({ content: 'PP with tournament top plays', files: [attachment] });
+					data = [];
+				}
+			}
+
+			if (interaction.client.hostCommands.includes(randomString)) {
+				interaction.client.hostCommands.splice(interaction.client.hostCommands.indexOf(randomString), 1);
+			}
 		}
 	},
 };
+
+async function getTournamentTopPlayData(osuUserId, mode) {
+	let modeName = getGameModeName(mode);
+	modeName = modeName.substring(0, 1).toUpperCase() + modeName.substring(1);
+
+	if (modeName === 'Catch') {
+		modeName = 'Catch the Beat';
+	}
+
+	let where = {
+		osuUserId: osuUserId,
+		mode: modeName,
+		tourneyMatch: true,
+		score: {
+			[Op.gte]: 10000,
+		},
+		scoringType: 'Score v2',
+	};
+
+	//Get all scores from tournaments
+	logDatabaseQueries(4, 'commands/osu-host.js DBOsuMultiScores 1');
+	let multiScores = await DBOsuMultiScores.findAll({
+		attributes: [
+			'id',
+			'score',
+			'gameRawMods',
+			'rawMods',
+			'teamType',
+			'pp',
+			'beatmapId',
+			'createdAt',
+			'gameStartDate',
+			'osuUserId',
+			'count50',
+			'count100',
+			'count300',
+			'countGeki',
+			'countKatu',
+			'countMiss',
+			'maxCombo',
+			'perfect',
+			'matchName',
+			'mode',
+		],
+		where: where
+	});
+
+	for (let i = 0; i < multiScores.length; i++) {
+		if (parseInt(multiScores[i].score) <= 10000 || multiScores[i].teamType === 'Tag Team vs' || multiScores[i].teamType === 'Tag Co-op') {
+			multiScores.splice(i, 1);
+			i--;
+		}
+	}
+
+	//Translate the scores to bancho scores
+	for (let i = 0; i < multiScores.length; i++) {
+		if (parseInt(multiScores[i].gameRawMods) % 2 === 1) {
+			multiScores[i].gameRawMods = parseInt(multiScores[i].gameRawMods) - 1;
+		}
+		if (parseInt(multiScores[i].rawMods) % 2 === 1) {
+			multiScores[i].rawMods = parseInt(multiScores[i].rawMods) - 1;
+		}
+		multiScores[i] = await multiToBanchoScore(multiScores[i]);
+
+		if (!multiScores[i].pp || parseFloat(multiScores[i].pp) > 2000 || !parseFloat(multiScores[i].pp)) {
+			multiScores.splice(i, 1);
+			i--;
+			continue;
+		}
+	}
+
+	//Sort scores by pp
+	multiScores.sort((a, b) => parseFloat(b.pp) - parseFloat(a.pp));
+
+	//Remove duplicates by beatmapId
+	for (let i = 0; i < multiScores.length; i++) {
+		for (let j = i + 1; j < multiScores.length; j++) {
+			if (multiScores[i].beatmapId === multiScores[j].beatmapId) {
+				multiScores.splice(j, 1);
+				j--;
+			}
+		}
+	}
+
+	logDatabaseQueries(4, 'commands/osu-host.js DBOsuBeatmaps 1');
+	let dbBeatmaps = await DBOsuBeatmaps.findAll({
+		attributes: [
+			'beatmapId',
+			'beatmapsetId',
+			'approvalStatus',
+			'mods',
+			'updatedAt',
+			'starRating',
+			'maxCombo',
+			'mode',
+		],
+		where: {
+			beatmapId: {
+				[Op.in]: multiScores.map(score => score.beatmapId)
+			},
+			mods: 0
+		}
+	});
+
+	for (let i = 0; i < multiScores.length; i++) {
+		let dbBeatmap = dbBeatmaps.find(dbBeatmap => dbBeatmap.beatmapId === multiScores[i].beatmapId);
+
+		multiScores[i].beatmap = await getOsuBeatmap({ beatmapId: multiScores[i].beatmapId, beatmap: dbBeatmap });
+
+		if (!multiScores[i].beatmap || multiScores[i].beatmap.approvalStatus !== 'Approved' && multiScores[i].beatmap.approvalStatus !== 'Ranked') {
+			multiScores.splice(i, 1);
+			i--;
+			continue;
+		}
+	}
+
+	let data = {
+		scores: []
+	};
+
+	//Feed the scores into the array
+	for (let i = 0; i < multiScores.length && i < 100; i++) {
+		if (multiScores[i].pp) {
+			data.scores.push(multiScores[i]);
+		}
+	}
+
+	return data;
+}
