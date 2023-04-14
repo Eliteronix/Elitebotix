265fe917ed62d46ad3b8ae4bd4c77478f6038e60
Eliteronix <zimmermann.mariomarvin@gmail.com>
2023-04-14 18:43:54 +0200

    Create remainingMaidbotMatches.js

---
+++ commands/admin/remainingMaidbotMatches.js
@@ -0,0 +1,21 @@
+const { DBOsuMultiScores } = require('../../dbObjects');
+const { logDatabaseQueries } = require('../../utils');
+const { Op } = require('sequelize');
+
+module.exports = {
+	name: 'remainingMaidbotMatches',
+	usage: 'None',
+	async execute(interaction) {
+		logDatabaseQueries(4, 'commands/admin/remainingMaidbotMatches.js DBOsuMultiScores');
+		let count = await DBOsuMultiScores.count({
+			where: {
+				matchName: {
+					[Op.startsWith]: 'o!mm'
+				},
+			},
+			group: ['matchId'],
+		});
+
+		return await interaction.editReply(`Remaining unverified maidbot matches: ${count.length}`);
+	},
+};
