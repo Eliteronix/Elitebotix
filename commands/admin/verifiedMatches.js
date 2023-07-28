13a6d4dc64876027f8c5902ad371faf9482ae237
Eliteronix <zimmermann.mariomarvin@gmail.com>
2023-07-28 19:28:55 +0200

    Added more info

--- commands/admin/verifiedMatches.js
+++ commands/admin/verifiedMatches.js
@@ -8,7 +8,7 @@ module.exports = {
 	usage: 'None',
 	async execute(interaction) {
 		const verifiedMatches = await DBOsuMultiScores.findAll({
-			attributes: ['matchId', 'osuUserId', 'score', 'beatmapId', 'gameRawMods', 'rawMods', 'matchName', 'mode', 'matchStartDate', 'gameStartDate'],
+			attributes: ['osuUserId', 'matchId', 'gameId', 'scoringType', 'score', 'beatmapId', 'gameRawMods', 'rawMods', 'matchName', 'mode', 'matchStartDate', 'gameStartDate', 'freeMod', 'forceMod', 'teamType', 'team'],
 			where: {
 				verifiedAt: {
 					[Op.not]: null,
@@ -16,32 +16,88 @@ module.exports = {
 				tourneyMatch: true,
 				warmup: {
 					[Op.not]: true,
-				}
+				},
+				[Op.and]: [
+					{
+						[Op.not]: {
+							matchName: {
+								[Op.like]: '%qual%'
+							},
+						},
+					},
+					{
+						[Op.not]: {
+							matchName: {
+								[Op.like]: '%qf-%'
+							},
+						},
+					},
+					{
+						[Op.not]: {
+							matchName: {
+								[Op.like]: '%o!mm%'
+							},
+						},
+					},
+					{
+						[Op.not]: {
+							matchName: {
+								[Op.like]: '%scrim%'
+							},
+						},
+					},
+					{
+						[Op.not]: {
+							matchName: {
+								[Op.like]: '%etx%'
+							},
+						},
+					},
+					{
+						[Op.not]: {
+							matchName: {
+								[Op.like]: '%tryout%'
+							},
+						},
+					},
+				]
 			},
 		});
+
+		// Remove entries with only one player per gameId
+		const gameIds = verifiedMatches.map(match => match.gameId);
+		const gameIdsUnique = [...new Set(gameIds)];
+		const gameIdsUniqueCount = gameIdsUnique.map(gameId => {
+			return {
+				gameId: gameId,
+				count: gameIds.filter(gameId2 => gameId2 === gameId).length,
+			};
+		});
+		const gameIdsUniqueCountFiltered = gameIdsUniqueCount.filter(gameId => gameId.count > 1);
+		const verifiedMatchesFiltered = verifiedMatches.filter(match => gameIdsUniqueCountFiltered.find(gameId => gameId.gameId === match.gameId));
 
 		const players = await DBDiscordUsers.findAll({
 			attributes: ['osuUserId', 'osuRank', 'osuBadges', 'osuDuelStarRating'],
 			where: {
 				osuUserId: {
-					[Op.in]: verifiedMatches.map(match => match.osuUserId),
+					[Op.in]: verifiedMatchesFiltered.map(match => match.osuUserId),
 				},
 			},
 		});
 
-		for (let i = 0; i < verifiedMatches.length; i++) {
-			const player = players.find(player => player.osuUserId === verifiedMatches[i].osuUserId);
-			verifiedMatches[i].dataValues.osuRank = player.osuRank;
-			verifiedMatches[i].dataValues.osuBadges = player.osuBadges;
-			verifiedMatches[i].dataValues.osuDuelStarRating = player.osuDuelStarRating;
+		for (let i = 0; i < verifiedMatchesFiltered.length; i++) {
+			const player = players.find(player => player.osuUserId === verifiedMatchesFiltered[i].osuUserId);
+			verifiedMatchesFiltered[i].dataValues.osuRank = player.osuRank;
+			verifiedMatchesFiltered[i].dataValues.osuBadges = player.osuBadges;
+			verifiedMatchesFiltered[i].dataValues.osuDuelStarRating = player.osuDuelStarRating;
 		}
 
 		let data = [];
 
-		for (let i = 0; i < verifiedMatches.length; i++) {
-			data.push(verifiedMatches[i].dataValues);
+		for (let i = 0; i < verifiedMatchesFiltered.length; i++) {
+			data.push(verifiedMatchesFiltered[i].dataValues);
 
-			if (i % 100000 === 0 && i > 0 || verifiedMatches.length - 1 === i) {
+			if (i % 100000 === 0 && i > 0 || verifiedMatchesFiltered.length - 1 === i) {
 				const developerUser = await interaction.client.users.cache.find(user => user.id === interaction.user.id);
 				let csv = new ObjectsToCsv(data);
 				csv = await csv.toString();
