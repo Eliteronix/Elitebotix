52677ac1db3a278c9b74c0f6b4f9e5646a1648ef
Marvin Zimmermann <64707347+Eliteronix@users.noreply.github.com>
2023-09-21 10:02:09 +0200

    Added error handling

--- commands/osu-duel.js
+++ commands/osu-duel.js
@@ -1327,10 +1327,15 @@ module.exports = {
 				let sentMessage = null;
 
 				try {
-					if (interaction.id) {
+					let now = new Date();
+					now.setUTCMinutes(now.getUTCMinutes() - 15);
+
+					if (interaction.id && interaction.createdAt > now) {
 						sentMessage = await interaction.editReply({ content: `The data is based on matches played using </osu-duel queue1v1:${interaction.client.slashCommandData.find(command => command.name === 'osu-duel').id}> and any other tournament matches.\nThe values are supposed to show a star rating where a player will get around 350k average score with Score v2.`, files: [leagueRatings] });
 					} else {
-						processingMessage.delete();
+						if (processingMessage) {
+							processingMessage.delete();
+						}
 						sentMessage = await interaction.channel.send({ content: `The data is based on matches played using </osu-duel queue1v1:${interaction.client.slashCommandData.find(command => command.name === 'osu-duel').id}> and any other tournament matches.\nThe values are supposed to show a star rating where a player will get around 350k average score with Score v2.`, files: [leagueRatings] });
 					}
 
