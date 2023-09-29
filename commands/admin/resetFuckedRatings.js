10dee764042bd587ef7f5646d53db7a564e8d1ac
Eliteronix <zimmermann.mariomarvin@gmail.com>
2023-09-30 00:03:48 +0200

    Create resetFuckedRatings.js

---
+++ commands/admin/resetFuckedRatings.js
@@ -0,0 +1,40 @@
+const { DBDiscordUsers, DBDuelRatingHistory } = require('../../dbObjects');
+const { logDatabaseQueries } = require('../../utils');
+const { Op } = require('sequelize');
+
+module.exports = {
+	name: 'resetSavedRatings',
+	usage: 'None',
+	async execute(interaction) {
+		let fuckedUp = new Date();
+
+		fuckedUp.setDate(26);
+
+		logDatabaseQueries(4, 'commands/admin/resetSavedRatings.js DBDuelRatingHistory');
+		let deleted = await DBDuelRatingHistory.destroy({
+			where: {
+				id: {
+					[Op.gt]: 0,
+				},
+			}
+		});
+
+		await interaction.followUp(`Deleted ${deleted} duel rating histories.`);
+
+		logDatabaseQueries(4, 'commands/admin/resetSavedRatings.js DBDiscordUsers');
+		let updated = await DBDiscordUsers.update({
+			lastDuelRatingUpdate: null,
+		}, {
+			where: {
+				lastDuelRatingUpdate: {
+					[Op.not]: null,
+				},
+			},
+			silent: true
+		});
+
+		return await interaction.followUp(`Updated ${updated} discord users.`);
+	},
+};
+
+
