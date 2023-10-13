fed84d8b78aba30ebfb3d82d0989678d8c46f826
Eliteronix <zimmermann.mariomarvin@gmail.com>
2023-10-14 00:37:07 +0200

    Added admin command

--- commands/admin/noEndDate.js
+++ commands/admin/noEndDate.js
@@ -7,15 +7,18 @@ module.exports = {
 	async execute(interaction) {
 		logDatabaseQueries(4, 'commands/admin/noEndDate.js DBOsuMultiScores');
 		let noEndDate = await DBOsuMultiScores.findAll({
-			attributes: ['matchId', 'matchName', 'matchEndDate', 'createdAt', 'updatedAt'],
+			attributes: ['matchId', 'matchName'],
 			where: {
 				matchEndDate: null,
+				tourneyMatch: true,
 			},
-			group: ['matchId', 'matchName', 'matchEndDate', 'createdAt', 'updatedAt'],
+			group: ['matchId', 'matchName'],
 		});
 
-		// eslint-disable-next-line no-console
-		console.log(noEndDate);
+		for (let i = 0; i < noEndDate.length; i++) {
+			// eslint-disable-next-line no-console
+			console.log('matchId', noEndDate[i].matchId, 'matchName', noEndDate[i].matchName);
+		}
 
 		return await interaction.editReply(`Logged ${noEndDate.length} matches with no end date.`);
 	},
