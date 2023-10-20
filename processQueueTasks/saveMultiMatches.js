d8905d02719cd06f6a1537d3814e78f387892f77
Marvin Zimmermann <64707347+Eliteronix@users.noreply.github.com>
2023-10-20 10:15:09 +0200

    Update saveMultiMatches.js

--- processQueueTasks/saveMultiMatches.js
+++ processQueueTasks/saveMultiMatches.js
@@ -226,7 +226,7 @@ async function processIncompleteScores(osuApi, client, processQueueEntry, channe
 	}
 
 	if (!incompleteMatchScore) {
-		let logVerificationProcess = true;
+		let logVerificationProcess = false;
 
 		// Verify matches instead
 		logDatabaseQueries(2, 'saveOsuMultiScores.js DBOsuMultiScores verify matches');
