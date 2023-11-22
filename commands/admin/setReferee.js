d42a55ddc84abdd08bda1db4d259e527f572ca08
Eliteronix <zimmermann.mariomarvin@gmail.com>
2023-11-22 21:44:11 +0100

    Create setReferee.js

---
+++ commands/admin/setReferee.js
@@ -0,0 +1,26 @@
+const { DBOsuMultiScores } = require('../../dbObjects');
+const { logDatabaseQueries } = require('../../utils');
+
+module.exports = {
+	name: 'setReferee',
+	usage: '<matchId> <referee>',
+	async execute(interaction) {
+		await interaction.deferReply();
+
+		const args = interaction.options.getString('argument').split(/ +/);
+
+		const matchId = args[0];
+		const referee = args[1];
+
+		logDatabaseQueries(4, 'commands/admin/setReferee.js DBOsuMultiScores');
+		let count = await DBOsuMultiScores.update({
+			referee: referee,
+		}, {
+			where: {
+				matchId: matchId,
+			},
+		});
+
+		return await interaction.editReply(`Set ${count} scores to reffed by ${referee}.`);
+	},
+};
