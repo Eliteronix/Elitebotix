2ecb165a21dc0bbc01ec23b96294c9cc413d7d06
Eliteronix <zimmermann.mariomarvin@gmail.com>
2024-02-04 18:58:55 +0100

    Added more info about missing data

--- commands/admin/refereeLeaderboard.js
+++ commands/admin/refereeLeaderboard.js
@@ -6,7 +6,49 @@ module.exports = {
 	name: 'refereeLeaderboard',
 	usage: 'None',
 	async execute(interaction) {
-		logDatabaseQueries(4, 'commands/admin/refereeLeaderboard.js DBOsuMultiMatches');
+		logDatabaseQueries(4, 'commands/admin/refereeLeaderboard.js DBOsuMultiMatches count matches with missing referee info');
+		const refereesToBeDetermined = await DBOsuMultiMatches.count({
+			where: {
+				tourneyMatch: true,
+				referee: null,
+			},
+		});
+
+		let additionalInfo = '';
+
+		if (refereesToBeDetermined > 0) {
+			logDatabaseQueries(4, 'commands/admin/refereeLeaderboard.js DBOsuMultiMatches oldest match with missing referee info');
+			const oldestMissingMatch = await DBOsuMultiMatches.findOne({
+				attributes: [
+					'matchStartDate',
+				],
+				where: {
+					tourneyMatch: true,
+					referee: null,
+				},
+				order: [
+					['matchId', 'ASC'],
+				],
+			});
+
+			logDatabaseQueries(4, 'commands/admin/refereeLeaderboard.js DBOsuMultiMatches youngest match with missing referee info');
+			const youngestMissingMatch = await DBOsuMultiMatches.findOne({
+				attributes: [
+					'matchStartDate',
+				],
+				where: {
+					tourneyMatch: true,
+					referee: null,
+				},
+				order: [
+					['matchId', 'DESC'],
+				],
+			});
+
+			additionalInfo = `There are ${refereesToBeDetermined} matches from <t:${Date.parse(oldestMissingMatch.matchStartDate) / 1000}:f> till <t:${Date.parse(youngestMissingMatch.matchStartDate) / 1000}:f> that are still missing referee info.\n`;
+		}
+
+		logDatabaseQueries(4, 'commands/admin/refereeLeaderboard.js DBOsuMultiMatches find all referees per match');
 		const refereesPerMatch = await DBOsuMultiMatches.findAll({
 			attributes: [
 				'referee',
@@ -39,7 +81,7 @@ module.exports = {
 		// Sort by number of matches reffed descending
 		refereeLeaderboard.sort((a, b) => b.refereeCount - a.refereeCount);
 
-		let leaderboardString = '# Referee leaderboard\n```';
+		let leaderboardString = `# Referee leaderboard\n${additionalInfo}\`\`\``;
 
 		for (let i = 0; i < refereeLeaderboard.length && i < 100; i++) {
 			const refereeUsername = await getOsuPlayerName(refereeLeaderboard[i].referee);
