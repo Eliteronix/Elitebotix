7936cfe4b96a19307a2c31f170e89906c4a3bb6a
Eliteronix <zimmermann.mariomarvin@gmail.com>
2024-02-10 22:06:57 +0100

    Added unique amount

--- commands/admin/multiScoresDBSize.js
+++ commands/admin/multiScoresDBSize.js
@@ -1,5 +1,5 @@
 const { DBOsuMultiMatches, DBOsuMultiGames, DBOsuMultiGameScores } = require('../../dbObjects');
-const { logDatabaseQueries } = require('../../utils');
+const { logDatabaseQueries, humanReadable } = require('../../utils');
 
 module.exports = {
 	name: 'multiScoresDBSize',
@@ -7,17 +7,20 @@ module.exports = {
 	async execute(interaction) {
 		logDatabaseQueries(4, 'commands/admin/multiScoresDBSize.js DBOsuMultiMatches');
 		const matchesAmount = await DBOsuMultiMatches.count();
+		const uniqueMatchesAmount = await DBOsuMultiMatches.count({ distinct: true, col: 'matchId' });
 
-		await interaction.followUp(`Matches DB Size: ${matchesAmount}`);
+		await interaction.followUp(`Matches DB Size: ${humanReadable(matchesAmount)} (Unique: ${humanReadable(uniqueMatchesAmount)})`);
 
 		logDatabaseQueries(4, 'commands/admin/multiScoresDBSize.js DBOsuMultiGames');
 		const gamesAmount = await DBOsuMultiGames.count();
+		const uniqueGamesAmount = await DBOsuMultiGames.count({ distinct: true, col: 'gameId' });
 
-		await interaction.followUp(`Games DB Size: ${gamesAmount}`);
+		await interaction.followUp(`Games DB Size: ${humanReadable(gamesAmount)} (Unique: ${humanReadable(uniqueGamesAmount)})`);
 
 		logDatabaseQueries(4, 'commands/admin/multiScoresDBSize.js DBOsuMultiGames');
 		const gameScoresAmount = await DBOsuMultiGameScores.count();
+		const uniqueGameScoresAmount = await DBOsuMultiGameScores.count({ distinct: true, group: ['gameId', 'osuUserId'] }); // distinct by gameId and osuUserId
 
-		await interaction.followUp(`Games Scores DB Size: ${gameScoresAmount}`);
+		await interaction.followUp(`Games Scores DB Size: ${humanReadable(gameScoresAmount)} (Unique: ${humanReadable(uniqueGameScoresAmount.length)})`);
 	},
 };
