856a3e1cf458e9322084b9101a330a65a530019f
Eliteronix <zimmermann.mariomarvin@gmail.com>
2025-04-06 22:17:48 +0200

    Create interactionResponse.js

---
+++ processQueueTasks/interactionResponse.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+module.exports = {
+	async execute(client, bancho, processQueueEntry) {
+		let args = processQueueEntry.additions.split(';');
+
+		let interaction = new Discord.InteractionWebhook(client, client.application.id, args[0]);
+
+		await interaction.editMessage(args[1]);
+
+		processQueueEntry.destroy();
+	},
+};
