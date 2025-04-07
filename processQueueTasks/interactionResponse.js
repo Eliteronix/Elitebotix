123dce0930d88ae82cd7bb860916c08bf80486bb
Marvin Zimmermann <zimmermann.mariomarvin@gmail.com>
2025-04-07 21:05:10 +0200

    Added logging

--- processQueueTasks/interactionResponse.js
+++ processQueueTasks/interactionResponse.js
@@ -3,6 +3,9 @@ const Discord = require('discord.js');
 module.exports = {
 	async execute(client, bancho, processQueueEntry) {
 		let args = processQueueEntry.additions.split(';');
+		console.log(args[0]);
+		console.log(args[1]);
+		console.log(args[2]);
 
 		let interaction = new Discord.InteractionWebhook(client, client.application.id, args[0]);
 
