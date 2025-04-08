3d1569ff43af4353a16de994e7e428dceeeab2e7
Marvin Zimmermann <64707347+Eliteronix@users.noreply.github.com>
2025-04-08 07:13:56 +0000

    Update interactionResponse.js

--- processQueueTasks/interactionResponse.js
+++ processQueueTasks/interactionResponse.js
@@ -1,26 +1,12 @@
+const Discord = require('discord.js');
+
 module.exports = {
-	async execute(client, bancho, processQueueEntry) {
-		client.shard.broadcastEval(async (c, { additions }) => {
-			try{
-			const Discord = require('discord.js');
-			
-			let args = additions.split(';');
+	async execute(client, bancho, processQueueEntry) {			
+		let args = additions.split(';');
 
-			let interaction = new Discord.InteractionWebhook(c, c.application.id, args[0]);
+		let interaction = new Discord.InteractionWebhook(client, client.application.id, args[0]);
 
-			console.log(interaction);
-
-			let message = await interaction.fetchMessage();
-
-			console.log(message);
-
-			console.log(interaction);
-
-			await interaction.editMessage(args[1], args[2]);
-			} catch (e) {
-				console.error(e);
-			}
-		}, { context: { additions: processQueueEntry.additions } });
+		await interaction.send(args[2]);
 
 		processQueueEntry.destroy();
 	},
