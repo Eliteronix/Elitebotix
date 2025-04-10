044691008b9b4a79edda5d29e694dde2878fe816
Eliteronix <zimmermann.mariomarvin@gmail.com>
2025-04-10 21:38:09 +0200

    Updated rosupp

--- utils.js
+++ utils.js
@@ -4212,6 +4212,8 @@ module.exports = {
 			// Free the beatmap manually to avoid risking memory leakage.
 			map.free();
 
+			console.log(currAttrs.pp, 'pp');
+
 			return currAttrs.pp;
 		} catch (e) {
 			if (depth < 3) {
