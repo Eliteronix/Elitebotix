3551acf267c2f8c49694158612e5e89389264c3b
Eliteronix <zimmermann.mariomarvin@gmail.com>
2025-04-16 19:26:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -239,7 +239,7 @@ let osuWebRequestQueue = [];
 let manager = new ShardingManager('./bot.js', {
 	token: process.env.BOTTOKEN,
 	execArgv: ['--use_strict', '--unhandled-rejections=warn', '--max-old-space-size=4096'],
-	totalShards: 7
+	totalShards: 6
 });
 
 if (process.env.SERVER === 'Dev') {
