2626200f1dd44071111bd49b7eabd8e32dacf199
Eliteronix <zimmermann.mariomarvin@gmail.com>
2025-04-30 22:06:19 +0200

    Removed todo

--- commands/admin/reffedMatches.js
+++ commands/admin/reffedMatches.js
@@ -10,7 +10,6 @@ module.exports = {
 	name: 'reffedMatches',
 	usage: '<username>',
 	async execute(interaction) {
-		//TODO: Check for use of acronym instead of match name
 		let username = interaction.options.getString('argument');
 
 		//Get the user from the database if possible
