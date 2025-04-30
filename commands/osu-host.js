5e5933bce529328870788e6e40714b1c636b4a74
Eliteronix <zimmermann.mariomarvin@gmail.com>
2025-04-30 23:15:54 +0200

    Removed todo

--- commands/osu-host.js
+++ commands/osu-host.js
@@ -216,7 +216,6 @@ module.exports = {
 				)
 		),
 	// eslint-disable-next-line no-unused-vars
-	//TODO: Check for use of acronym instead of match name
 	async execute(msg, args, interaction, additionalObjects) {
 		try {
 			await interaction.deferReply();
