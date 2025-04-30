a18545812c5e5fc808e12e81ec48201a4b9b4eef
Eliteronix <zimmermann.mariomarvin@gmail.com>
2025-04-30 23:25:48 +0200

    Removed todo

--- commands/osu-skills.js
+++ commands/osu-skills.js
@@ -170,7 +170,6 @@ module.exports = {
 				.setRequired(false)
 		),
 	async execute(msg, args, interaction) {
-		//TODO: Check for use of acronym instead of match name
 		try {
 			await interaction.deferReply();
 		} catch (error) {
