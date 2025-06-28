57324d7ead1c7bef66edceb9037cc77d300b220e
Eliteronix <zimmermann.mariomarvin@gmail.com>
2025-06-28 17:26:07 +0200

    Removed message code

--- commands/weather-set.js
+++ commands/weather-set.js
@@ -1,7 +1,6 @@
 
 const weather = require('weather-js');
 const util = require('util');
-const { populateMsgFromInteraction } = require('../utils');
 const { PermissionsBitField, SlashCommandBuilder, MessageFlags } = require('discord.js');
 const { DBDiscordUsers } = require('../dbObjects');
 const { showUnknownInteractionError } = require('../config.json');
@@ -90,115 +89,86 @@ module.exports = {
 						})
 						.setRequired(true)
 						.addChoices(
-							{ name: 'Celcius', value: 'c' },
-							{ name: 'Fahrenheit', value: 'f' },
+							{ name: 'Celcius', value: 'C' },
+							{ name: 'Fahrenheit', value: 'F' },
 						)
 				)
 		),
-	async execute(interaction, msg, args) {
-		//TODO: Remove message code and replace with interaction code
-		if (msg) {
-			return msg.reply('Please use `/weather-set` instead.');
+	async execute(interaction) {
+		try {
+			await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+		} catch (error) {
+			if (error.message === 'Unknown interaction' && showUnknownInteractionError || error.message !== 'Unknown interaction') {
+				console.error(error);
+			}
+			const timestamps = interaction.client.cooldowns.get(this.name);
+			timestamps.delete(interaction.user.id);
+			return;
 		}
 
-		if (interaction) {
-			msg = await populateMsgFromInteraction(interaction);
+		const discordUser = await DBDiscordUsers.findOne({
+			attributes: ['id', 'weatherDegreeType', 'weatherLocation'],
+			where: {
+				userId: interaction.user.id
+			}
+		});
 
-			try {
-				await interaction.deferReply({ flags: MessageFlags.Ephemeral });
-			} catch (error) {
-				if (error.message === 'Unknown interaction' && showUnknownInteractionError || error.message !== 'Unknown interaction') {
+		if (interaction.options.getSubcommand() === 'unit') {
+			const unit = interaction.options.getString('unit');
+
+			if (discordUser) {
+				discordUser.weatherDegreeType = unit;
+				await discordUser.save();
+			} else {
+				await DBDiscordUsers.create({
+					userId: interaction.user.id,
+					weatherDegreeType: unit
+				});
+			}
+
+			if (unit === 'F') {
+				return await interaction.editReply('Set weather degree type to `Fahrenheit`');
+			} else if (unit === 'C') {
+				return await interaction.editReply('Set weather degree type to `Celsius`');
+			}
+		}
+
+		if (interaction.options.getSubcommand() === 'location') {
+			const location = interaction.options.getString('location');
+
+			const findWeather = util.promisify(weather.find);
+
+			for (let triesBeforeError = 0; triesBeforeError < 5; triesBeforeError++) {
+				try {
+					await findWeather({ search: location, degreeType: 'C' })
+						.then(async (result) => {
+							if (!result[0]) {
+								triesBeforeError = Infinity;
+
+								return await interaction.editReply(`Could not find location \`${location.replace(/`/g, '')}\``);
+							}
+
+							let weatherLocation = result[0].location.name;
+
+							if (discordUser) {
+								discordUser.weatherLocation = weatherLocation;
+								await discordUser.save();
+							} else {
+								await DBDiscordUsers.create({
+									userId: interaction.user.id,
+									weatherDegreeType: 'C',
+									weatherLocation: weatherLocation
+								});
+							}
+
+							await interaction.editReply(`Set weather location to \`${weatherLocation}\``);
+						}).catch(err => {
+							throw err;
+						});
+				} catch (error) {
 					console.error(error);
 				}
-				const timestamps = interaction.client.cooldowns.get(this.name);
-				timestamps.delete(interaction.user.id);
-				return;
-			}
-
-			args = [];
-
-			const discordUser = await DBDiscordUsers.findOne({
-				attributes: ['id', 'weatherDegreeType', 'weatherLocation'],
-				where: {
-					userId: msg.author.id
-				}
-			});
-
-			if (interaction.options.getSubcommand() === 'unit') {
-				for (let i = 0; i < interaction.options._hoistedOptions.length; i++) {
-					args.push(interaction.options._hoistedOptions[i].value);
-				}
-				if (interaction.options.getSubcommand() === 'unit') {
-					// set default degree type to fahrenheit
-					if (args[0] === 'f') {
-						if (discordUser) {
-							discordUser.weatherDegreeType = 'F';
-							await discordUser.save();
-						} else {
-							await DBDiscordUsers.create({
-								userId: msg.author.id,
-								weatherDegreeType: 'F'
-							});
-						}
-
-						return await interaction.editReply('Set weather degree type to `Fahrenheit`');
-						// set default degree type to celcius
-
-					} else if (args[0] == 'c') {
-						if (discordUser) {
-							discordUser.weatherDegreeType = 'C';
-							await discordUser.save();
-						} else {
-							await DBDiscordUsers.create({
-								userId: msg.author.id,
-								weatherDegreeType: 'C'
-							});
-						}
-
-						return await interaction.editReply('Set weather degree type to `Celsius`');
-					}
-				}
-
-			}
-
-			if (interaction.options.getSubcommand() === 'location') {
-				for (let i = 0; i < interaction.options._hoistedOptions.length; i++) {
-					args.push(interaction.options._hoistedOptions[i].value);
-				}
-				const findWeather = util.promisify(weather.find);
-
-				for (let triesBeforeError = 0; triesBeforeError < 5; triesBeforeError++) {
-					try {
-						await findWeather({ search: args[0], degreeType: 'C' })
-							.then(async (result) => {
-								if (!result[0]) {
-									triesBeforeError = Infinity;
-
-									return await interaction.editReply(`Could not find location \`${args.join(' ').replace(/`/g, '')}\``);
-								}
-
-								let weatherLocation = result[0].location.name;
-
-								if (discordUser) {
-									discordUser.weatherLocation = weatherLocation;
-									await discordUser.save();
-								} else {
-									await DBDiscordUsers.create({
-										userId: msg.author.id,
-										weatherDegreeType: 'C',
-										weatherLocation: weatherLocation
-									});
-								}
-
-								await interaction.editReply(`Set weather location to \`${weatherLocation}\``);
-							}).catch(err => {
-								throw err;
-							});
-					} catch (error) {
-						console.error(error);
-					}
-					return triesBeforeError = Infinity;
-				}
+				return triesBeforeError = Infinity;
 			}
 		}
 	}
