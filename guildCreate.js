2cf1d3605426188481b6e7dfb2f2e34fd57ab672
Eliteronix <zimmermann.mariomarvin@gmail.com>
2025-07-10 21:00:35 +0200

    Updated permissions check

--- guildCreate.js
+++ guildCreate.js
@@ -5,10 +5,10 @@ module.exports = async function (guild) {
 
 	try {
 		const systemChannel = await guild.channels.cache.get(guild.systemChannelId);
-		if (systemChannel) {
+		if (systemChannel && systemChannel.permissionsFor(guild.members.cache.find(member => member.id === guild.client.user.id)).has('SEND_MESSAGES')) {
 			await systemChannel.send(message);
 		} else {
-			const generalChannel = await guild.channels.cache.find(channel => channel.name === 'general');
+			const generalChannel = await guild.channels.cache.find(channel => channel.name === 'general' && channel.permissionsFor(guild.members.cache.find(member => member.id === guild.client.user.id)).has('SEND_MESSAGES'));
 			if (generalChannel) {
 				await generalChannel.send(message);
 			} else {
@@ -19,6 +19,6 @@ module.exports = async function (guild) {
 			}
 		}
 	} catch (error) {
-		console.error('guildCreate.js | added message' + error);
+		console.error('guildCreate.js | added message ' + error);
 	}
 };
